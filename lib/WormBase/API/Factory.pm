45628d372ed5965da46d555d7f6bca40d072622c
tharris <todd@wormbase.org>
2009-08-22 15:14:08 -0400

    basic non-pluggable api structure

---
+++ lib/WormBase/API/Factory.pm
@@ -0,0 +1,11 @@
+package WormBase::API::Factory;
+use MooseX::AbstractFactory;
+
+# Roles that the factory should implement:
+
+implementation_does qw/WormBase::API::Role::Object/;
+implementation_class_via sub { 'WormBase::API::Object::' . shift };
+
+
+
+1;
