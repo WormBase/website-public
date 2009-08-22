f2ddd1fa032a40101d80858028e3e4a89bbb95a9
tharris <todd@wormbase.org>
2009-08-22 15:22:45 -0400

    pluggable service, role-based architecture

--- lib/WormBase/API/Object/Gene.pm
+++ lib/WormBase/API/Object/Gene.pm
@@ -6,9 +6,16 @@ with 'WormBase::API::Role::Object';
 
 sub name {
     my $self = shift;
-    my $ace  = $self->ace_object;
+    my $ace  = $self->object;
 #    $self->log->debug("here we are " . $self->ace_object);
     return $ace->name;
+}
+
+sub common_name {
+    my $self = shift;
+    my $object = $self->object;
+    my $name = $object->Public_name;
+    return $name;
 }
 
 1;
