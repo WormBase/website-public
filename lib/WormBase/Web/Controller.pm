38bf488da5e63d0e5894e3c12efc07a260005f0d
tharris <todd@wormbase.org>
2009-08-22 17:06:27 -0400

    reinstated generic accessors

--- lib/WormBase/Web/Controller.pm
+++ lib/WormBase/Web/Controller.pm
@@ -2,29 +2,29 @@ package WormBase::Web::Controller;
 
 use strict;
 use warnings;
-use base 'Catalyst::Controller';
+use parent 'Catalyst::Controller';
 
 #########################################
 # Accessors for configuration variables #
 #########################################
 sub pages {
-  my ( $self, $c ) = @_;
-  my @pages = keys %{ $c->config->{pages} };
-  return sort @pages;
+    my ( $self, $c ) = @_;
+    my @pages = keys %{ $c->config->{pages} };
+    return sort @pages;
 }
 
 sub widgets {
-  my ( $self, $page, $c ) = @_;
-  my (@widgets) = @{ $c->config->{pages}->{$page}->{widget_order} };
-  return @widgets;
+    my ( $self, $page, $c ) = @_;
+    my (@widgets) = @{ $c->config->{pages}->{$page}->{widget_order} };
+    return @widgets;
 }
 
 sub fields {
-  my ( $self, $page, $widget, $c ) = @_;
-  my @fields = eval { @{ $c->config->{pages}->{$page}->{widgets}->{$widget} }; };
+    my ( $self, $page, $widget, $c ) = @_;
+    my @fields = eval { @{ $c->config->{pages}->{$page}->{widgets}->{$widget} }; };
 #  @fields || die
 #    "Check configuration for $page:$widget: all widgets specified in widget_order must exist in 'widgets'";
-  return @fields;
+    return @fields;
 }
 
 1;
