5a74614f5b536124058d828c9bd9773621fb4d53
Todd Harris <bitbucket@tharris.org>
2009-10-19 19:03:57 -0400

    new independent model stubs

---
+++ lib/WormBase/API/Object/CDS.pm
@@ -0,0 +1,21 @@
+package WormBase::API::Object::CDS;
+
+use Moose;
+
+with 'WormBase::API::Role::Object';
+
+sub name {
+    my $self = shift;
+    my $ace  = $self->object;
+#    $self->log->debug("here we are " . $self->ace_object);
+    return $ace->name;
+}
+
+sub common_name {
+    my $self = shift;
+    my $object = $self->object;
+    my $name = $object->Public_name;
+    return $name;
+}
+
+1;
