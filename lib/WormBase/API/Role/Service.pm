db82ecff8fb1936f14706fcf7f00e31291d1398b
Todd Harris <bitbucket@tharris.org>
2009-10-19 20:02:01 -0400

    small tweaks to Service roles

--- lib/WormBase/API/Role/Service.pm
+++ lib/WormBase/API/Role/Service.pm
@@ -2,11 +2,13 @@ package WormBase::API::Role::Service;
 
 use Moose::Role;
 
+# Every Service should implement a connect method
 requires 'connect';
 
 has symbolic_name => (
     is => 'ro',
     isa => 'Str',
+    documentation => 'A simple symbolic name for the service, typically a single word, e.g. "acedb"',
     );
 
 has version => (
