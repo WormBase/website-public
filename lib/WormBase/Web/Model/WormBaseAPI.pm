6da82f355677297f1e56f1c38becb16e0542b17a
Todd Harris <bitbucket@tharris.org>
2009-10-20 21:12:33 -0400

    glue to bring in the standalone API

---
+++ lib/WormBase/Web/Model/WormBaseAPI.pm
@@ -0,0 +1,14 @@
+package WormBase::Web::Model::WormBaseAPI;
+use parent qw/Catalyst::Model::Adaptor/;
+
+# Fetch the default args and pass along some extras
+# including our C::Log::Log4perl
+sub prepare_arguments {
+  my ($self, $c) = @_;
+  my $args     = $c->config->{'Model::WormBaseAPI'}->{args};
+  $args->{log} = $c->log;
+  return $args;
+}
+
+
+1;
