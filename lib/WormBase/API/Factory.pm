a8136942fc680671d5691487389443ca95079aec
Todd Harris <bitbucket@tharris.org>
2009-10-25 12:10:47 -0400

    added some docs

--- lib/WormBase/API/Factory.pm
+++ lib/WormBase/API/Factory.pm
@@ -3,6 +3,8 @@ use MooseX::AbstractFactory;
 
 # Roles that the factory should implement:
 implementation_does qw/WormBase::API::Role::Object/;
+
+# Generate the appropriate class name
 implementation_class_via sub { 'WormBase::API::Object::' . shift };
 
 
