099ba2a3edcde72110e8769519f4282a8f4a6752
Todd Harris <bitbucket@tharris.org>
2009-12-03 14:40:18 -0500

    simplifying service/. Really no need for nested directories

--- lib/WormBase/API/Service/gff_c_elegans.pm
+++ lib/WormBase/API/Service/gff_c_elegans.pm
@@ -1,16 +1,21 @@
-package WormBase::API::Service::gff::gff_c_elegans;
+package WormBase::API::Service::gff_c_elegans;
 
 use Moose;
 use Bio::DB::GFF;
 
 with 'WormBase::API::Role::Service::gff';    # Must come first as it provides connect();
 with 'WormBase::API::Role::Service';
-with 'WormBase::API::Role::Logger';
+#with 'WormBase::API::Role::Logger';
 
 has 'species' => (
     is => 'ro',
     isa => 'Str',
     default => 'c_elegans',
+    );
+
+has function => (
+    is => 'ro',
+    default => 'fjfkdfk',
     );
 
 
@@ -26,7 +31,7 @@ sub fetch_gff_gene {
     
     my $dbh = $self->dbh;
 
-    ($trans)      = grep {$_->method eq 'full_transcript'} $dbh->fetch_group(Transcript => $transcript) unless $trans;
+    ($trans) = grep {$_->method eq 'full_transcript'} $dbh->fetch_group(Transcript => $transcript) unless $trans;
     
     # Now pseudogenes
     ($trans) = grep {$_->method eq 'pseudo'} $dbh->fetch_group(Pseudogene => $transcript) unless ($trans);
