f7aa445e788441129bcb1f3b46d292cf9781f45f
Xiaoqi Shi <xshi@oicr.on.ca>
2010-03-12 17:43:17 -0500

    reading from config file

--- lib/WormBase/API/Service/acedb.pm
+++ lib/WormBase/API/Service/acedb.pm
@@ -1,41 +1,18 @@
 package WormBase::API::Service::acedb;
 
 use Moose;
-use Ace;
+use Ace ();
 
 has 'dbh' => (
     is        => 'rw',
     isa       => 'Ace',
     predicate => 'has_dbh',
     writer    => 'set_dbh',
-    handles   => [qw/fetch/],
+#     handles   => [qw/fetch/],
     );
 
 # Roles to consume.
 with 'WormBase::API::Role::Service';
-#with 'WormBase::API::Role::Logger';
-
-has 'cache_root' => (
-    is => 'ro',
-    isa => 'Str',
-    );
-
-has 'cache_expires' => (
-    is => 'ro',
-    isa => 'Str',
-    default => '1 day',
-    );
-
-has 'cache_size' => (
-    is => 'ro',
-    isa => 'Str',
-    );
-
-has 'cache_auto_purge_interval' => (
-    is  => 'ro',
-    isa => 'Str',
-    default => '6 hours',
-    );
 
 has 'path' => (
     is => 'ro',
@@ -54,16 +31,16 @@ has 'query_timeout' => (
 
 sub connect {
     my $self = shift;
-    my ($host,$port,$user,$pass)=@_;
+    my ($host)=@_;
     my @cache = (-cache => {
-	cache_root => $self->cache_root,
-	max_size   => $self->cache_size
+	cache_root => $self->conf->{cache_root},
+	max_size   => $self->conf->{cache_size}
 	    || $Cache::SizeAwareCache::NO_MAX_SIZE
 	    || -1,  # hardcoded $NO_MAX_SIZE constant
-	    default_expires_in  => $self->cache_expires,
-	    auto_purge_interval => $self->cache_auto_purge_interval,
+	    default_expires_in  => $self->conf->{cache_expires},
+	    auto_purge_interval => $self->conf->{cache_auto_purge_interval},
 		 } 
-	) if $self->cache_root;
+	) if $self->conf->{cache_root};
     
     return Ace->connect(-host => $host,
 			      -port => $self->port,
@@ -83,11 +60,16 @@ sub BUILD {
     my $self = shift;
     $self->symbolic_name("acedb");
     $self->function("get connection to AceDB database");
-    $self->species('');
     # record all the info from Conf file $self->conf_dir
-    $self->hosts([qw/aceserver.cshl.edu/]);
-    $self->port(2005);
+    my @hosts;
+#     push @hosts ,$self->conf->{acedb_host};
+    $self->hosts([$self->conf->{acedb_host}]);
+    $self->port($self->conf->{acedb_port});
 
 }
 
+sub fetch {
+    my $self=shift;
+    return $self->dbh->fetch(@_); 
+}
 1;
