68fd39c3dbf7f45f56377d9303af7ff01fa8162c
Xiaoqi Shi <xshi@oicr.on.ca>
2010-03-12 17:48:59 -0500

    buid database object from conf file and pass it to each WormBase::API::Object::* when creating them

--- lib/WormBase/API.pm
+++ lib/WormBase/API.pm
@@ -1,13 +1,12 @@
 package WormBase::API;
 
 use Moose;                       # Moosey goodness
-use Module::Pluggable::Object;   # Support for pluggable services
 
 use namespace::clean -except => 'meta';
 use WormBase::API::Factory;      # Our object factory
+use Config::General;
 
 with 'WormBase::API::Role::Logger';           # A basic Log::Log4perl screen appender
-
 
 # We assume that there is a single default data source.
 # For now this is AceDB.
@@ -20,16 +19,10 @@ has 'default_datasource' => (
 
 # Dynamically establish a list of available data services.
 # This includes the default_datasource and other singletons.
-# During BUILD we will establish data handles to each.
 has '_services' => (
     is         => 'ro',
     isa        => 'HashRef',
     lazy_build => 1,
-    );
-
-has '_classes' => (
-    is         => 'rw',
-    isa        => 'HashRef',
     );
 
 has 'stringified_responses' => (
@@ -45,73 +38,59 @@ has conf_dir => (
     is       => 'ro',
     required => 1,
     );
+# This is the configuration object/hashref for databases info e.g. connection hosts password...
+has database => (
+    is       => 'ro',
+    required => 1,
+    lazy_build => 1,
+    );
 
+# this is here just for the testing script to load database configuration
+# may be removed or changed in furutre! 
+sub _build_database {
+    my $self = shift;
+    my $root  = $self->conf_dir;
+    my $conf = new Config::General(
+				  -ConfigFile      => "$root/../wormbase.conf",
+				  -InterPolateVars => 1
+    );
+    return $conf->{'DefaultConfig'}->{'Model::WormBaseAPI'}->{args}->{database} ;
+}
+ 
 
 # Version should be provided by the default datasource or set explicitly.
 sub version {
     my $self = shift;
-    
     # Fetch the dbh for the default datasource
     my $service = $self->_services->{$self->default_datasource};
     return $service->version;
 }
 
-# Fetch a service object by its symbolic name
-sub service {
-    my $self = shift;
-    my $name = shift;
-    return $self->_services->{$name};
-}
-
-# This is really just a convenience accessor
-# as all the gff databases are also stored under services.
-# Perhaps services should be a deeper data structure, 
-# for example, keyed by species...
-sub gff_dsn {
-    my $self    = shift;
-    my $species = shift;
-
-    unless(exists $self->_services->{"gff_$species"}) {
-      my $class=$self->_classes->{"gff"};
-      $self->_services->{"gff_$species"} = $class->new({conf_dir => $self->conf_dir,
-			      log      => $self->log,
-			      species => $species,
-				    });
-    }
-    return $self->_services->{"gff_".$species}; 
-
-}
-
+ 
 # Build a hashref of services, including things like the 
 # default datasource, GFF databases, etc.
 # Note the double underscores...
 sub _build__services {
     my ($self) = @_;
-    my $base = __PACKAGE__;
-
-    my $mp = Module::Pluggable::Object->new( search_path => [ $base . "::Service" ] );
-    
     my %services;
-    my %api_classes;
-    my @classes = $mp->plugins;
-    foreach my $class (@classes) {
-	$class =~ /.*::?(.*)/;	
-	Class::MOP::load_class($class);
-	
-	# Fetch the base name of the class. Could possibly be nested
-#	(my $name = $class) =~ s/\Q${base}::\E//;
-	$class =~ /.*::?(.*)/;	
-	my $type=$1;
-	$api_classes{$type}= $class;
-	# Instantiate the service providing it with
-	# access to some of our configuration variables
-	my $new = $class->new({conf_dir => $self->conf_dir,
-				     log      => $self->log,
-				    });
-	$type=$new->species? $type.'_'.$new->species:$type;
-	$services{$type} = $new; 
+    for my $dbn (sort keys %{$self->database}) {
+      my $class = __PACKAGE__ . "::Service::$dbn" ;	
+      Class::MOP::load_class($class);
+      # Instantiate the service providing it with
+      # access to some of our configuration variables
+      my @species = sort keys %{$self->database->{$dbn}->{data_sources}}; 
+      push @species, '' unless @species;
+        
+      foreach my $sp (@species) {
+	  my $new = $class->new({	conf => $self->database->{$dbn},
+					log      => $self->log,
+					species	 => $sp,
+				      });
+	  my $type=$sp? $dbn.'_'.$sp:$dbn;
+	  $services{$type} = $new; 
+	  $self->log->debug( "service $type registered but not connected yet");
+      }
     }
-    $self->_classes(\%api_classes);
     return \%services;
 } 
 
@@ -129,33 +108,19 @@ sub fetch {
     my $object = $args->{object};
     
     if ($object) {
-	my $class = $object->class;
-	return WormBase::API::Factory->create($class,
-					      { object => $object });
-    } else {
-	
-	# Try fetching an object (from the default data source)
-	my $service_symbolic = $self->default_datasource;
-	 
-	my $service_instance = $self->_services->{$service_symbolic};
-#	my $driver = $self->service($service)
-	my $dbh = $service_instance->dbh;
-	  
-	my $object = $service_instance->fetch(-class=>$class,-name=>$name);
-#	my $object = $dbh->fetch(-class=>$class,-name=>$name);
-	# TODO!!
-	# Calling my factory causes instantiation of new Service::* objects
-	# (ie new ddatabase connections).  This is bad.
-	# - It also *requires* that we pass the conf_dir
-	#   which is only used for setting up the log object
-	
-	# To get around this (for now) I'm passing the conf_dir
-	# but this is SERIOUSLY NOT OPTIMAL
-	return WormBase::API::Factory->create($class,
-					      { object   => $object,
-						conf_dir => $self->conf_dir,
-					      });
+	$class = $object->class;
     }
+    else {
+	# Try fetching an object (from the default data source)
+	my $service_instance = $self->_services->{$self->default_datasource}; 
+	$object = $service_instance->fetch(-class=>$class,-name=>$name);
+    }
+    return WormBase::API::Factory->create($class,
+					      { object   => $object,
+						log => $self->log,
+						dsn	 => $self->_services,
+					      });
+    
 }
 
 
