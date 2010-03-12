e0554f711d6c28ff4a055f1d6e70ccfaa934fdf6
Xiaoqi Shi <xshi@oicr.on.ca>
2010-03-12 17:47:09 -0500

    add dsn(database name) hashref and gff_dsn method to provide persistent db connection

--- lib/WormBase/API/Role/Object.pm
+++ lib/WormBase/API/Role/Object.pm
@@ -8,4 +8,15 @@ has 'object' => (
     required => 1,
     );
 
+has 'dsn' => (
+    is  => 'ro',
+    isa => 'HashRef',
+    required => 1,
+    );
+
+sub gff_dsn {
+    my $self    = shift;
+    my $species = shift;
+    return $self->dsn->{"gff_".$species}; 
+}
 1;
