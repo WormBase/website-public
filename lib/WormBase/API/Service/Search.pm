46b84b96127382a0162fbbabff2cdb911b0876ff
Abigail Cabunoc <acabunoc@oicr.on.ca>
2010-05-18 17:13:07 -0400

    Brought over gene serach algorithms from old site

--- lib/WormBase/API/Service/Search.pm
+++ lib/WormBase/API/Service/Search.pm
@@ -12,30 +12,93 @@ sub basic {
   my ($self,$args) = @_;
   my $class  = $args->{class};
   my $pattern   = $args->{pattern};
-  my ($count,@objs);
-  @objs = $self->dbh->fetch(-class=>$class,
-			    -pattern=>$pattern,
-			    -total=>\$count);
+  my @objs = $self->dbh->fetch(-class=>$class,
+			    -pattern=>$pattern);
   return (\@objs) if @objs;
 }
 
 # Search for gene objects
 sub gene {
   my ($self,$args) = @_;
-  my $pattern   = $args->{pattern};
-  my ($count,@objs);
+  my $query   = $args->{pattern};
+#   my ($count,@objs);
 
-# implement searching for things other than WBGeneID
-  @objs = $self->dbh->fetch(-class=>'Gene',
-			    -pattern=>$pattern,
-			    -total=>\$count);
+  my $DB = $self->dbh;
+  my (@genes,%seen);
 
-  # don't bother formatting object if we're going to redirect
-  if(scalar @objs == 1){ return \@objs;} 
+  if ($query =~ /^WBG.*\d+/) {
+    @genes = $self->dbh->fetch(-class=>'Gene',
+			    -pattern=>$query);
+  } elsif (my @gene_names = $DB->fetch(-class=>'Gene_name',-name=>$query,-fill=>1)) {
+      # HACK!  For cases in which a gene is assigned to more than one Public_name_for.
+      @genes = grep { !$seen{$_}++} map { $_->Public_name_for } @gene_names;
+
+      @genes = grep {!$seen{$_}++} map {$_->Sequence_name_for
+					    || $_->Molecular_name_for
+					    || $_->Other_name_for
+					} @gene_names unless @genes;
+      undef @gene_names;
+  } elsif (my @gene_classes = $DB->fetch(-class=>'Gene_class',-name=>$query,-fill=>1)) {
+      @genes = map { $_->Genes } @gene_classes;
+  } elsif (my @transcripts = $DB->fetch(-class=>'Transcript',-name=>$query,-fill=>1)) {
+      @genes = map { eval { $_->Corresponding_CDS->Gene } } @transcripts;
+  } elsif (my @ests = $DB->fetch(-class=>'Sequence',-name=>$query,-fill=>1)) {
+    foreach (@ests) {
+      if (my $gene = $_->Gene(-filled=>1)) {
+	push @genes,$gene;
+      } elsif (my $cds = $_->Matching_CDS(-filled=>1)) {
+	my $gene = $cds->Gene(-filled=>1);
+	push @genes,$gene if $gene;
+      }
+    }
+  }elsif (my @variations = $DB->fetch(-class=>'Variation',-name=>$query,-fill=>1)) {
+      @genes = map { eval { $_->Gene} } @variations;
+  }
+
+# Try finding genes using general terms
+  # 1. Homology_group
+  # 2. Concise_description
+  # 3. Gene_class
+
+  unless (@genes) {
+      my @homol = $DB->fetch(-query=>qq{find Homology_group where Title=*$query*});
+      @genes = map { eval { $_->Protein->Corresponding_CDS->Gene } } @homol;
+      push (@genes,map { $_->Genes } $DB->fetch(-query=>qq{find Gene_class where Description="*$query*"}));
+      push (@genes,$DB->fetch(-query=>qq{find Gene where Concise_description=*$query*}));
+  }
+
+  unless (@genes) {
+      my @accession_number = $DB->fetch(Accession_number => $query);
+      my %seen;
+      my @cds = grep {  $seen{$_}++ } map { $_->CDS } @accession_number;
+      push @cds,grep { !$seen{$_}++ } map { eval {$_->Protein->Corrsponding_CDS } } @accession_number;
+      push @cds,grep { !$seen{$_}++ } map { eval {$_->Sequence->Matching_CDS }  } @accession_number;
+      @genes =  grep { !$seen{$_}++ } map { $_->Gene } @cds;
+  }
+  # Analyze the Other_name_for of the Gene_name to see if the gene
+  # corresponds to another named gene.
+  my (@unique_genes);
+  %seen = ();
+  foreach my $gene (@genes) {
+    next if defined $seen{$gene};
+    my $gene_name  = $gene->Public_name;
+    my @other_names = eval { $gene_name->Other_name_for; };
+    foreach my $other_name (@other_names) {
+      if ($other_name ne $gene) {
+	$seen{$other_name}++;
+      }
+    }
+    push (@unique_genes,$gene);
+    $seen{$gene}++;
+  }
 
   my $result = __PACKAGE__ . "::Result";
-  @objs = map { $result->new($_)} @objs;
-  return (\@objs) if @objs;
+  @unique_genes = map { $result->new($_)} @unique_genes;
+
+  # don't bother formatting object if we're going to redirect
+  if(scalar @unique_genes == 1){ return \@unique_genes;} 
+
+  return (\@unique_genes) if @unique_genes;
 }
 
 # Search for variataion objects
