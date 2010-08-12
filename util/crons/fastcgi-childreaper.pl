978aa36c27a00c3fa568ce68c1e11053f5aee050
tharris <bitbucket@tharris.org>
2010-08-12 15:42:28 -0600

    set an upper limit in the child reaper at 250 MB, in bytes no less, cuz that is how I roll

--- util/crons/fastcgi-childreaper.pl
+++ util/crons/fastcgi-childreaper.pl
@@ -3,14 +3,17 @@
 # FastCGI child reaper (to trap slow copy-to-write memory leaks)
 
 # Run as cron every 30 minutes:
-# */30 * * * * /usr/local/wormbase/admin/crons/fastcgi-childreaper.pl `cat /var/run/wormbase/wormbase.pid` 104857600
+# */30 * * * * /usr/local/wormbase/admin/crons/fastcgi-childreaper.pl `cat /var/run/wormbase/wormbase.pid` [104857600]
 
 
 use strict;
 
 my ($ppid, $bytes) = @ARGV;
 
-die "Usage: kidreaper PPID ram_limit_in_bytes\n" unless $ppid && $bytes;
+die "Usage: kidreaper PPID ram_limit_in_bytes\n" unless $ppid;
+
+# 250 MB limit for now
+$bytes ||= '262144000' unless $by
 
 my $kids;
 
