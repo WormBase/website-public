6f875a26c0102c4e1866bd656a01aeaab33886e8
Abigail Cabunoc <acabunoc@oicr.on.ca>
2010-08-19 14:06:54 -0400

    fixing sortable in firefox

--- root/js/effect.js
+++ root/js/effect.js
@@ -32,24 +32,5 @@
 	});
   });
 
-   //     // Tooltips
-   //     $("a[title]").tooltip({
-   //
-   //  // use div.tooltip as our tooltip
-   //  //  tip: '.tooltip',
-   //
-   //  // use the fade effect instead of the default
-   //  effect: 'fade',
-   //
-   //  // make fadeOutSpeed similar to the browser's default
-   //  fadeOutSpeed: 100,
-   //
-   //  // the time before the tooltip is shown
-   //  predelay: 400,
-   //
-   //  // tweak the position
-   //   position: "bottom right",
-   //   offset: [-50, -80]
-   //  });
 
  
