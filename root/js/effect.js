44eab9bb54fb46dd577e56cf732c7055f8608fdd
Abigail Cabunoc <acabunoc@oicr.on.ca>
2010-08-31 14:27:16 -0400

    guest store session widget display

--- root/js/effect.js
+++ root/js/effect.js
@@ -30,46 +30,48 @@
         $(mytitle).slideToggle("fast");
         $(mytitle).next().slideToggle("fast");
         $(this).parent().toggleClass("minimized");
-        if ($(this).attr("show") == 0){
+        if ($(this).attr("show") != 1){
           $(this).attr("show", 1);
+          $(this).attr("title", "maximize");
           $(this).removeClass("ui-icon-circle-triangle-s");
           $(this).removeClass("ui-icon-triangle-1-s");
           $(this).addClass("ui-icon-circle-triangle-e");
         }else{
           $(this).attr("show", 0);
+          $(this).attr("title", "minimize");
           $(this).removeClass("ui-icon-circle-triangle-e");
           $(this).addClass("ui-icon-circle-triangle-s");
         }
       });
 
 //       $(".toggle").addClass("ui-icon ui-icon-plus").show();
-      $(".module-min").addClass("ui-icon ui-icon-triangle-1-s");
+      $(".module-min").addClass("ui-icon ui-icon-triangle-1-s").attr("title", "minimize");;
       $(".module-close").addClass("ui-icon ui-icon-close").hide();
       $(".widget-container").children("footer").hide();
       $("#nav-min-icon").addClass("ui-icon ui-icon-triangle-1-w");
 
       $(".widget-container").hover(
         function () {
-          $(this).children("header").children(".module-close").show();
-          if($(this).children("header").children("h3").children(".module-min").attr("show") == 0){
+          $(this).children("header").children(".ui-icon").show();
+          if($(this).children("header").children("h3").children(".module-min").attr("show") != 1){
             $(this).children("footer").show();
           }
         }, 
         function () {
-          $(this).children("header").children(".module-close").hide();
+          $(this).children("header").children(".ui-icon").hide();
           $(this).children("footer").hide();
         }
       );
 
       $(".module-min").hover(
         function () {
-          if ($(this).attr("show")==0){ $(this).addClass("ui-icon-circle-triangle-s");
+          if ($(this).attr("show")!=1){ $(this).addClass("ui-icon-circle-triangle-s");
           }else{ $(this).addClass("ui-icon-circle-triangle-e");}
         }, 
         function () {
           $(this).removeClass("ui-icon-circle-triangle-s");
           $(this).removeClass("ui-icon-circle-triangle-e");
-          if ($(this).attr("show")==0){ $(this).addClass("ui-icon-triangle-1-s");
+          if ($(this).attr("show")!=1){ $(this).addClass("ui-icon-triangle-1-s");
           }else{ $(this).addClass("ui-icon-triangle-1-e");}
         }
       );
