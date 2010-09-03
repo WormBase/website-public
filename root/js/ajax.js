71ce363713b3271f185bd623ad15d9b0e0c78987
Xiaoqi Shi <xshi@oicr.on.ca>
2010-09-03 13:08:11 -0400

    add status-bar ajax

--- root/js/ajax.js
+++ root/js/ajax.js
@@ -11,6 +11,16 @@
 				      });
 	return false;
       });
+      
+       $(".status-bar").load("/rest/auth", function(response, status, xhr) {
+	if (status == "error") {
+	  var msg = "Sorry but there was an error: ";
+	  $("#error").html(msg + xhr.status + " " + xhr.statusText);
+	}
+      });
+    });
+
+
     });
 
   $(".update").live('click',function() {
