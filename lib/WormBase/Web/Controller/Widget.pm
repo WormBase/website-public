0b70f476f4fbb06a3e901a3a566e5e2847365e9d
Abigail Cabunoc <acabunoc@oicr.on.ca>
2010-09-27 16:27:49 -0400

    incorporating bench with report style

--- lib/WormBase/Web/Controller/Widget.pm
+++ lib/WormBase/Web/Controller/Widget.pm
@@ -7,23 +7,23 @@ use parent 'WormBase::Web::Controller';
 
 sub toggle :Path('/widget/toggle')  {
     my ( $self, $c, $class, $widget_name) = @_;
-    my $curr = $c->user_session->{$class}->{$widget_name};
+    my $curr = $c->user_session->{'display'}->{$class}->{$widget_name};
     $curr = -1 unless defined $curr;
-    $c->user_session->{$class}->{'count'} ||= 1;
+    $c->user_session->{'display'}->{$class}->{'count'} ||= 1;
     if ($curr == -1) { 
-      my $count = $c->user_session->{$class}->{'count'}++; 
-      $c->user_session->{$class}->{$widget_name} = $count;
+      my $count = $c->user_session->{'display'}->{$class}->{'count'}++; 
+      $c->user_session->{'display'}->{$class}->{$widget_name} = $count;
     } else { 
-      delete $c->user_session->{$class}->{$widget_name};
+      delete $c->user_session->{'display'}->{$class}->{$widget_name};
     }
 }
 
 sub order :Path('/widget/order') {
     my ($self, $c, $class, $widget_name, $order) = @_;
     if($order){
-      $c->user_session->{$class}->{$widget_name} = $order;
+      $c->user_session->{'display'}->{$class}->{$widget_name} = $order;
     } else {
-      $c->user_session->{$class}->{'count'} = $widget_name;
+      $c->user_session->{'display'}->{$class}->{'count'} = $widget_name;
     }
 }
 
