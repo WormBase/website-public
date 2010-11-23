3459287149f040a4ffa55ab1de9934d197cc23f8
Abigail Cabunoc <acabunoc@oicr.on.ca>
2010-11-23 11:13:37 -0500

    restructure

--- lib/WormBase/Web/Controller/Tool.pm
+++ lib/WormBase/Web/Controller/Tool.pm
@@ -25,4 +25,30 @@ sub tool :Path("/tools") Args {
     }
 }
 
+
+sub issue :Path("tools/issues") Args {
+    my ( $self, $c ,$id) = @_;
+    unless($id) {
+      $c->stash->{template} = "feed/issue.tt2";
+      my @issues = $c->model('Schema::Issue')->search(undef);
+      $c->stash->{issues} = \@issues;
+      $c->stash->{issues_type} = "all";
+      $c->stash->{current_time}=time();
+      return;
+    }
+    $c->stash->{template} = "feed/issue_page.tt2";
+    my $issue = $c->model('Schema::Issue')->find($id);
+    $c->stash->{issue} = $issue;
+    my @threads= $issue->issues_to_threads(undef,{order_by=>'thread_id ASC' } ); 
+    $c->stash->{current_time}=time();
+    my $last;
+    if(@threads){
+      $c->stash->{threads} = \@threads ;
+      $last = $threads[scalar @threads -1];
+      $c->stash->{last_edit}=$last->user ;
+    }
+    $c->stash->{last_edit}= $issue->owner unless($last);
+     
+} 
+
 1;
