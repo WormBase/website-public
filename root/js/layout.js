fa0385706f537d223eda49c52aa71ba134ee63bc
Abigail Cabunoc <acabunoc@oicr.on.ca>
2010-11-29 11:25:57 -0500

    layouting

--- root/js/layout.js
+++ root/js/layout.js
@@ -113,16 +113,16 @@ $(function() {
     var $sidebar   = $("#navigation"),
         $window    = $(window),
         offset     = 100,
-        at_default = 0-($sidebar.children("#title").outerHeight() + 5);
+        at_default = -45;
 
     $window.scroll(function() {
-      var bottomPos = $sidebar.parent().height() - $sidebar.outerHeight() -2;
+      var bottomPos = $sidebar.parent().height() - $sidebar.outerHeight();
       if( bottomPos < at_default )
           bottomPos = at_default;
       var objBiggerThanWindow = $sidebar.outerHeight() > $(window).height();
       if (objBiggerThanWindow){ return; }
       if ($window.scrollTop() > offset) {
-          var newpos = $window.scrollTop() - offset + 3 + at_default;
+          var newpos = $window.scrollTop() - offset + 10 + at_default;
           if (newpos > bottomPos)
               newpos = bottomPos;
               $sidebar.stop().css(
