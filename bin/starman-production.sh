86bdffbf4013181b6113db788a0f68a7e1d0b0a4
Todd Harris <bitbucket@tharris.org>
2010-12-01 15:18:17 -0500

    fixed some paths for the new website/VERSION structure

--- bin/starman-production.sh
+++ bin/starman-production.sh
@@ -5,6 +5,13 @@ export APP="production"
 export APPLIB="WormBase"
 export WORKERS=5
 export PORT=5000
+export PERL5LIB=/usr/local/wormbase/website/production/extlib/lib/perl5:/usr/local/wormbase/website/production/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/production/lib:$PERL5LIB
+
+#export PATH="/usr/local/wormbase/website-classic/extlib/bin:$PATH"
+export MODULEBUILDRC="/usr/local/wormbase/website/production/extlib/.modulebuildrc"
+export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/website/production/extlib"
+export PATH="/usr/local/wormbase/website/production/extlib/bin:$PATH"
+
 
 # this runs site-init.sh, assuming it's in the same directory
 . "$( cd "$( dirname "$0" )" && pwd )/wormbase-init.sh"
