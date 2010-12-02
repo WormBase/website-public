4b14f607dad53a44fb20aa70f4fb7920d33ddc82
Todd Harris <bitbucket@tharris.org>
2010-12-02 18:05:16 -0500

    starman configs for dev sites

---
+++ bin/starman-tharris.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# settings
+export APP="tharris"
+export APPLIB="WormBase"
+export WORKERS=5
+export PORT=6001
+export PERL5LIB=/usr/local/wormbase/website/tharris/extlib/lib/perl5:/usr/local/wormbase/website/tharris/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/tharris/lib:$PERL5LIB
+
+#export PATH="/usr/local/wormbase/website-classic/extlib/bin:$PATH"
+export MODULEBUILDRC="/usr/local/wormbase/website/tharris/extlib/.modulebuildrc"
+export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/website/tharris/extlib"
+export PATH="/usr/local/wormbase/website/tharris/extlib/bin:$PATH"
+
+
+# this runs site-init.sh, assuming it's in the same directory
+. "$( cd "$( dirname "$0" )" && pwd )/wormbase-init.sh"
