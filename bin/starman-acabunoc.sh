d0ad45d1a52ffb6114650f4cb627f43065fbd693
Todd Harris <bitbucket@tharris.org>
2010-12-03 09:41:42 -0500

    adjusted ports

--- bin/starman-acabunoc.sh
+++ bin/starman-acabunoc.sh
@@ -4,7 +4,7 @@
 export APP="acabunoc"
 export APPLIB="WormBase"
 export WORKERS=5
-export PORT=10002
+export PORT=9003
 export PERL5LIB=/usr/local/wormbase/website/acabunoc/extlib/lib/perl5:/usr/local/wormbase/website/acabunoc/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/acabunoc/lib:$PERL5LIB
 
 #export PATH="/usr/local/wormbase/website-classic/extlib/bin:$PATH"
