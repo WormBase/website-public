447c5444c17cbdb402abbfafbdc6241b136b020b
Xiaoqi Shi <xshi@oicr.on.ca>
2010-12-07 20:11:58 -0500

    user registration

--- root/js/miscellaneous.js
+++ root/js/miscellaneous.js
@@ -3,6 +3,24 @@
 /***************************/
 $jq('input[value=[% c.user.roles %]]:radio').attr('checked', 'checked');
 
+function validate_fields(email,username, password, confirm_password){
+	if( email.val() ==""){
+		      alert("Please provide your email address."); email.focus();return false;
+	} else if( validate_email(email.val(),"Not a valid email address!")==false) {
+		      email.focus();return false;
+	} else if( username.val() =="") {
+		      alert("Please provide your username");username.focus(); return false;
+	} else if(password) {
+	    if( password.val() ==""){
+		      alert("Please provide your password."); password.focus();return false;
+	    } else if( password.val() != confirm_password.val()) {
+			alert("The passwords do not match. Please enter again"); password.focus();return false;
+	    }  
+	} else {
+	  return true;
+	}
+}
+
 function validate_email(field,alerttxt)
 {
  
