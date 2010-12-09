9db4bac9972eb88b7fb23c5afc3ac685374e63fc
norie <noriedlc3@gmail.com>
2010-12-09 10:36:29 -0500

    lab.pm: updated for display

--- lib/WormBase/API/Object/Laboratory.pm
+++ lib/WormBase/API/Object/Laboratory.pm
@@ -4,41 +4,11 @@ use Moose;
 with 'WormBase::API::Role::Object';
 extends 'WormBase::API::Object';
 
-has 'ao_template' => (    
-	is  => 'ro',
-    isa => 'Ace::Object',
-    lazy => 1,
-    default => sub {
-    	
-    	my $self = shift;
-    	my $ao_object = $self->pull;
-    	return $ao_object;
-  	}
-);
 
 
-#######
+########
 
-sub template {
-
-	my $self = shift;
-    my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my %data_pack;
-
-	#### data pull and packaging
-
-	####
-
-	$data{'data'} = \%data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-}
-
-### mainly for text data; and single layer hash ###
-
-sub template_simple {
+sub name {
 
 	my $self = shift;
     my $object = $self->object;
@@ -48,7 +18,32 @@ sub template_simple {
 
 	#### data pull and packaging
 
-	$data_pack = $object->Tag;
+	$data_pack = {
+	
+		'id' => "$object",
+		'label' => "$object",
+		'class' => 'Laboratory'
+	
+	};
+
+	####
+	
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+sub phone {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->Phone;
 
 	####
 	
@@ -58,7 +53,66 @@ sub template_simple {
 }
 
 
-########
+
+sub fax {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->Fax;
+
+	####
+	
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+
+sub email {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->Email;
+	####
+	
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+
+sub web_site {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->URL;
+
+	####
+	
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
 
 
 ##########
@@ -108,25 +162,25 @@ sub representatives {
     
       my @representatives = $lab->Representative;
 
-      foreach my $rep (@representatives) {
-	my $name = $rep->Standard_name;
-	my $laboratory = $rep->Laboratory;
-	my @a   = $rep->Address(2);
-	foreach (@a) {
-	  $_ = $_->right if $_->right;  # AtDB damnation
-	}
+   	foreach my $rep (@representatives) {
+		my $name = $rep->Standard_name;
+		my $laboratory = $rep->Laboratory;
+		my @a   = $rep->Address(2);
+		foreach (@a) {
+	  		$_ = $_->right if $_->right;  # AtDB damnation
+		}
 
-	my $email = $rep->get('Email' => 1);
-	$email = eval{$email->right if $email->right;};
+		my $email = $rep->get('Email' => 1);
+		$email = eval{$email->right if $email->right;};
 
-	$data_pack{$rep} = {
+		$data_pack{$rep} = {
 	      'ace_id' => $rep,
 	      'name' => $name,
 	      'laboratory' => $laboratory,
 	      'class' => 'Person',
 	      'address' => \@a,
 	      'email' => $email
-	  };
+	  	};
 
 	}
 
@@ -137,51 +191,113 @@ sub representatives {
 	return \%data;
 }
 
-# 
-
-#   if ($name) {
-#     print h2($name);
-#     print "Fax  : " . . br if $lab->Fax;
-#     print "Phone: " .  . br if $lab->Phone;
-#     print "Email: " .  . br if $lab->Email;
-#   }
 
 ####################
 # genes and alleles
 ####################
 
-sub gene_classes {
+sub gene_class {
 
 	my $self = shift;
-	my $lab = $self->object;
+    my $object = $self->object;
 	my %data;
 	my $desc = 'notes';
-	my %data_pack;
+	my @data_pack = [];
 
 	#### data pull and packaging
 
 	my @gene_classes;
-	@gene_classes = $lab->get('Gene_classes');
+	@gene_classes = $object->get('Gene_classes');	
 
 	foreach my $gene_class (@gene_classes) {
-	    
-	  my $phenotype = $gene_class->Phenotype;
-	  my $description = $gene_class->Description;
-
-	  $data_pack{$gene_class} = {
-	    'phenotype' => $phenotype,
-	    'description' => $description
-	  };
-	  
+	
+	push @data_pack, {
+	
+						'id' => "$gene_class",
+						'label' => "$gene_class",
+						'class' => 'Gene_class'
+						};
 	}
 
-
 	####
-
-	$data{'data'} = \%data_pack;
+	
+	$data{'data'} = \@data_pack;
 	$data{'description'} = $desc;
 	return \%data;
 }
+
+sub allele_designation {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->Allele_designation;
+
+	####
+	
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+sub strain_designation {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->Strain_designation;
+
+	####
+	
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+sub alleles {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my @data_pack;
+
+	#### data pull and packaging
+
+	my @alleles = $object->Alleles;
+
+	foreach my $allele (@alleles) {
+	
+		my $allele_name = $allele->Public_name;
+	
+		push @data_pack, {
+		
+			'id' => "$allele",
+			'lable' => "$allele_name",
+			'class' => 'Variation'
+		
+		};
+	}
+
+	####
+	
+	$data{'data'} = \@data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
 
 sub allele_prefixes {
 
@@ -216,40 +332,69 @@ sub allele_prefixes {
 # lab personnel
 #####################
 
-sub current_members {
+sub current_member {
 
 	my $self = shift;
     my $object = $self->object;
 	my %data;
 	my $desc = 'notes';
-	my %data_pack;
+	my @data_pack;
 
 	#### data pull and packaging
-
+	
+	my @current_members = $object->Registered_lab_members;
+	
+	foreach my $current_member (@current_members) {
+	
+		my $cm_name = $current_member->Full_name;
+		
+		push @data_pack, {
+		
+			'id' => "$current_member",
+			'label' => "$cm_name",
+			'class' => 'Person'
+		};
+	}
+	
 	####
-
-	$data{'data'} = \%data_pack;
+	
+	$data{'data'} = \@data_pack;
 	$data{'description'} = $desc;
 	return \%data;
 }
 
 
-sub past_members {
+sub former_member {
 
 	my $self = shift;
     my $object = $self->object;
 	my %data;
 	my $desc = 'notes';
-	my %data_pack;
+	my @data_pack;
 
 	#### data pull and packaging
+	
+	my @former_members = $object->Past_lab_members;
+	
+	foreach my $former_member (@former_members) {
+	
+		my $fm_name = $former_member->Full_name;
+		
+		push @data_pack, {
+		
+			'id' => "$former_member",
+			'label' => "$fm_name",
+			'class' => 'Person'
+		};
+	}
+	
 
 	####
-
-	$data{'data'} = \%data_pack;
+	
+	$data{'data'} = \@data_pack;
 	$data{'description'} = $desc;
 	return \%data;
-  }
+}
 
 
 1; 
