1ac28d7a77e26078daaf2a1d7e02c2063db16862
Abigail Cabunoc <acabunoc@oicr.on.ca>
2010-12-22 10:14:16 -0500

    link saving and history to session - not user

--- lib/WormBase/Schema/Result/Session.pm
+++ lib/WormBase/Schema/Result/Session.pm
@@ -20,4 +20,12 @@ __PACKAGE__->table('sessions');
 __PACKAGE__->add_columns(qw/id session_data expires/);
 __PACKAGE__->set_primary_key('id');
 
+
+
+__PACKAGE__->many_to_many(pages => 'user_saved', 'page');
+__PACKAGE__->has_many(user_saved=>'WormBase::Schema::Result::UserSave', 'session_id');
+
+__PACKAGE__->many_to_many(visited => 'user_history', 'page');
+__PACKAGE__->has_many(user_history=>'WormBase::Schema::Result::UserHistory', 'session_id');
+
 1;
--- lib/WormBase/Schema/Result/UserHistory.pm
+++ lib/WormBase/Schema/Result/UserHistory.pm
@@ -23,8 +23,8 @@ __PACKAGE__->table("user_history");
 __PACKAGE__->add_columns(
   "user_history_id",
   { data_type => "integer", default_value => 0, is_nullable => 0 },
-  "user_id",
-  { data_type => "integer", default_value => 0, is_nullable => 0 },
+  "session_id",
+  { data_type => "char(72)", default_value => 0, is_nullable => 0 },
   "page_id",
   { data_type => "integer", default_value => 0, is_nullable => 0 },
 );
@@ -34,7 +34,7 @@ __PACKAGE__->set_primary_key("user_history_id");
 # Created by DBIx::Class::Schema::Loader v0.07001 @ 2010-08-20 14:27:11
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:aWUX+zL4WtBgQQBgwyvCyg
 
-__PACKAGE__->belongs_to(user=>'WormBase::Schema::Result::User','user_id');
+__PACKAGE__->belongs_to(session=>'WormBase::Schema::Result::Session','session_id');
 __PACKAGE__->belongs_to(page=>'WormBase::Schema::Result::Page','page_id');
 
 __PACKAGE__->has_many(visits=>'WormBase::Schema::Result::HistoryVisits', 'user_history_id');
