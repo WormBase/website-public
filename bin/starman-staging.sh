2dd59d3a77d3bc76abe6bdbf9219d623097e1ad4
Todd Harris <bitbucket@tharris.org>
2010-12-24 16:47:52 -0500

    launcher for QA/QC site

---
+++ bin/starman-staging.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# settings
+export APP="staging"
+export APPLIB="WormBase"
+export WORKERS=5
+export PORT=5000
+export PERL5LIB=/usr/local/wormbase/website/staging/extlib/lib/perl5:/usr/local/wormbase/website/staging/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/staging/lib:$PERL5LIB
+
+#export PATH="/usr/local/wormbase/website-classic/extlib/bin:$PATH"
+export MODULEBUILDRC="/usr/local/wormbase/website/staging/extlib/.modulebuildrc"
+export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/staging/development/extlib"
+export PATH="/usr/local/wormbase/website/staging/staging/bin:$PATH"
+
+
+# this runs site-init.sh, assuming it's in the same directory
+. "$( cd "$( dirname "$0" )" && pwd )/wormbase-init.sh"
