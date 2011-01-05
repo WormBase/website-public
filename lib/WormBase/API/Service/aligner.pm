816bf2df95725d3df65a81abbfc5e667b76553e5
Xiaoqi Shi <xshi@oicr.on.ca>
2011-01-05 17:54:26 -0500

    add caching to aligner

--- lib/WormBase/API/Service/aligner.pm
+++ lib/WormBase/API/Service/aligner.pm
@@ -92,7 +92,6 @@ sub run {
     my $gff_dsn= $self->gff_dsn;
     $gff_dsn->ace($sequence->db);
     $gff_dsn->reconnect();
-    
     my $dbgff =  $gff_dsn->dbh ;
     $self->log->debug("GFF database:",$dbgff);
     $dbgff->add_aggregator('waba_alignment') if ($dbgff); 
@@ -105,7 +104,7 @@ sub run {
     my $user_start = $param->{"start"};
     my $user_end   = $param->{"end"};
     my $flip_user  = $param->{"flip"};
-    my $user_ragged =  $param->{"ragged"} || BLUMENTHAL_FACTOR;
+    my $user_ragged =  defined $param->{"ragged"} ? $param->{"ragged"} : BLUMENTHAL_FACTOR;
     my $user_override = defined $user_start && defined $user_end && ($user_start || $user_end);
     my $hash = {	
 		sequence => $sequence_id,
@@ -113,7 +112,7 @@ sub run {
 		start => $user_start,
 		end => $user_end,
 		flip => 0,
-		factor => BLUMENTHAL_FACTOR,
+		factor => $user_ragged,
 		algorithm => $self->algorithms,
 		types	  =>\%TYPES,
 		labels	=> \%LABELS,
