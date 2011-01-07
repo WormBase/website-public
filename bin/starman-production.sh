464c71f0dfbbc9ce10e10e055a9ea7d52063b4b9
Todd Harris <bitbucket@tharris.org>
2011-01-07 11:58:38 -0500

    update production starman, which really does double duty as both staging and production

--- bin/starman-production.sh
+++ bin/starman-production.sh
@@ -5,9 +5,8 @@ export APP="production"
 export APPLIB="WormBase"
 export WORKERS=5
 export PORT=5000
-export PERL5LIB=/usr/local/wormbase/website/production/extlib/lib/perl5:/usr/local/wormbase/website/production/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/production/lib:$PERL5LIB
 
-#export PATH="/usr/local/wormbase/website-classic/extlib/bin:$PATH"
+export PERL5LIB=/usr/local/wormbase/website/production/extlib/lib/perl5:/usr/local/wormbase/website/production/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/production/lib:/usr/local/wormbase/website/production/extlib/gbrowse2/current/lib/perl5/x86_64-linux-gnu-thread-multi:$PERL5LIB
 export MODULEBUILDRC="/usr/local/wormbase/website/production/extlib/.modulebuildrc"
 export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/website/production/extlib"
 export PATH="/usr/local/wormbase/website/production/extlib/bin:$PATH"
