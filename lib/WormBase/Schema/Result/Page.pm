203e73d59571fb6c65975145fd3ff022bd9cdf9a
Abigail Cabunoc <acabunoc@oicr.on.ca>
2011-01-17 11:47:49 -0500

    sql fix, advisory board

--- lib/WormBase/Schema/Result/Page.pm
+++ lib/WormBase/Schema/Result/Page.pm
@@ -22,11 +22,11 @@ __PACKAGE__->add_columns(
   "page_id",
   { data_type => "integer", is_nullable => 0 },
    "url",
-  { data_type => "char(72)", is_nullable => 0 },
+  { data_type => "char(255)", is_nullable => 0 },
   "title",
-  { data_type => "text", is_nullable => 1 },
+  { data_type => "char(255)", is_nullable => 1 },
   "is_obj",
-  { data_type => "bool", is_nullable => 1 },
+  { data_type => "boolean", is_nullable => 1 },
 );
 
 __PACKAGE__->set_primary_key("page_id");
