3a06ef092001c89ef2e842a6a37655ba184b09ce
Xiaoqi Shi <xshi@oicr.on.ca>
2011-01-31 16:37:58 -0500

    delete all session related history info

---
+++ lib/WormBase/Web/Controller/Cron.pm
@@ -0,0 +1,19 @@
+package WormBase::Web::Controller::Cron;
+
+use strict;
+use warnings;
+use parent 'WormBase::Web::Controller';
+
+sub remove_sessions : Private {
+    my ( $self, $c ) = @_;
+
+    my $field = $c->session_store_dbi_expires_field;
+    my $rs=  $c->model("Schema::Session")->search({ $field => { '!=', undef },$field => { '<', time },});
+    while(my $obj=$rs->next){
+	$c->log->debug("delete session:",$obj->id);
+	$obj->delete();
+	$obj->update();
+    }
+}
+
+1;
