9fa3ee444ca99ba621aa668e8de96697698e67d7
Adrian Duong <adrian.duong@oicr.on.ca>
2011-02-04 15:54:50 -0500

    Simplified paper overview. Added link box with PMID&DOI (if applicable), CiteULike and Delicious links. CiteULike and Delicious are currently hardcoded external links. Corrected a small spelling mistake in conf file. Paper model cleaned and revised.

--- lib/WormBase/API/Object/Paper.pm
+++ lib/WormBase/API/Object/Paper.pm
@@ -1,6 +1,7 @@
 package WormBase::API::Object::Paper;
 
 use Moose;
+use ParseName qw(parse_name parse_name_initials);
 
 with 'WormBase::API::Role::Object';
 extends 'WormBase::API::Object';
@@ -9,15 +10,15 @@ extends 'WormBase::API::Object';
 
 sub name {
     my $self = shift;
-    my $title = eval {$self ~~ 'Title'} || $self ~~ 'name';
+    my $title = $self ~~ 'Title' // $self ~~ 'name';
     $title =~ s/\.*$//;
     my $data = {
-					description => 'The object name of the paper',
-					data => {
-								id		=> $self ~~ 'name',
-								label	=> $title,
-								class	=> $self ~~ 'class'
-					},
+		description => 'The object name of the paper',
+		data => {
+			id		=> $self ~~ 'name',
+			label	=> $title,
+			class	=> $self ~~ 'class'
+		   },
 	};
     return $data;
 }
@@ -30,172 +31,203 @@ sub name {
 ############################################################
 
 sub title {
-    my $self = shift;
-    my $title = eval {$self ~~ 'Title'} || return;
+	my ($self) = @_;
+    my $title = $self ~~ 'Title' // return;
     $title =~ s/\.*$//;
     my $data = {
-					description	=> 'The title of the paper',
-					data		=> $title,
+		description	=> 'The title of the paper',
+		data		=> $title,
     };
     return $data;
 }
 
 sub journal {
-    my $self = shift;
-    my $journal = eval {$self ~~ 'Journal'} || return;
+	my ($self) = @_;
+    my $journal = $self ~~ 'Journal' // return;
     $journal =~ s/\.*$//;
     my $data = { description => 'The journal the paper was published in',
-		 data        => $journal,
-    };
+				 data        => $journal,
+			 };
     return $data;
 }
 
 sub page {
-    my $self = shift;
-    my $page = eval {$self ~~ 'Page'} || return;
+	my ($self) = @_;
+    my $page = $self ~~ 'Page' // return;
     $page =~ s/\.*$//;
-    my $data = { description => 'The page numbers of the paper',
-		 data        => $page,
+    my $data = {
+		description => 'The page numbers of the paper',
+		data        => $page,
     };
     return $data;
 }
 
 sub volume {
-    my $self = shift;
-    my $volume = eval {$self ~~ 'Volume'} || return;
+	my ($self) = @_;
+    my $volume = $self ~~ 'Volume' // return;
     $volume =~ s/\.*$//;
-    my $data = { description => 'The volume the paper was published in',
-		 data        => $volume,
+    my $data = { 
+		description => 'The volume the paper was published in',
+		data        => $volume,
     };
     return $data;
 }
 
 sub year {
     my $self = shift;
-    my $year = $self->_parse_year($self ~~ 'Publication_date');
-    return unless $year;
-    my $data = { description => 'The publication year of the paper',
-		 data        => $year,
+	my $date = $self ~~ 'Publication_date' // return;
+    my $data = {
+		description => 'The publication year of the paper',
+		data        =>  $self->_parse_year($date),
     };
     return $data;
 }
 
 sub publication_date {
-	my $self = shift;
-	my $date = $self ~~ 'Publication_date';
+	my ($self) = @_;
+	my $date = $self ~~ 'Publication_date' // return;
 	my $data = {
-					description => 'The publication date of the paper',
-					data		=> $date,
+		description => 'The publication date of the paper',
+		data		=> $date,
 	};
 	return $data;
 }
 
 sub authors {
-    my $self = shift;
-  # this is the long form of author
+	my ($self) = @_;
+	# this is the long form of author
+	my $authors = $self ~~ '@Author';
+	return unless @$authors;
+
     my @authors;
-    foreach my $author (@{$self ~~ '@Author'}) {
-	  my $obj = $author;
-	  foreach my $col ($author->col) {
-	    if($col eq 'Person') {
-	      $obj = $col->right;
-	    }
-	  }
-	  push(@authors,{
-			  id=>$obj,
-			  class=>$obj->class,
-			  label =>$author ,
-		      });
+    foreach my $author (@$authors) {
+		my $obj = $author;
+		foreach my $col ($author->col) {
+			$obj = $col->right if $col eq 'Person';
+		}
+
+		my @authorname = parse_name_initials($author);
+		my $label = @authorname > 1 ?
+		  "$authorname[-1], " . join('. ', @authorname[0..$#authorname-1]) . '.' :
+			$authorname[0];		# author's name in APA format
+
+		push(@authors,{
+			id=>$obj,
+			class=>$obj->class,
+			label => $label,
+		});
     }
-    my $data = { description => 'The authors of the paper',
-		 data        => \@authors,
-    };
+    my $data = {
+		description => 'The authors of the paper',
+		data        => \@authors,
+	};
     return $data;
 }
 
 sub publication_type {
-	my $self = shift;
-	my $paper = $self->object;
+	my ($self) = @_;
+	my $type = $self ~~ '@Type';
+	return unless @$type;
 
-    return ''; # TODO
+	return {
+		description => 'Type of publication',
+		data => $type,
+	};
 }
 
 sub abstract {
-    my $self = shift;
-    my $abs = $self ~~ 'Abstract';
+	my ($self) = @_;
+    my $abs = $self ~~ 'Abstract' // return;
     my $abstext = $self->ace_dsn->fetch(LongText=>$abs);
     my $text = "";
 
     if ($abstext =~ /WBPaper/i ) {
-	 $text = $abstext->right;
-	 $text=~s/^\n+//gs;
-	 $text=~s/\n+$//gs;
+		$text = $abstext->right;
+		$text=~s/^\n+//gs;
+		$text=~s/\n+$//gs;
     }
 
-    my $data = {description => 'The abstract of the paper',
-		 data        => $text,
-    };
+    my $data = {
+		description => 'The abstract of the paper',
+		data        => $text,
+	};
     return $data;
 }
 
 sub keywords {
-	my $self = shift;
+	my ($self) = @_;
 	my $keywords = $self->_pack_objects($self ~~ '@Keyword');
 	my $data = {
-					description => 'Keywords related to the paper',
-					data		=> $keywords,
+		description => 'Keywords related to the paper',
+		data		=> $keywords,
 	};
 	return $data;
 }
 
-sub PMID {
-	my $self = shift;
+sub doi {
+	my ($self) = @_;
+	my $name = $self ~~ 'Name' // return;
+	my $field;
+	if ($name =~ m{^(?:doi.*)?(10\.[^/]+.+)$}) {
+		$field = {
+			description => 'DOI of paper',
+			data => $1,
+		};
+	}
+	return $field;
+}
+
+sub pmid {
+	my ($self) = @_;
 	my @dbfields = $self->object->Database(2);
 	foreach (@dbfields) {
-		return $_->right if $_ == 'PMID'; # ?Accession_number (should be PMID)
+		if ($_ == 'PMID') {
+			return {
+				description => 'PubMed ID of paper',
+				data => $_->right,
+			};
+		}
 	}
-	return ''; # why can't I return; ?
+	return;
 }
 
 sub intext_citation {
-	my $self = shift;
+	my ($self) = @_;
 
-	my $packed_authors = $self->authors->{data};
+	my $packed_authors = $self->authors->{data} // return;
 	my $year = $self->year->{data};
 
 	my $innertext;
-	if (@$packed_authors > 5) { # 6..inf
+	if (@$packed_authors > 5) {					 # 6..inf
 		my $author = $packed_authors->[0]->{id}; # will be author or person object
-		$innertext = _extract_last_name_from_person($author) . ' et al.';
+		$innertext = (parse_name($author))[-1] . ' et al.';
 	}
 	else {
 		my @authors = map $_->{id}, @$packed_authors;
-		if (@authors > 2) { # 3..5
-			my @lnames = map {_extract_last_name_from_person($_)} @authors;
+		if (@authors > 2) {		# 3..5
+			my @lnames = map {(parse_name($_))[-1]} @authors;
 			$innertext = join(', ', @lnames[0..$#lnames]) . ', & ' . $lnames[-1];
 		}
-		elsif (@authors > 0) {  # 1..2
-			$innertext = join(' & ', map {_extract_last_name_from_person($_)} @authors);
+		else {
+			$innertext = join(' & ', map {(parse_name($_))[-1]} @authors);
 		}
 	}
 
 	return unless $innertext;
 
-	if (defined $year) {
-		$innertext .= ", $year";
-	}
-	$innertext = "($innertext)";
+	$innertext .= ", $year" if defined $year;
 
 	my $data = {
-				description => 'APA in-text citation',
-				data => $innertext
-			   };
+		description => 'APA in-text citation',
+		data => '(' . $innertext . ')',
+		paper => $self->object,
+	};
 	return $data;
 }
 
 
 sub refers_to {
-	my $self = shift;
+	my ($self) = @_;
 	my %data;
 	my @refers_to = $self->object->Refers_to;
 	foreach my $ref_type (@refers_to) {
@@ -203,50 +235,56 @@ sub refers_to {
 		$data{$ref_type} = $self->_pack_objects(\@columns);
 	}
 	my $fields = {
-				  description => 'Objects that the paper refers to',
-				  data => \%data,
-				 };
+		description => 'Objects that the paper refers to',
+		data => \%data,
+	};
 	return $fields;
 }
 
-# below are deprecated in favour of refers_to()
-
 sub genes {
-	my $self = shift;
+	my ($self) = @_;
 	my $genes = $self->_pack_objects($self ~~ '@Gene');
+	return unless @$genes;
+
 	my $data = {
-					description => 'Genes related to or mentioned in paper',
-					data => $genes,
-				};
+		description => 'Genes related to or mentioned in paper',
+		data => $genes,
+	};
 	return $data;
 }
 
 sub alleles {
-	my $self = shift;
+	my ($self) = @_;
 	my $alleles = $self->_pack_objects($self ~~ '@Allele');
+	return unless @$alleles;
+
 	my $data = {
-					description => 'TODO',
-					data		=> $alleles,
+		description => 'Alleles referenced by the paper',
+		data		=> $alleles,
 	};
 	return $data;
 }
 
 sub interactions {
-	my $self = shift;
+	my ($self) = @_;
 	my $interactions = $self->_pack_objects($self ~~ '@Interaction');
+	return unless @$interactions;
+
 	my $data = {
-					description => '',
-					data => $interactions,
+		description => 'Interactions referenced by the paper',
+		data => $interactions,
 	};
 	return $data;
 }
 
 sub strains {
-	my $self = shift;
+	my ($self) = @_;
 	my $strains = $self->_pack_objects($self ~~ '@Strain');
+	return unless @$strains;
+
 	my $data = {
-					description => '',
-					data 		=> $strains,
+		description => 'Strains referenced by the paper',
+		data 		=> $strains,
 	};
 	return $data;
 }
@@ -257,40 +295,5 @@ sub strains {
 #
 ############################################################
 
-# should consider moving this into Person?
-sub _extract_last_name_from_person {
-	my $person = shift;
-	return unless eval{$person->isa('Ace::Object')};
-
-	my $lastname;
-	if ($person->class eq 'Author') {
-		$lastname ||= _extract_last_name($person->Full_name);
-		$lastname ||= _extract_last_name($person->Also_known_as);
-		$lastname ||= _extract_last_name("$person");
-	}
-	elsif ($person->class eq 'Person') {
-		$lastname ||= $person->Last_name;
-		$lastname ||= _extract_last_name($person->Full_name);
-		$lastname ||= _extract_last_name($person->Standard_name);
-	}
-	return $lastname;
-}
-
-sub _extract_last_name {
-	$_ = shift;
-	s/^[^A-Za-z]+//; # strip non-alphas from front
-	s/[^A-Za-z.]+$//; # strip non-alphas (exc. '.') from back
-	s/_/ /g; # underscores should be spaces
-	s/- +/-/g; # fix hyphenation
-	my $lastname_part;
-	if (/,/) {
-		$lastname_part = (split /,/)[0];
-	}
-	else {
-	  s/ +([A-Z]\.? *)+$//;
-	  $lastname_part = $_;
-	}
-	return (split/[. ]/, $lastname_part)[-1];
-}
-
 1;
+
