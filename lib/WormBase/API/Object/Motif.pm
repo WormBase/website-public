7c8763e0493fb28345840fd27a7a471a613f7747
norie <noriedlc3@gmail.com>
2011-02-21 15:06:08 -0500

    update for display considerations

--- lib/WormBase/API/Object/Motif.pm
+++ lib/WormBase/API/Object/Motif.pm
@@ -4,61 +4,9 @@ use Moose;
 with 'WormBase::API::Role::Object';
 extends 'WormBase::API::Object';
 
-has 'ao_template' => (    
-	is  => 'ro',
-    isa => 'Ace::Object',
-    lazy => 1,
-    default => sub {
-    	
-    	my $self = shift;
-    	my $ao_object = $self->pull;
-    	return $ao_object;
-  	}
-);
-
 
 #######
 
-sub template {
-
-	my $self = shift;
-    my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my %data_pack;
-
-	#### data pull and packaging
-
-	####
-
-	$data{'data'} = \%data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-}
-
-### mainly for text data; and single layer hash ###
-
-sub template_simple {
-
-	my $self = shift;
-    my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my $data_pack;
-
-	#### data pull and packaging
-
-	$data_pack = $object->Tag;
-
-	####
-	
-	$data{'data'} = $data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-}
-
-
-########
 
 sub identification {
 
@@ -111,7 +59,208 @@ sub identification {
 	return \%data;
 }
 
+###################
+## Identification
+###################
+
+sub title {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->Title;
+
+
+	####
+
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+sub remarks {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my $data_pack;
+
+	#### data pull and packaging
+
+	$data_pack = $object->Remark;
+
+	####
+
+	$data{'data'} = $data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+sub database  {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my %data_pack;
+
+	#### data pull and packaging
+
+	my ($database,$accession1,$accession2) = $object->Database('@')->row;
+	my $accession = $accession2 || $accession1;
+	
+	%data_pack = ('database' => "$database",
+					'accession' => "$accession"
+	             );
+
+	####
+
+	$data{'data'} = \%data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+sub data {
+
+	my $self = shift;
+	my $tag = shift; ## Associated_transposon_family Match_sequence Num_mismatch
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my %data_pack;
+
+	#### data pull and packaging
+
+	$data_pack{'data'} = $object->Title;
+	$data_pack{'title'} = $tag;
+	$data_pack{'title'} =~ s/_/ /g;
+
+	####
+
+	$data{'data'} = \%data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+
+
+####################
+## homology
+####################
+
+
 sub homologies {
+
+	my $self = shift;
+    my $object = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my @data_pack;
+
+	#### data pull and packaging
+	
+	
+    foreach (qw/DNA_homol Pep_homol Motif_homol Homol_homol/) {
+    
+		if (my @homol = $motif->$_) {
+				
+			foreach (@homol) {
+			
+				my $id;
+				my $label;
+				my $class;
+				my $homolog_data;
+						 
+				if ($_ =~ /.*RepeatMasker/g) {
+					$_ =~ /(.*):.*/;
+					my $clone = $1;
+					$id = "$clone";
+					$label = "$clone";
+					$class = 'Clone';
+					
+					$homolog_data = {
+						
+						'id' => "$id",
+						'label' => "$label",
+						'class' => $class
+					};
+
+				} else
+				
+					$homolog_data = _pack_obj($_);	
+				}
+				
+				push @data_pack, $homolog_data;
+			}
+		}       
+	####
+
+	$data{'data'} = \@data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+###################
+## gene ontology
+###################
+
+
+
+sub go  {
+
+	my $self = shift;
+    my $motif = $self->object;
+	my %data;
+	my $desc = 'notes';
+	my v;
+
+	#### data pull and packaging
+
+	my @go_terms;
+	
+	@go_terms = $motif->GO_term;
+	
+	foreach my $go_term (@go_terms) {
+	
+		my $definition = $go_term->Definition;
+		my ($evidence) = $go_term->right;
+		my $term = $go_term->GO_term;
+		my $go_data;
+
+		$go_data = (		
+					'term_data' => {
+						'id'=> "$go_term",
+						'label' => "$term",
+						'class'=>'GO_term'
+					},
+				
+				'definition'=>$definition,
+				'evidence'=>$evidence
+				);
+				
+		push @data_pack, $go_data;		
+	}
+
+	####
+
+	$data{'data'} = \%data_pack;
+	$data{'description'} = $desc;
+	return \%data;
+}
+
+
+
+sub homologies_old {
 
 	my $self = shift;
     my $motif = $self->object;
@@ -147,45 +296,6 @@ sub homologies {
 }
 
 
-sub go  {
-
-	my $self = shift;
-    my $motif = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my %data_pack;
-
-	
-
-	#### data pull and packaging
-
-	my @go_terms;
-	
-	@go_terms = $motif->GO_term;
-	
-	foreach my $go_term (@go_terms) {
-	
-		my $definition = $go_term->Definition;
-		my ($evidence) = $go_term->right;
-	
-	
-	
-		$data_pack{$go_term} = (
-								'ace_id'=>$go_term
-								,'class'=>'GO_term'
-								,'definition'=>$definition
-								,'evidence'=>$evidence
-								);
-	}
-
-
-
-	####
-
-	$data{'data'} = \%data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-}
 
 
 
