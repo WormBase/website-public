270b48b8445a13b025ea1e403ec92f0eacc637fd
Adrian Duong <adrian.duong@oicr.on.ca>
2011-02-25 14:15:44 -0500

    Intial commmit (dummy template). Clone model draft.

---
+++ lib/WormBase/API/Object/Clone.pm
@@ -0,0 +1,135 @@
+package WormBase::API::Object::Clone;
+
+use Moose;
+
+with 'WormBase::API::Role::Object';
+extends 'WormBase::API::Object';
+
+
+sub type {
+	my ($self) = @_;
+
+	my $type = $self ~~ 'Type';
+	return {
+		description => 'The type if this clone',
+		data		=> $type && $type->name,
+	};
+}
+
+sub sequences {
+	my ($self) = @_;
+
+	my $sequences = $self->_pack_objects($self ~~ '@Sequence');
+	# TODO: there's some bit of extra Sequence data displayed in classic...
+	return {
+		description => 'Sequences assocaited with this clone',
+		data		=> %$sequences ? $sequences : undef,
+	}
+}
+
+sub length {
+	my ($self) = @_;
+
+	my %data;
+	foreach (qw(Seq_length Gel_length)) {
+		my $length = $self ~~ $_;
+		$data{lc $_} = $length if $length;
+	}
+
+	return {
+		description => 'Lengths relevant to this clone',
+		data		=> %data ? \%data : undef,
+	};
+}
+
+sub map {
+	my ($self) = @_;
+
+	my $map = $self ~~ '@Map';
+	$map = eval {[$self->object->Pmap->Map] } unless @$map;
+
+	return {
+		description => 'Maps this Clone is assigned to',
+		data		=> $map && @$map ? $self->_pack_objects($map) : undef,
+	};
+}
+
+
+sub sequence_status {
+	my ($self) = @_;
+
+	my %status = map { $_ => $_->right->name } @{$self ~~ 'Sequence_status'};
+	return {
+		description => 'Sequence status of clone',
+		data		=> %status ? \%status : undef;
+	};
+}
+
+sub canonical_for {
+	my ($self) = @_;
+
+	my $canonical = $self->_pack_obj($self ~~ '@Canonical_for');
+	return {
+		description => 'Canonical for',
+		data		=> %$canonical ? $canonical : undef,
+	};
+}
+
+sub canonical_parent {
+	my ($self) = @_;
+
+	my @canonical_parent = map {$self->_pack_obj($_)}  (
+		$self ~~ 'Approximate_Match_to',
+		$self ~~ 'Exact_Match_to',
+		$self ~~ 'Funny_Match_to',
+	   );
+
+	return {
+		description => 'Canonical parent for clone',
+		data		=> \@canonical_parent,
+	}
+}
+
+
+sub screened_positive {
+	my ($self) = @_;
+
+	my $data = $self->_pack_objects([$self->object->Positive(2)]);
+	# TODO: "weak" logic from classic...
+	return {
+		description => 'Screened positive for',
+		data		=> %$data ? $data : undef,
+	};
+}
+
+sub screened_negative {
+	my ($self) = @_;
+
+	my $data = $self->_pack_objects([$self->object->Negative(2)]);
+	return {
+		description => 'Screened negative for',
+		data		=> %$data ? $data : undef,
+	};
+}
+
+sub gridded_on {
+	my ($self) = @_;
+
+	my $data = $self->_pack_objects($self ~~ '@Gridded');
+	return {
+		description => 'Grid this clone was gridded on',
+		data		=> %$data ? $data : undef,
+	};
+}
+
+sub references {
+	my ($self) = @_;
+
+	my $data = $self->_pack_objects($self ~~ '@Reference');
+	return {
+		description => 'References for this clone',
+		data		=> %$data ? $data : undef,
+	};
+}
+
+1;
