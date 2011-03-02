a4ffb5c90fd52893de5a12115ee92f792ac09402
norie <noriedlc3@gmail.com>
2011-03-01 23:44:58 -0500

    converted go_term.pm

--- lib/WormBase/API/Object/GO_Term.pm
+++ lib/WormBase/API/Object/GO_Term.pm
@@ -36,246 +36,416 @@ http://wormbase.org/species/go_term
 # Supplied by Role; POD will automatically be inserted here.
 # << include name >>
 
+=head2 term
 
+<headvar>This method will return a data structure with the term for this GO_Term.
 
+=head3 PERL API
+
+ $data = $model->term();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/term
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub term {
-
 	my $self = shift;
     my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my $data_pack;
-
-	#### data pull and packaging
-
-	$data_pack = $object->Term;
-
-	####
-	
-	$data{'data'} = $data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-
+	my $tag_object = $object->Term;
+	my $data_pack = $self->_pack_obj($object, $tag_object);
+	return {
+		'data'=> $data_pack,
+		'description' => 'GO term'
+		};
 }
 
+=head2 definition
+
+This method will return a data structure definition for this GO_Term.
+
+=head3 PERL API
+
+ $data = $model->definition();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/definition
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub definition {
-
 	my $self = shift;
     my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my $data_pack;
-
-	#### data pull and packaging
-
-	$data_pack = $object->Definition;
-
-	####
-	
-	$data{'data'} = $data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-
-
+	my $data_pack = $object->Definition; 
+	return {
+		'data'=> $data_pack,
+		'description' => 'term definition'
+	};
 }
 
+=head2 type
 
-sub type {
+This method will return a data structure with the type of this GO_Term.
 
+=head3 PERL API
+
+ $data = $model->type();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/type
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
+
+sub type {	
 	my $self = shift;
     my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my $data_pack;
-
-	#### data pull and packaging
-
-	$data_pack = $object->Type;
+	my $data_pack = $object->Type;
 	$data_pack =~ s/\_/\ /;
-
-	####
-	
-	$data{'data'} = $data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-
+	return {
+		'data'=> $data_pack,
+		'description' => 'type for this term'
+	};
 }
 
-sub details {
+=head2 genes
 
-  my $self = shift;
-  my $term = $self->object;
-  my %data;
-  my $desc = 'notes';
-  my %data_pack;
+This method will return a data structure with genes annotated to this GO_Term.
 
-  #### data pull and packaging
-	
-	%data_pack = (
-					'acedb_id' => $term,
-					'term' => $term->Term,
-					'definition' =>$term->Definition,
-					'type' => $term->Type
-					);
-  ####
+=head3 PERL API
 
-  $data{'data'} = \%data_pack;
-  $data{'description'} = $desc;
-  return \%data;
-}
+ $data = $model->genes();
 
+=head3 REST API
 
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/genes
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub genes {
-
-  my $self = shift;
-  my $object = $self->object;
-  my %data;
-  my $desc = 'notes';
-  my @data_pack; ## %data_pack
-
-  #### data pull and packaging
+  	my $self = shift;
+    my $object = $self->object;
+    my @data_pack;
+	my @genes = eval{$object->Genes};
 	
-	my @genes;
-	eval{@genes = $object->Gene;};
-
 	if (@genes) {
-	
 		foreach my $gene (@genes) {
 		
-			my ($evidence_code, $evidence_details) = get_GO_evidence($object,$gene);
-			my $gene_name = $gene->Public_name;
+			my ($evidence_code, $evidence_details) = $self->_get_GO_evidence($object,$gene);
+			my $gene_info = $self->_pack_object($gene);
 			my $gene_data = {
-								'gene' => {
-									'ace_id' => "$gene",
-									'common_name' => "$gene_name",
-									'class' => 'Gene'
-								},
-								'evidence_code' => $evidence_code,
-								'evidence_details' => $evidence_details
-								};
+				'gene' => $gene_info,
+				'evidence_code' => $evidence_code,
+				'evidence_details' => $evidence_details
+				};
 			push @data_pack, $gene_data;				
 		}
 	}
- 
-	
-	#%data_pack = basic_package(\@genes);
-
-  ####
-
-  $data{'data'} = \@data_pack;
-  $data{'description'} = $desc;
-  return \%data;
+	return {
+		'data'=> \@data_pack,
+		'description' => 'genes annotated with this term'
+	};  
 }
+
+=head2 cds
+
+This method will return a data structure cds annotated with this GO_Term.
+
+=head3 PERL API
+
+ $data = $model->cds();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/cds
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub cds {
-
-  my $self = shift;
-  my $object = $self->object;
-  my %data;
-  my $desc = 'notes';
-  my @data_pack;
-
-  #### data pull and packaging
+  	my $self = shift;
+    my $object = $self->object;
+    my @data_pack;
+	my @genes = eval{$object->CDS};
 	
-	my @genes = $object->CDS;
-	
-	foreach my $gene (@genes) {
-	
-			my ($evidence_code, $evidence_details) = get_GO_evidence($object,$gene);		
-			my $cds_data = {
-								'cds_info' => {
-									'id' => "$gene",
-									'label' => public_name($gene,'CDS'),
-									'class' => 'CDS'
-								},
-							
-								'evidence_code' => $evidence_code,
-								'evidence_details' => $evidence_details
-							};
-	}
-	
-
-  ####
-
-  $data{'data'} = \@data_pack;
-  $data{'description'} = $desc;
-  return \%data;
-  
+	if (@genes) {
+		foreach my $gene (@genes) {
+		
+			my ($evidence_code, $evidence_details) = $self->_get_GO_evidence($object,$gene);
+			my $gene_info = $self->_pack_object($gene);
+			my $gene_data = {
+				'gene' => $gene_info,
+				'evidence_code' => $evidence_code,
+				'evidence_details' => $evidence_details
+				};
+			push @data_pack, $gene_data;				
+		}
+	}	
+	return {
+		'data'=> \@data_pack,
+		'description' => 'CDS annotated with this term'
+	};  
 }
 
+=head2 genes_n_cds
+
+This method will return a data structure with genes and cds annotated with this GO_Term.
+
+=head3 PERL API
+
+ $data = $model->genes_n_cds();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/genes_n_cds
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
+
 sub genes_n_cds {
-
-  my $self = shift;
-  my $term = $self->object;
-  my %data;
-  my %mol;
-  my %cgc;
-  my $desc = 'notes';
-  my @data_pack;
+  	my $self = shift;
+  	my $term = $self->object;
+  	my %mol;
+  	my %cgc;
+  	my @data_pack;
 	my $DB = $self->ace_dsn();
-
-  #### data pull and packaging
+	my @objs;
 	
-my @objs;# = $term->$tag if $tag;
+  	push (@objs,$term->Gene,$term->CDS) unless @objs;
 
-  push (@objs,$term->Gene,$term->CDS) unless @objs;
+  	foreach my $obj (@objs) {
+    	my ($gene,$key);
+	  	if ($obj->class eq 'CDS') {
+	    	$gene = $obj->Gene;
+	      	$key  = $obj;
+	  	} else {
+	      	$gene = $obj;
+	      	$key  = $gene;
+	  	}
+      	next unless $gene;
+      	next if (defined $cgc{$key} || defined $mol{$key});
 
-  foreach my $obj (@objs) {
-      my ($gene,$key);
-      # We need to key searches by CDS in order to display them all
-      # The main display creates a hybrid list of genes and CDSs.
+     	if ($gene->CGC_name) {
+	  		$cgc{$key} = [$obj,$gene,$gene->CGC_name];
+      	} else {
+	  		$mol{$key} = [$obj,$gene,$gene->Sequence_name];
+      	}
+  	}
 
-	  if ($obj->class eq 'CDS') {
-	      $gene = $obj->Gene;
-	      $key  = $obj;
-	  } else {
-	      $gene = $obj;
-	      $key  = $gene;
-	  }
+  	my @sorted = keys %cgc;
+  	push @sorted, keys %mol;
 
-      # Ignore CDSs without associated genes
-      next unless $gene;
-      next if (defined $cgc{$key} || defined $mol{$key});
-
-      if ($gene->CGC_name) {
-	  $cgc{$key} = [$obj,$gene,$gene->CGC_name];
-      } else {
-	  $mol{$key} = [$obj,$gene,$gene->Sequence_name];
-      }
-  }
-
-  my @sorted = sort {$cgc{$a}->[2] cmp $cgc{$b}->[2] } keys %cgc;
-  push @sorted,sort {$mol{$a}->[2] cmp $mol{$b}->[2] } keys %mol;
-
-  my @genes;
-  foreach (@sorted) {
-      my ($obj,$gene,$junk) = eval { @{$cgc{$_}} };
-      ($obj,$gene,$junk) = eval { @{$mol{$_}} } unless $gene;
+  	my @genes;
+  
+  	foreach (@sorted) {
+    	my ($obj,$gene,$junk) = eval { @{$cgc{$_}} };
+      	($obj,$gene,$junk) = eval { @{$mol{$_}} } unless $gene;
       # UGH!  Return a list of CDSs instead.
-	  push @genes,$obj;
-	  push @genes,$DB->fetch(Gene=>$gene);
-	  }
+	  	push @genes,$obj;
+	  	push @genes,$DB->fetch(Gene=>$gene);
+	}
 	
 	foreach my $gene (@genes) {
 	
 			my $cgc_name = $gene->CGC_name;
 			my $seq = $gene->Sequence_name;
-			my $desc = $gene->Concise_description || $gene->Provisional_description;
-			
-					
-			my ($evidence_code, $evidence_details) = get_GO_evidence($term,$gene);
+			my $desc = $gene->Concise_description || $gene->Provisional_description;		
+			my ($evidence_code, $evidence_details) = $self->_get_GO_evidence($term,$gene);
+			my $gene_info = $self->_pack_obj($gene);
 			
 			my $gene_data = {
-								'gene_info' => {
-									
-									'id' => "$gene",
-									'label' => public_name($gene,'Gene'),
-									'class' => 'Gene'
-								},
-								
+								'gene_info' => $gene_info,
 								'cgc_name' => $cgc_name,
 								'seq' => $seq,
 								'description' => $desc,
@@ -284,134 +454,227 @@ my @objs;# = $term->$tag if $tag;
 							};
 			push @data_pack, $gene_data;		
 	}
-	
-  ####
-
-  $data{'data'} = \@data_pack;
-  $data{'description'} = $desc;
-  return \%data;
+	return {
+		'data'=> \@data_pack,
+		'description' => 'genes and cds annoted with this term'
+	};
 }
 
+=head2 phenotype
+
+This method will return a data structure phenotypes annotated with this GO_Term.
+
+=head3 PERL API
+
+ $data = $model->phenotype();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/phenotype
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub phenotype {
-
-  my $self = shift;
-  my $term = $self->object;
-  my %data;
-  my $desc = 'notes';
-  my @data_pack;
-
-  #### data pull and packaging
-	my @phenotypes;
-	eval {@phenotypes = $term->Phenotype;};
-	
+  	my $self = shift;
+  	my $term = $self->object;
+  	my @data_pack;
+	my @phenotypes= $term->Phenotype;
 	foreach my $phenotype (@phenotypes) {
-	
-		my $phenotype_term = $phenotype->Name->right;
 		my $phenotype_desc = $phenotype->Description;
-		
-		
-		
-		my ($evidence_code, $evidence_details) = get_GO_evidence($term,$phenotype);
-		
-		
+		my ($evidence_code, $evidence_details) = $self->_get_GO_evidence($term,$phenotype);
+		my $phenotype_info = $self->_pack_obj($phenotype);
+
 		my $pheno_data = {
-							'phene_info' => {
-								'id' => "$phenotype",
-								'label' => "$phenotype_term",
-								'class' => 'Phenotype'
-							},
+							'phenotype_info' => $phenotype_info,
 							'description' => $phenotype_desc,
 							'evidence_code' => $evidence_code,
 							'evidence_details' => $evidence_details									
 						 };
 		push @data_pack, $pheno_data;			 
 	}
-	
- ####
-
-  $data{'data'} = \%data_pack;
-  $data{'description'} = $desc;
-  return \%data;
+	return {
+		'data'=> \@data_pack,
+		'description' => 'phenotypes annotated with this term'
+	};
 }
- 
- 
+
+=head2 motif
+
+This method will return a data structure motifs annotated with this GO_Term.
+
+=head3 PERL API
+
+ $data = $model->motif();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/motif
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
+
 sub motif {
+	my $self = shift;
+  	my $term = $self->object;
+  	my @data_pack;
 
-  my $self = shift;
-  my $term = $self->object;
-  my %data;
-  my $desc = 'notes';
-  my @data_pack;
-
-  #### data pull and packaging
 	my @motifs;
 	eval {@motifs = $term->Motif;};
 	
 	foreach my $motif (@motifs) {
-	
 		my $motif_desc = $motif->Description;
 		my ($evidence_code, $evidence_details) = get_GO_evidence($term,$motif);
-		
+		my $motif_info = $self->_pack_obj($motif);
 		my $motif_data = {
-								'motif_info' => {
-									'id' => "$motif",
-									'label' => "$motif",
-									'class' => 'Motif'
-								},
-								
-								'description' => $motif_desc,
-								'evidence_code' => $evidence_code,
-								'evidence_details' => $evidence_details
-								};
-								
+			'motif_info' => $motif_info,
+			'description' => $motif_desc,
+			'evidence_code' => $evidence_code,
+			'evidence_details' => $evidence_details
+		};						
 		push @data_pack, $motif_data;
 	}
-	
-
- ####
-
-  $data{'data'} = \@data_pack;
-  $data{'description'} = $desc;
-  return \%data;
+	return {
+		'data'=> \@data_pack,
+		'description' => 'motifs annotated with this term'
+	};	
 }
-  
- sub get_tag_data {
 
-  my $self = shift;
-  my $tag = shift;
-  my $term = $self->object;
-  my %data;
-  my $desc = 'notes';
-  my %data_pack;
+=head2 get_tag_data
 
-  #### data pull and packaging
+This method will return a data structure with the tag_object annoted with this GO_Term.
+
+=head3 PERL API
+
+ $data = $model->get_tag_data();
+
+=head3 REST API
+
+=head4 Request Method
+
+GET
+
+=head4 Requires Authentication
+
+No
+
+=head4 Parameters
+
+a GO_Term ID GO:0016311
+
+=head4 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head4 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/gene/GO:0016311/get_tag_data
+
+=head4 Response example
+
+<div class="response-example"></div>
+
+=cut
+
+sub get_tag_data {
+	my $self = shift;
+  	my $tag = shift;
+  	my $term = $self->object;
+  	my @data_pack;
 	my @motifs;
-	my @tag_data;
-	eval {@tag_data = $term->$tag;};
+	my @tag_data = $term->$tag;
 	
 	foreach my $tag_datum (@tag_data) {
-	
 		my $tag_datum_desc = $tag_datum->Description;
 		my ($evidence_code, $evidence_details) = get_evidence($term,$tag_datum);
-		$data_pack{$tag_datum} = {
-								'term' =>$tag_datum,
-								'description' => $tag_datum_desc,
-								'class' => $tag,
-								'evidence_code' => $evidence_code,
-								'evidence_details' => $evidence_details
-								};
+		
+		push @data_pack, {
+			'term' =>"$tag_datum",
+			'description' => "$tag_datum_desc",
+			'class' => $tag,
+			'evidence_code' => $evidence_code,
+			'evidence_details' => $evidence_details
+		};
 	}
-	
-
- ####
-
-  $data{'data'} = \%data_pack;
-  $data{'description'} = $desc;
-  return \%data;
+	return {
+		'data'=> \@data_pack,
+		'description' => 'Objects annotated with this term'
+	};
 }
   
-sub get_GO_evidence {
+sub _get_GO_evidence {
 
 	my ($term,$gene) = @_;
 	my @go_terms;
@@ -429,62 +692,6 @@ sub get_GO_evidence {
 		}
 	}
 	return ($evidence_code, $evidence_detail);
-}
-
-
-
-### copied and pasted, need to get to work in Object.pm
-
-
-sub basic_package {
-
-	my ($self,$data_ar) = @_;
-	my %package;
-	
-	foreach my $object (@$data_ar) {
-				
-				
-				my $class;
-				eval{$class = $object->class;};
-
-				my $common_name = public_name($object,$class);  ## 
-				$package{$object} = 	{
-										'class' => $class,
-										'common_name' => $common_name
-										}	
-	}
-	return \%package;
-}
-
-
-# Probably unnecessary
-sub public_name {
-    
-	my ($object,$class) = @_;
-    my $common_name;    
-   
-    if ($class =~ /gene/i) {
-		$common_name = 
-		$object->Public_name
-		|| $object->CGC_name
-		|| $object->Molecular_name
-		|| eval { $object->Corresponding_CDS->Corresponding_protein}
-		|| $object;
-    }
-    elsif ($class =~ /protein/i) {
-    	$common_name = 
-    	$object->Gene_name
-    	|| eval { $object->Corresponding_CDS->Corresponding_protein}
-    	||$object;
-    }
-    else {
-    	$common_name = $object;
-    }
-	
-	my $data = $common_name;
-    return $data;
-
-
 }
 
 1;
