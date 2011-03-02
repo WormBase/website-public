c1e93a8eab796c416f190623b0babb60db1e95f5
norie <noriedlc3@gmail.com>
2011-03-01 23:45:36 -0500

    tested -- homology_group model

--- lib/WormBase/API/Object/Homology_group.pm
+++ lib/WormBase/API/Object/Homology_group.pm
@@ -1,56 +1,32 @@
-package WormBase::API::Object::<object>;
+package WormBase::API::Object::Homology_group;
 use Moose;
 
 with 'WormBase::API::Role::Object';
 extends 'WormBase::API::Object';
 
-=pod 
+=head2 title
 
-=head1 NAME
+This method will return a data structure the title for the Homology_group.
 
-
-WormBase::API::Object::Homology_group
-
-=head1 SYNPOSIS
-
-Model for the Ace ?Homology_group class.
-
-=head1 URL
-
-http://wormbase.org/species/homology_group
-
-=head1 METHODS/URIs
-
-=cut
-
-#######################################
-#
-# The Overview Widget
-#
-#######################################
-
-=head3 title
-
-This method will return a data structure with this Homology_groups title
-=head4 PERL API
+=head3 PERL API
 
  $data = $model->title();
 
-=head4 REST API
+=head3 REST API
 
-=head5 Request Method
+=head4 Request Method
 
 GET
 
-=head5 Requires Authentication
+=head4 Requires Authentication
 
 No
 
-=head5 Parameters
+=head4 Parameters
 
-a Homology_group ID InP_Cae_006282
+a Homology_group ID InP_Cae_000935
 
-=head5 Returns
+=head4 Returns
 
 =over 4
 
@@ -64,16 +40,15 @@ a Homology_group ID InP_Cae_006282
 
 =back
 
-=head5 Request example
+=head4 Request example
 
-curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_006282/title
+curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_000935/title
 
-=head5 Response example
+=head4 Response example
 
 <div class="response-example"></div>
 
 =cut
-
 
 sub title {
 	my $self = shift;
@@ -86,34 +61,29 @@ sub title {
 	return $data;
 }
 
+=head2 group_type
 
-########################################
-#<todo> sub db  ### for external links?
-#########################################
+This method will return a data structure on the group_type of the Homology_group.
 
-=head3 group_type
-
-This method will return a data structure with info on the group type.
-
-=head4 PERL API
+=head3 PERL API
 
  $data = $model->group_type();
 
-=head4 REST API
+=head3 REST API
 
-=head5 Request Method
+=head4 Request Method
 
 GET
 
-=head5 Requires Authentication
+=head4 Requires Authentication
 
 No
 
-=head5 Parameters
+=head4 Parameters
 
-a Homology_group ID InP_Cae_006282
+a Homology_group ID InP_Cae_000935
 
-=head5 Returns
+=head4 Returns
 
 =over 4
 
@@ -127,11 +97,11 @@ a Homology_group ID InP_Cae_006282
 
 =back
 
-=head5 Request example
+=head4 Request example
 
-curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_006282/group_type
+curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_000935/group_type
 
-=head5 Response example
+=head4 Response example
 
 <div class="response-example"></div>
 
@@ -147,41 +117,39 @@ sub group_type {
 	if ($homology_group =~ /COG/) {
 		$homology_code = $object->COG_code;
 	}
-	my data_pack = {
+	my $data_pack = {
 		'homology_group' => "$homology_group",
 		'cog_code' => $homology_code
 	};
-	my $data = {
+	return {
 		'data'=> $data_pack,
 		'description' => 'type of homology_group'
 		};
-	return $data;
 }
 
+=head2 go_term
 
-=head3 go_term
+This method will return a data structure with the go terms for the Homology_group.
 
-This method will return a data structure for go terms related to this homology group
-
-=head4 PERL API
+=head3 PERL API
 
  $data = $model->go_term();
 
-=head4 REST API
+=head3 REST API
 
-=head5 Request Method
+=head4 Request Method
 
 GET
 
-=head5 Requires Authentication
+=head4 Requires Authentication
 
 No
 
-=head5 Parameters
+=head4 Parameters
 
-a Homology_group ID InP_Cae_006282
+a Homology_group ID InP_Cae_000935
 
-=head5 Returns
+=head4 Returns
 
 =over 4
 
@@ -195,11 +163,11 @@ a Homology_group ID InP_Cae_006282
 
 =back
 
-=head5 Request example
+=head4 Request example
 
-curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_006282/go_term
+curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_000935/go_term
 
-=head5 Response example
+=head4 Response example
 
 <div class="response-example"></div>
 
@@ -215,38 +183,35 @@ sub go_term {
 		my $gt_info = _pack_obj($go_term);
 		push @data_pack, $gt_info;
 	}
-	
-	my $data = {
-				'data'=> \@data_pack,
-				'description' => 'go terms related to this homology group'
-				};
-	return $data;
+	return {
+		'data'=> \@data_pack,
+		'description' => 'go terms related to this homology group'
+	};
 }
 
+=head2 protein
 
-=head3 protein
+This method will return a data structure with the proteins in the Homology_group.
 
-This method will return a data structure containing information for proteins related to this homology_group.
-
-=head4 PERL API
+=head3 PERL API
 
  $data = $model->protein();
 
-=head4 REST API
+=head3 REST API
 
-=head5 Request Method
+=head4 Request Method
 
 GET
 
-=head5 Requires Authentication
+=head4 Requires Authentication
 
 No
 
-=head5 Parameters
+=head4 Parameters
 
-a Homology_group ID InP_Cae_006282
+a Homology_group ID InP_Cae_000935
 
-=head5 Returns
+=head4 Returns
 
 =over 4
 
@@ -260,11 +225,11 @@ a Homology_group ID InP_Cae_006282
 
 =back
 
-=head5 Request example
+=head4 Request example
 
-curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_006282/protein
+curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_000935/protein
 
-=head5 Response example
+=head4 Response example
 
 <div class="response-example"></div>
 
@@ -277,162 +242,20 @@ sub protein {
 	my @tag_objects = $object->Protein;
 
 	foreach my $tag_object (@tag_objects) {
-		my $tag_info = _pack_obj($tag_object);
-		push, @data_pack, $tag_info;
+		my $tag_info = $self->_pack_obj($tag_object);
+		push @data_pack, $tag_info;
 	}
 	
-	my $data = {
-				'data'=> \@data_pack,
-				'description' => 'proteins related to this homology_group'
-				};
-	return $data;
+	return  {
+		'data'=> \@data_pack,
+		'description' => 'proteins related to this homology_group'
+	};
 }
-
-
-=head3 remarks
-
-This method will return a data structure with remarks re: this homology_group
-
-=head4 PERL API
-
- $data = $model->remarks();
-
-=head4 REST API
-
-=head5 Request Method
-
-GET
-
-=head5 Requires Authentication
-
-No
-
-=head5 Parameters
-
-a Homology_group ID InP_Cae_006282
-
-=head5 Returns
-
-=over 4
-
-=item *
-
-200 OK and JSON, HTML, or XML
-
-=item *
-
-404 Not Found
-
-=back
-
-=head5 Request example
-
-curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_006282/remarks
-
-=head5 Response example
-
-<div class="response-example"></div>
-
-=cut
-
-
-
-=head3 name
-
-This method will return a data structure of the 
-name and ID of the requested transgene.
-
-=head4 PERL API
-
- $data = $model->name();
-
-=head4 REST API
-
-=head5 Request Method
-
-GET
-
-=head5 Requires Authentication
-
-No
-
-=head5 Parameters
-
-a Transgene ID (gmIs13)
-
-=head5 Returns
-
-=over 4
-
-=item *
-
-200 OK and JSON, HTML, or XML
-
-=item *
-
-404 Not Found
-
-=back
-
-=head5 Request example
-
-curl -H content-type:application/json http://api.wormbase.org/rest/field/transgene/gmIs13/name
-
-=head5 Response example
-
-<div class="response-example"></div>
-
-=cut 
 
 # sub name { }
 # Supplied by Role; POD will automatically be inserted here.
 # << include name >>
 
-=head3 remarks
-
-This method will return a data structure with remarks re: this homology_group
-
-=head4 PERL API
-
- $data = $model->remarks();
-
-=head4 REST API
-
-=head5 Request Method
-
-GET
-
-=head5 Requires Authentication
-
-No
-
-=head5 Parameters
-
-a Homology_group ID InP_Cae_006282
-
-=head5 Returns
-
-=over 4
-
-=item *
-
-200 OK and JSON, HTML, or XML
-
-=item *
-
-404 Not Found
-
-=back
-
-=head5 Request example
-
-curl -H content-type:application/json http://api.wormbase.org/rest/field/homology_group/InP_Cae_006282/remarks
-
-=head5 Response example
-
-<div class="response-example"></div>
-
-=cut
 # sub remarks {}
 # Supplied by Role; POD will automatically be inserted here.
 # << include remarks >>
