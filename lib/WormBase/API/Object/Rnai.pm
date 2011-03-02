85f5192690f2538a40feadcf1c263243a0c02055
norie <noriedlc3@gmail.com>
2011-03-01 23:48:31 -0500

    on going conversion -- rnai model

--- lib/WormBase/API/Object/Rnai.pm
+++ lib/WormBase/API/Object/Rnai.pm
@@ -36,10 +36,6 @@ http://wormbase.org/species/rnai
 # Supplied by Role; POD will automatically be inserted here.
 # << include name >>
 
-# sub laboratory { }
-# Supplied by Role; POD will automatically be inserted here.
-# << include laboratory >>
-
 # sub taxonomy { }
 # Supplied by Role; POD will automatically be inserted here.
 # << include taxonomy >>
@@ -47,14 +43,11 @@ http://wormbase.org/species/rnai
 sub targets {
 	my ($self) = @_;
 	my %data;
-
 	my $targets_hr = _classify_targets($self->object);
-
 	foreach my $target_type ('Primary targets','Secondary targets') {
-		my $genes = eval { $targets_hr->{$target_type}};
+		my $genes = eval {$targets_hr->{$target_type}};
 		$data{$target_type} = $genes; # are the key,value pair important? otherwise omit...
   	}
-
 	return {
 		description => 'notes',
 		data => %data || undef,
@@ -89,88 +82,26 @@ sub identification {
 	};
 }
 
-## development notes
+
+
+
 
 sub history_name {
 	my ($self) = @_;
-
 	return {
 		description => 'notes',
 		data => $self ~~ 'History_name' || $self->object,
 	};
 }
 
-################
-## SOURCE
-################
+
+# sub laboratory { }
+# Supplied by Role; POD will automatically be inserted here.
+# << include laboratory >>
 
 # sub remarks {}
 # Supplied by Role; POD will automatically be inserted here.
 # << include remarks >>
-
-
-sub laboratory_details { # TODO ???
-	my ($self) = @_;
-    my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my %data_pack;
-
-	#### data pull and packaging
-
-	#	my $exp = shift;
-	#  	my ($experiment_link,$experiment_url,$link_location);
-	#  	# experiment URL also used to construct links to movies
-	#  	my $history_name = $exp->History_name;
-	#  	if ($exp->Laboratory eq 'KK' || $exp->Laboratory eq 'PF') {
-	#    
-	#    	($experiment_link = $history_name) =~ s/^KK://;
-	#    	$experiment_url = Configuration->Rnaidb.$experiment_link;
-	#    	$experiment_link = a({-href=>$experiment_url},$history_name);
-	#    	$link_location = a({-href=>'http://www.rnai.org/'},'RNAiDB');
-	#  	} elsif ($exp->Laboratory eq 'TH'){
-	#    
-	#    	# Links to PhenoBank are stored as remarks
-	#    	my @remark = map {escapeHTML($_)} $exp->Remark;
-	#    	($experiment_link) = "[" 
-	#      	. join('; ',map { a({-href=>$_},/GeneID=(.*)&/) } grep { /phenobank/ } @remark) . ']';
-	#    	$link_location = a({-href=>Configuration->Phenobank},'PhenoBank');
-	#  	}
-	#
-	#  	my @remark = map {escapeHTML($_)} $exp->Remark;
-	#  	@remark = grep { !/phenobank/ } @remark;
-	#
-	#  	SubSection('Laboratory',map { ObjectLink($_) } $exp->Laboratory);
-	#
-	#  	SubSection("Further details available at $link_location",$experiment_link);
-	#
-	#  	if (my @refs = $exp->Reference) {
-	#    
-	#    	SubSection('Reference',format_references(-references=>\@refs,-format=>'long',-pubmed_link=>'image',
-	#				     -suppress_years=>1));
-	#  	} else {
-	#  	
-	#    	my $author = join(' ',map { ObjectLink($_) } $exp->Author);
-	#    
-	#    	SubSection('Authors',$author);
-	#    	my $date = $exp->Date;
-	#    	$date =~ s/ 00:00:00$//;
-	#    
-	#    	SubSection('Publication date',$date);
-	#  }
-	#  
-	#  SubSection('Remarks',@remark);
-
-	####
-
-	$data{'data'} = \%data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-}
-
-################
-## EXPERIMENTAL CONDITIONS
-################
 
 
 sub genotype {
@@ -227,53 +158,10 @@ sub delivered_by {
 	};
 }
 
-#sub experimental_conditions {
-#
-#	my ($self) = @_;
-#    my $exp = $self->object;
-#	my %data;
-#	my $desc = 'notes';
-#	my %data_pack;
-#
-#	#### data pull and packaging
-#
-#	my $species = $exp->Species;
-#	my $genotype = $exp->Genotype;
-#	my $strain = $exp->Strain;
-#	my $treatment = $exp->Treatment;
-#	my $life_stage = $exp->Life_stage;
-#	my $delivered_by = $exp->Delivered_by;
-#	
-#	my $interaction = $exp->Interaction;
-#	my $interactor = eval{$interaction->Interactor;};
-#	
-#	%data_pack = (
-#					'species' => $species,
-#					'genotype' => $genotype,
-#					'strain' => $strain,
-#					'treatment' => $treatment,
-#					'life_stage' => $life_stage,
-#					'delivered_by' => $delivered_by,
-#					'interaction' => $interaction,
-#					'interactor' => $interactor
-#					);
-#	####
-#
-#	$data{'data'} = \%data_pack;
-#	$data{'description'} = $desc;
-#	return \%data;
-#}
-
-###############
-## PHENOTYPES
-###############
-
 sub phenotypes {
 	my ($self) = @_;
-
 	my @data = map {$self->_pack_obj($_, scalar $_->Primary_name)}
 	               @{$self ~~ '@Phenotype'};
-
 	return {
 		description => 'notes',
 		data => @data ? \@data : undef,
@@ -283,92 +171,11 @@ sub phenotypes {
 
 sub phenotype_nots {
 	my ($self) = @_;
-
 	my @data = map {$self->_pack_obj($_, scalar $_->Primary_name)}
 	               @{$self ~~ '@Phenotype_not_observed'};
-
 	return {
 		description => 'notes',
 		data => @data ? \@data : undef,
-	};
-}
-
-
-
-###############
-## MOVIES AND IMAGES
-###############
-
-sub movies { # TODO ???
-	my ($self) = @_;
-    my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my %data_pack;
-
-	#### data pull and packaging
-
-	## work with XS to get precise details of data needed for images
-
-	####
-
-	$data{'data'} = \%data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-}
-
-
-###############
-## GENOMIC ENVIRONS
-###############
-
-# See genomic picture
-sub genomic_environs { # TODO ???
-	my ($self) = @_;
-    my $object = $self->object;
-	my %data;
-	my $desc = 'notes';
-	my %data_pack;
-
-	#### data pull and packaging
-
-	## work with XS to get precise details of data needed for images
-
-	####
-
-	$data{'data'} = \%data_pack;
-	$data{'description'} = $desc;
-	return \%data;
-}
-
-
-
-###############
-## NOTES
-###############
-
-# NO NO NO GOD NO. This belongs in the template.
-sub display_notes {
-	my ($self) = @_;
-
-	my $data = 'Primary targets
-
-	Primary targets have sequence identity to the RNAi probe of at least
-	95% over a stretch of at least 100 nucleotides, identified using a
-	combination of BLAST and BLAT algorithms.  These are usually the
-	intended target genes of an RNAi experiment.
-
-	Secondary targets
-
-	Secondary targets have between 80 and 94.99% sequence identity over a
-	stretch of at least 200 nucleotides to the RNAi probe. Targets (and
-	overlapping genes) that satisfy these criteria may or may not be
-	susceptible to an RNAi effect with the given probe and represent
-	secondary (unintended) genomic targets of an RNAi experiment.';
-
-	return {
-		description => 'notes',
-		data => $data,
 	};
 }
 
@@ -380,7 +187,6 @@ sub _classify_targets {
   	my $exp = shift;
   	my %seen;
   	my %categories;
-
 	my @genes = grep { !$seen{$_->Molecular_name}++ } $exp->Gene;
   	push @genes, grep { !$seen{$_}++ } $exp->Predicted_gene;
 
@@ -393,7 +199,6 @@ sub _classify_targets {
 			push @{$categories{$status}},$gene;
     	}
   	}
-
 	return \%categories;
 }
 
