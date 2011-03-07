a4cacd5abd08d60ca97e2e8de7ad1cd7c6d08d2f
norie <noriedlc3@gmail.com>
2011-03-07 17:10:38 -0500

    small tweeks to formatting -- anatomy_term

--- lib/WormBase/API/Object/Anatomy_term.pm
+++ lib/WormBase/API/Object/Anatomy_term.pm
@@ -87,8 +87,8 @@ sub definition {
     my $object = $self->object;
     my $data   = $object->Definition;
     return {
-		data        => $data ? "$data" : undef,
-		description => 'definition of the anatomy term',
+        data => $data ? "$data" : undef,
+        description => 'definition of the anatomy term',
     };
 }
 
@@ -143,21 +143,22 @@ sub synonyms {
     my $self     = shift;
     my $object   = $self->object;
     my @synonyms = $object->Synonym;
-    
-    foreach my $entry (@synonyms) {
-	my $synonym = $entry->Primary_name->right if $entry->Primary_name;
-	my $tag_info = $self->_pack_obj($synonym);
-	push @data,$self->_pack_obj($synonym);
-    }
-    return { description => 'synonyms that have been used to describe the anatomy term',
-	     data        => @data ? \@data : undef };
-}
 
+    foreach my $entry (@synonyms) {
+        my $synonym = $entry->Primary_name->right if $entry->Primary_name;
+        my $tag_info = $self->_pack_obj($synonym);
+        push @data, $self->_pack_obj($synonym);
+    }
+    return {
+        description =>
+          'synonyms that have been used to describe the anatomy term',
+        data => @data ? \@data : undef
+    };
+}
 
 # sub remarks {}
 # Supplied by Role; POD will automatically be inserted here.
 # << include remarks >>
-
 
 =head3 transgenes
 
@@ -207,17 +208,20 @@ curl -H content-type:application/json http://api.wormbase.org/rest/field/anatomy
 =cut
 
 sub transgenes {
-    my $self = shift;
+    my $self   = shift;
     my $object = $self->object;
     my @transgenes;
-    eval{@transgenes = map{$_->Transgene} grep {/marker/i&& defined $_->Transgene} $term->Expr_pattern;};
-    my @data_pack = map {$_ = $self->_pack_obj($_)} @transgenes;
-	return {
-		'data'=> \@data_pack,
-		'description' => 'transgenes annotated with this anatomy_term'
-		};	
+    eval {
+        @transgenes =
+          map { $_->Transgene }
+          grep { /marker/i && defined $_->Transgene } $term->Expr_pattern;
+    };
+    my @data_pack = map { $_ = $self->_pack_obj($_) } @transgenes;
+    return {
+        'data'        => \@data_pack,
+        'description' => 'transgenes annotated with this anatomy_term'
+    };
 }
-
 
 #####################
 ## browser
@@ -283,38 +287,38 @@ B<Response example>
 
 =cut 
 
+sub expr_patterns {
+    my $self          = shift;
+    my $object        = $self->object;
+    my @expr_patterns = $object->Expr_pattern;
 
-sub expr_patterns{
-	my $self = shift;
-    my $object = $self->object;
-	my @expr_patterns = $object->Expr_pattern;
-	
-	foreach my $expr_pattern (@expr_patterns) {
-		my $ep_data = {
-			'id' =>"$expr_pattern",
-			'label' =>"$expr_pattern",
-			'Class' => 'Expr_pattern'				
-		};
-		
-		my $ep_gene = $expr_pattern->Gene if $expr_pattern->Gene;
-		my $ep_pattern = $expr_pattern->Pattern if $expr_pattern->Pattern;
-		my $ep_xgene = $expr_pattern->Transgene if $expr_pattern->Transgene;
-		
-		my $gene_data = $self->_pack_obj($ep_gene) if $ep_gene;
-		my $ep_pattern_data = $self->_pack_obj($ep_pattern) if $ep_pattern;
-		my $ep_transgene_data = $self->_pack_obj($ep_xgene) if $ep_xgene;
-		
-		push @data_pack, {
-			'ep_data' => $ep_data,
-			'gene' => $gene_data,
-			'pattern' => ep_pattern_data,
-			'trans_gene' => $ep_transgene_data		
-			};
-	}
-	return {
-		'data'=> \@data_pack,
-		'description' => 'expr_patterns annotated with this anatomy_term'
-		};
+    foreach my $expr_pattern (@expr_patterns) {
+        my $ep_data = {
+            'id'    => "$expr_pattern",
+            'label' => "$expr_pattern",
+            'Class' => 'Expr_pattern'
+        };
+
+        my $ep_gene    = $expr_pattern->Gene      if $expr_pattern->Gene;
+        my $ep_pattern = $expr_pattern->Pattern   if $expr_pattern->Pattern;
+        my $ep_xgene   = $expr_pattern->Transgene if $expr_pattern->Transgene;
+
+        my $gene_data         = $self->_pack_obj($ep_gene)    if $ep_gene;
+        my $ep_pattern_data   = $self->_pack_obj($ep_pattern) if $ep_pattern;
+        my $ep_transgene_data = $self->_pack_obj($ep_xgene)   if $ep_xgene;
+
+        push @data_pack,
+          {
+            'ep_data'    => $ep_data,
+            'gene'       => $gene_data,
+            'pattern'    => ep_pattern_data,
+            'trans_gene' => $ep_transgene_data
+          };
+    }
+    return {
+        'data'        => \@data_pack,
+        'description' => 'expr_patterns annotated with this anatomy_term'
+    };
 }
 
 =head3 go_terms
@@ -365,28 +369,29 @@ curl -H content-type:application/json http://api.wormbase.org/rest/field/anatomy
 
 =cut 
 
-sub go_terms{
-	my $self = shift;
-    my $object = $self->object;
-	my @go_terms = $object->GO_term;
-	
-	foreach my $go_term (@go_terms) {
-		my $term = $go_term->Term;
-		my $ao_code = $go_term->right;
-		my $gt_data = {
-			'id' =>"$go_term",
-			'label' =>"$term",
-			'Class' => 'GO_term'			
-		};
-		push @data_pack, {
-				'term' => $gt_data,
-				'ao_code' => "$ao_code"
-			};
-	}
-	return {
-		'data'=> \@data_pack,
-		'description' => 'go_terms associated with this anatomy_term'
-		};
+sub go_terms {
+    my $self     = shift;
+    my $object   = $self->object;
+    my @go_terms = $object->GO_term;
+
+    foreach my $go_term (@go_terms) {
+        my $term    = $go_term->Term;
+        my $ao_code = $go_term->right;
+        my $gt_data = {
+            'id'    => "$go_term",
+            'label' => "$term",
+            'Class' => 'GO_term'
+        };
+        push @data_pack,
+          {
+            'term'    => $gt_data,
+            'ao_code' => "$ao_code"
+          };
+    }
+    return {
+        'data'        => \@data_pack,
+        'description' => 'go_terms associated with this anatomy_term'
+    };
 }
 
 =head3 anatomy_functions
@@ -439,13 +444,13 @@ B<Response example>
 =cut 
 
 sub anatomy_functions {
-	my $self = shift;
-    my $object = $self->object;
-	my $data_pack = $self->_anatomy_function('Anatomy_function');
-	return {
-		'data'=> $data_pack,
-		'description' => 'anatomy_functions associatated with this anatomy_term'
-		};
+    my $self      = shift;
+    my $object    = $self->object;
+    my $data_pack = $self->_anatomy_function('Anatomy_function');
+    return {
+        'data'        => $data_pack,
+        'description' => 'anatomy_functions associatated with this anatomy_term'
+    };
 }
 
 =head3 anatomy_function_nots
@@ -499,13 +504,13 @@ B<Response example>
 =cut 
 
 sub anatomy_function_nots {
-	my $self = shift;
-    my $object = $self->object;
-	my $data_pack = $self->_anatomy_function('Anatomy_function_not');
-	return {
-		'data'=> $data_pack,
-		'description' => 'anatomy_functions associatated with this anatomy_term'
-		};	
+    my $self      = shift;
+    my $object    = $self->object;
+    my $data_pack = $self->_anatomy_function('Anatomy_function_not');
+    return {
+        'data'        => $data_pack,
+        'description' => 'anatomy_functions associatated with this anatomy_term'
+    };
 }
 
 =head3 expression_clusters
@@ -559,26 +564,26 @@ B<Response example>
 =cut 
 
 sub expression_clusters {
-	my $self = shift;
+    my $self   = shift;
     my $object = $self->object;
-	my $desc = 'notes';
-	my @data_pack;
-	my @expression_clusters = $object->Expression_cluster;
-	foreach my $expression_cluster (@expression_clusters) {
-		my $ec_description = $expression_cluster->Description;
-		my $ec_data = $self->_pack_obj($expression_cluster);
-		
-		push @data_pack, {
-			'ec_data' => $ec_data,
-			'description' => $ec_description
-		};
-	}
-	return {
-		'data'=> \@data_pack,
-		'description' => 'expression_clusters associated with this anatomy_term'
-	};
-}
+    my $desc   = 'notes';
+    my @data_pack;
+    my @expression_clusters = $object->Expression_cluster;
+    foreach my $expression_cluster (@expression_clusters) {
+        my $ec_description = $expression_cluster->Description;
+        my $ec_data        = $self->_pack_obj($expression_cluster);
 
+        push @data_pack,
+          {
+            'ec_data'     => $ec_data,
+            'description' => $ec_description
+          };
+    }
+    return {
+        'data'        => \@data_pack,
+        'description' => 'expression_clusters associated with this anatomy_term'
+    };
+}
 
 #######################################
 #
@@ -602,46 +607,39 @@ sub expression_clusters {
 
 ## sub worm_atlas {} put under external resources
 
-
 #######################################
 #
 # Internal Methods
 #
 #######################################
 
-
 sub _anatomy_function {
-	my $self = shift;
-	my $tag = shift
-    my $object = $self->object;
-	my @anatomy_functions = $object->$tag;
-	
-	foreach my $anatomy_function (@anatomy_functions) {
-		my $phenotype = $anatomy_function->Phenotype;
-		my $phenotype_name = $phenotype->Primary_name;
-		my $gene_data = $self->_pack_obj($af->Gene) if $af->Gene;
+    my $self              = shift;
+    my $tag               = shift my $object = $self->object;
+    my @anatomy_functions = $object->$tag;
 
-		push @data_pack, {
-			'af_data' => {
-				'id' =>"$anatomy_function",
-				'label' =>"$anatomy_function",
-				'Class' => 'Anatomy_function'			
-			},						
-			'phenotype' => {
-				'id' => $phenotype,
-				'label' => $phenotype_name,
-				'class' => 'phenotype'
-			},
-			'gene' => $gene_data
-		};	
-	}
-	return \@data_pack;
+    foreach my $anatomy_function (@anatomy_functions) {
+        my $phenotype      = $anatomy_function->Phenotype;
+        my $phenotype_name = $phenotype->Primary_name;
+        my $gene_data      = $self->_pack_obj( $af->Gene ) if $af->Gene;
+
+        push @data_pack,
+          {
+            'af_data' => {
+                'id'    => "$anatomy_function",
+                'label' => "$anatomy_function",
+                'Class' => 'Anatomy_function'
+            },
+            'phenotype' => {
+                'id'    => $phenotype,
+                'label' => $phenotype_name,
+                'class' => 'phenotype'
+            },
+            'gene' => $gene_data
+          };
+    }
+    return \@data_pack;
 }
 
-
-
-
 1;
-
-
 
