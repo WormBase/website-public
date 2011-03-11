6f1490121ebe2238ab67880c01516aba9d0a0b41
norie <noriedlc3@gmail.com>
2011-03-11 14:48:35 -0500

    updated methods and pods for operon model

--- lib/WormBase/API/Object/Operon.pm
+++ lib/WormBase/API/Object/Operon.pm
@@ -95,14 +95,14 @@ B<Response example>
 =cut 
 
 sub species {
-	my $self = shift;
-    my $object = $self->object;
-	my $tag_object = $object->Species;
-	my $data_pack = $self->_pack_obj($tag_object);
-	return {
-		'data'=> $data_pack,
-		'description' => 'species containing the operon'
-		};
+    my $self       = shift;
+    my $object     = $self->object;
+    my $tag_object = $object->Species;
+    my $data_pack  = $self->_pack_obj($tag_object);
+    return {
+        'data'        => $data_pack,
+        'description' => 'species containing the operon'
+    };
 }
 
 =head3 structure
@@ -156,32 +156,34 @@ B<Response example>
 =cut 
 
 sub structure {
-	my $self = shift;
-	my $operon = $self->object;
-	my @data_pack;
-	my @member_gene = $operon->Contains_gene;
- 
-	foreach my $gene (@member_gene) {
-		my $gene_info = $self->_pack_obj($gene);
-	  	my %spliced_leader;
-	    foreach my $sl ($gene->col) {
-	    	my @evidence = $sl->col;
-			$spliced_leader{$sl} = \@evidence;      # each spliced leader key is linked to an array of evidence 
-	    }
-	    push @data_pack, {
-	    	gene_info => $gene_info,
-			splice_info => \%spliced_leader
-	    };
-	 } 
-	return {
-		'data'=> \@data_pack,
-		'description' => 'structure information for this operon'
-	};
+    my $self   = shift;
+    my $operon = $self->object;
+    my @data_pack;
+    my @member_gene = $operon->Contains_gene;
+
+    foreach my $gene (@member_gene) {
+        my $gene_info = $self->_pack_obj($gene);
+        my %spliced_leader;
+        foreach my $sl ( $gene->col ) {
+            my @evidence = $sl->col;
+            $spliced_leader{$sl} = \@evidence
+              ;    # each spliced leader key is linked to an array of evidence
+        }
+        push @data_pack,
+          {
+            gene_info   => $gene_info,
+            splice_info => \%spliced_leader
+          };
+    }
+    return {
+        'data'        => \@data_pack,
+        'description' => 'structure information for this operon'
+    };
 }
 
 =head3 history
 
-This method will return a data structure with history of 	the operon.
+This method will return a data structure with history of the operon.
 
 =over
 
@@ -230,24 +232,24 @@ B<Response example>
 =cut 
 
 sub history {
-	my $self = shift;
-	my $object = $self->object;
-	my %data_pack;
-	my  @history_types = $object->History;
-	
-	foreach my $history_type (@history_types) {
-		my %histories;
-	  	foreach my $h ($history_type->col) {
-	    	my @evidence = $h->col;	     
-	     	@evidence = _get_evidence_names(\@evidence);
-	    	$histories{$h} = \@evidence;                    
-	  	} 
-	  	$data_pack{$history_type} = \%histories;        
-	}
-	return {
-		'data'=> $data_pack,
-		'description' => 'history of the information on the operon'
-	};
+    my $self   = shift;
+    my $object = $self->object;
+    my %data_pack;
+    my @history_types = $object->History;
+
+    foreach my $history_type (@history_types) {
+        my %histories;
+        foreach my $h ( $history_type->col ) {
+            my @evidence = $h->col;
+            @evidence = _get_evidence_names( \@evidence );
+            $histories{$h} = \@evidence;
+        }
+        $data_pack{$history_type} = \%histories;
+    }
+    return {
+        'data'        => $data_pack,
+        'description' => 'history of the information on the operon'
+    };
 }
 
 #########################
@@ -257,27 +259,33 @@ sub history {
 ##########################
 
 sub _get_evidence_names {
-  my ($evidences)=shift;
-  my @ret;
-  
-  foreach my $ev (@$evidences) {
-    my @names = $ev->col;
-    if($ev eq "Person_evidence" || $ev eq "Author_evidence" || $ev eq "Curator_confirmed") {    
-      $ev =~ /(.*)_(evidence|confirmed)/;  #find a better way to do this?    
-      @names =  map{$1 . ': ' . $_->Full_name || $_} @names;
-    }elsif ($ev eq "Paper_evidence"){
-      @names = map{'Paper: ' . $_->Brief_citation || $_} @names;
-    }elsif ($ev eq "Feature_evidence"){
-      @names = map{'Feature: '.  $_->Visible->right || $_} @names;
-    }elsif ($ev eq "From_analysis"){
-      @names = map{'Analysis: '. $_->Description || $_} @names;
-    }else {
-      @names = map{$ev . ': ' . $_} @names;
-    }
-    push(@ret, @names);
-  }
-  return @ret;
-}
+    my ($evidences) = shift;
+    my @ret;
 
+    foreach my $ev (@$evidences) {
+        my @names = $ev->col;
+        if (   $ev eq "Person_evidence"
+            || $ev eq "Author_evidence"
+            || $ev eq "Curator_confirmed" )
+        {
+            $ev =~ /(.*)_(evidence|confirmed)/;   #find a better way to do this?
+            @names = map { $1 . ': ' . $_->Full_name || $_ } @names;
+        }
+        elsif ( $ev eq "Paper_evidence" ) {
+            @names = map { 'Paper: ' . $_->Brief_citation || $_ } @names;
+        }
+        elsif ( $ev eq "Feature_evidence" ) {
+            @names = map { 'Feature: ' . $_->Visible->right || $_ } @names;
+        }
+        elsif ( $ev eq "From_analysis" ) {
+            @names = map { 'Analysis: ' . $_->Description || $_ } @names;
+        }
+        else {
+            @names = map { $ev . ': ' . $_ } @names;
+        }
+        push( @ret, @names );
+    }
+    return @ret;
+}
 
 1;
