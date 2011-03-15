f1e2acab27b97754346b27d79f39b85180206efe
Adrian Duong <adrian.duong@oicr.on.ca>
2011-03-15 11:49:44 -0400

    Initial POD for Pcr_oligo

--- lib/WormBase/API/Object/Pcr_oligo.pm
+++ lib/WormBase/API/Object/Pcr_oligo.pm
@@ -5,6 +5,29 @@ use Moose;
 with 'WormBase::API::Role::Object';
 extends 'WormBase::API::Object';
 
+# TODO:
+#  Split according to widgets? (May be hard for this aggregated class)
+
+
+=pod
+
+=head1 NAME
+
+WormBase::API::Object::Pcr_oligo
+
+=head1 SYNPOSIS
+
+Aggregate model for the Ace ?PCR_product, ?Oligo_set, and ?Oligo classes.
+
+=head1 URL
+
+http://wormbase.org/resources/pcr_oligo
+
+=head1 METHODS/URIs
+
+=cut
+
+
 has '_segment' => (
 	is		 => 'ro',
 	required => 1,
@@ -16,7 +39,7 @@ has '_segment' => (
 		$class .= ':reagent' if $class eq 'Oligo_set';
 		return $self->gff_dsn->segment($class => $object);
 	},
-   );
+);
 
 has '_oligos' => (
 	is => 'ro',
@@ -26,9 +49,9 @@ has '_oligos' => (
 		my ($self) = @_;
 		return $self ~~ '@Oligo';
 	},
-   );
+);
 
-has 'object_class' => (
+has '_object_class' => (
 	is => 'ro',
 	required => 1,
 	default => sub {
@@ -36,26 +59,118 @@ has 'object_class' => (
 		(my $class = $self ~~ 'class') =~ s/_/ /g;
 		return $class;
 	},
-   );
+);
 
-
-#############################################
-## Methods pertaining to all three classes
-#############################################
+################################################################################
+# Methods pertaining to all three classes
+################################################################################
 
 # sub name { }
 # Supplied by Role; POD will automatically be inserted here.
 # << include name >>
+
+=head3 canonical_parent
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->canonical_parent();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/canonical_parent
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 
 sub canonical_parent {
 	my ($self) = @_;
 
 	return {
-		description => 'Canonical parent of this ' . $self->object_class,
+		description => 'Canonical parent of this ' . $self->_object_class,
 		data		=> $self->_pack_obj($self ~~ 'Canonical_parent'),
 	};
 }
+
+=head3 oligos
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->oligos();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/oligos
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
+
 
 sub oligos {
 	my ($self) = @_;
@@ -70,10 +185,56 @@ sub oligos {
 	}
 
 	return {
-		description => 'Oligos of this' . $self->object_class,
+		description => 'Oligos of this' . $self->_object_class,
 		data		=> @data ? \@data : undef,
 	};
 }
+
+=head3 overlapping_genes
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->overlapping_genes();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/overlapping_genes
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub overlapping_genes {
 	my ($self) = @_;
@@ -87,50 +248,282 @@ sub overlapping_genes {
     } $self->_overlapping_genes($self->_segment);
 
 	return {
-		description => 'Overlapping genes of this ' . $self->object_class,
+		description => 'Overlapping genes of this ' . $self->_object_class,
 		data		=> %gene_info ? \%gene_info : undef,
 	};
 }
+
+=head3 overlaps_CDS
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->overlaps_CDS();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/overlaps_CDS
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub overlaps_CDS {
 	my ($self) = @_;
 	my $CDS = $self->_pack_objects($self ~~ '@Overlaps_CDS');
 
 	return {
-		description => 'CDS\'s that this ' . $self->object_class . ' overlaps',
+		description => 'CDS\'s that this ' . $self->_object_class . ' overlaps',
 		data		=> %$CDS ? $CDS : undef,
 	};
 }
+
+=head3 overlaps_transcript
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->overlaps_transcript();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/overlaps_transcript
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
+
 
 sub overlaps_transcript {
 	my ($self) = @_;
 	my $transcripts = $self->_pack_objects($self ~~ '@Overlaps_Transcript');
 
 	return {
-		description => 'Transcripts that this ' . $self->object_class . ' overlaps',
+		description => 'Transcripts that this ' . $self->_object_class . ' overlaps',
 		data		=> %$transcripts ? $transcripts : undef,
 	};
 }
+
+=head3 overlaps_pseudogene
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->overlaps_pseudogene();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/overlaps_pseudogene
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub overlaps_pseudogene {
 	my ($self) = @_;
 	my $pseudogenes = $self->_pack_objects($self ~~ '@Overlaps_Pseudogene');
 
 	return {
-		description => 'Pseudogenes that this ' . $self->object_class . ' overlaps',
+		description => 'Pseudogenes that this ' . $self->_object_class . ' overlaps',
 		data		=> %$pseudogenes ? \$pseudogenes : undef,
 	};
 }
+
+=head3 overlaps_variation
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->overlaps_variation();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/overlaps_variation
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub overlaps_variation {
 	my ($self) = @_;
 	my $variations = $self->_pack_objects($self ~~ '@Overlaps_Variation');
 
 	return {
-		description => 'Variations that this ' . $self->object_class . ' overlaps',
+		description => 'Variations that this ' . $self->_object_class . ' overlaps',
 		data		=> %$variations ? \$variations : undef,
 	};
 }
+
+=head3 on_orfeome_project
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->on_orfeome_project();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/on_orfeome_project
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
+
 
 # classic site note: MRC Genesevice and Open Biosystems links are invalid now
 sub on_orfeome_project {
@@ -140,7 +533,7 @@ sub on_orfeome_project {
 	my $data = $1;
 
 	return {
-		description => 'Finding this ' . $self->object_class . ' on the ORFeome project',
+		description => 'Finding this ' . $self->_object_class . ' on the ORFeome project',
 		data		=> $data,
 	};
 }
@@ -149,15 +542,109 @@ sub on_orfeome_project {
 # Supplied by Role; POD will automatically be inserted here.
 # << include remarks >>
 
+=head3 microarray_results
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->microarray_results();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/microarray_results
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
+
+
 sub microarray_results {
 	my ($self) = @_;
 
 	my $results = $self->_pack_objects($self ~~ '@Microarray_results');
 	return {
-		description => 'Microarray results involving this ' . $self->object_class,
+		description => 'Microarray results involving this ' . $self->_object_class,
 		data => %$results ? $results : undef,
 	};
 }
+
+=head3 segment
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->segment();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/segment
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
+
 
 sub segment {
 	my ($self) = @_;
@@ -169,7 +656,7 @@ sub segment {
 	}
 
 	return {
-		description => 'Sequence/segment data about this ' . $self->object_class,
+		description => 'Sequence/segment data about this ' . $self->_object_class,
 		data		=> %data ? \%data : undef,
 	};
 }
@@ -178,6 +665,53 @@ sub segment {
 ########################################
 ## Methods pertaining to PCR_product
 ########################################
+
+
+=head3 amplified
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->amplified();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/amplified
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub amplified {
 	my ($self) = @_;
@@ -189,11 +723,55 @@ sub amplified {
 	};
 }
 
-# CHECK THIS!!
 # sub laboratory { }
 # Supplied by Role; POD will automatically be inserted here.
 # << include laboratory >>
 
+=head3 SNP_loci
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->SNP_loci();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/SNP_loci
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub SNP_loci {
 	my ($self) = @_;
@@ -204,6 +782,52 @@ sub SNP_loci {
 		data		=> @loci ? \@loci : undef,
 	};
 }
+
+=head3 assay_conditions
+
+!!!Description
+
+=head4 PERL API
+
+ $data = $model->assay_conditions();
+
+=head4 REST API
+
+=head5 Request Method
+
+GET
+
+=head5 Requires Authentication
+
+No
+
+=head5 Parameters
+
+WBPerson ID
+
+=head5 Returns
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+=head5 Request example
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/pcr_oligo/mv_F25B5.5/assay_conditions
+
+=head5 Response example
+
+<div class="response-example"></div>
+
+=cut
 
 sub assay_conditions {
 	my ($self) = @_;
