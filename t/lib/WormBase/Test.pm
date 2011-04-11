7a8dbcf588f2b5a48ecbf89c8e2d15e57c250ac8
Adrian Duong <adrian.duong@oicr.on.ca>
2011-04-11 17:25:22 -0400

    WormBase::Test modules now use Test::Builder which works better...

--- t/lib/WormBase/Test.pm
+++ t/lib/WormBase/Test.pm
@@ -3,14 +3,16 @@ package WormBase::Test;
 use strict;
 use warnings;
 use Carp;
-
 use YAML qw(Dump);
-use Test::More;
+use Test::More ();
+use Test::Builder;
 
 use namespace::autoclean;
 
 # generic WormBase tester
 # base class for other WormBase testers
+
+my $Test = Test::Builder->new;
 
 sub new {
     my ($class, $args) = @_;
@@ -24,11 +26,16 @@ sub new {
 sub dump {
     my $self = shift;
     if (@_) {
-        diag(Dump(@_));
+        $Test->diag(Dump(@_));
     }
     else {
-        diag(Dump($self));
+        $Test->diag(Dump($self));
     }
+}
+
+sub isa_ok {
+    my $self = shift;
+    return &Test::More::isa_ok; # calls isa_ok, passing @_ straight to it
 }
 
 1;
