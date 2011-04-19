9c68550363a9d207248989625c49dfaccae0ced4
Adrian Duong <adrian.duong@oicr.on.ca>
2011-04-19 19:14:18 -0400

    Small cleanup of blast_blat.pm to suppress errors and warnings. Add a consideration comment to Test::API. Initial work on Test::Web.

--- t/lib/WormBase/Test/Web.pm
+++ t/lib/WormBase/Test/Web.pm
@@ -2,16 +2,25 @@ package WormBase::Test::Web;
 
 use strict;
 use warnings;
+use Readonly;
 
-use Exporter 'import';
-our @EXPORT;
+use Test::WWW::Mechanize::Catalyst;
 
-################################################################################
-# <<Documentation and stuffs here>>
-################################################################################
+use base 'WormBase::Test';
 
-# WormBase test library for testing the web interface
+Readonly our $APP_BASE => 'WormBase::Web';
+sub APP_BASE () { return $APP_BASE; }
 
-use Catalyst::Test 'WormBase::Web'; # load application
+my $Mech = $ENV{CATALYST_SERVER} ? Test::WWW::Mechanize::Catalyst->new
+         : Test::WWW::Mechanize::Catalyst->new({catalyst_app => $APP_BASE});
+
+sub new {
+    my $class = shift;
+    return bless {}, $class;
+}
+
+sub mech {
+    return $Mech;
+}
 
 1;
