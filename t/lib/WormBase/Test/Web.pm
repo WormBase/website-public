10173f649af8272f2ff0785fc7131ccb9af1661c
Adrian Duong <adrian.duong@oicr.on.ca>
2011-04-20 18:25:40 -0400

    Preview of the static page crawler. Will require WWW/Robot.pm to run (can download off search.cpan.org and save source under t/lib).

--- t/lib/WormBase/Test/Web.pm
+++ t/lib/WormBase/Test/Web.pm
@@ -11,8 +11,11 @@ use base 'WormBase::Test';
 Readonly our $APP_BASE => 'WormBase::Web';
 sub APP_BASE () { return $APP_BASE; }
 
-my $Mech = $ENV{CATALYST_SERVER} ? Test::WWW::Mechanize::Catalyst->new
-         : Test::WWW::Mechanize::Catalyst->new({catalyst_app => $APP_BASE});
+my %Mech_options = (allow_external => 1);
+
+my $Mech = $ENV{CATALYST_SERVER} ? Test::WWW::Mechanize::Catalyst->new({%Mech_options})
+         : Test::WWW::Mechanize::Catalyst->new({catalyst_app => $APP_BASE,
+                                                %Mech_options});
 
 sub new {
     my $class = shift;
