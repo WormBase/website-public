c868536e512ce3afeae94d98a9bc8802f4369508
Adrian Duong <adrian.duong@oicr.on.ca>
2011-04-25 17:41:21 -0400

    Abstraction & encapsulation process. Moving from 01static.t to Test::Web. Test::Web now suitable for subclasses...

---
+++ t/lib/WormBase/Util/URI.pm
@@ -0,0 +1,22 @@
+package WormBase::Util::URI;
+
+use strict;
+use warnings;
+
+use URI;
+use URI::URL;
+use List::Util qw(reduce);
+use Exporter 'import';
+
+our @EXPORT;
+our @EXPORT_OK = qw(is_same_domain);
+
+sub is_same_domain {
+    my @absolute_hps = map { $_->can('host_port') ? $_->host_port : () } @_;
+    return 1 unless @absolute_hps;
+
+    # have to check equality amongst the absolute URIs
+    return reduce { $a eq $b && $b } @absolute_hps;
+}
+
+1;
