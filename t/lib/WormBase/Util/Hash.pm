5c0e769bfb597dcafc3f5637ae28c4a14ad9520a
Adrian Duong <adrian.duong@oicr.on.ca>
2011-04-26 16:03:11 -0400

    Added more flexibility in Util::URI. Added "slicing" function to Util::Hash

--- t/lib/WormBase/Util/Hash.pm
+++ t/lib/WormBase/Util/Hash.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp;
 use Exporter 'import';
 
-our @EXPORT_OK = qw(merge_hashes);
+our @EXPORT_OK = qw(merge_hashes elements_at_level);
 
 =over
 
@@ -69,6 +69,57 @@ sub merge_hashes {
     }
 
     return merge_hashes(\%hash1, @hashes);
+}
+
+=item B<elements_at_level($structured_obj, $level)>
+
+    @elems = elements_at_level($structured_obj, $level);
+
+Traverses a structured object (hash or array) down to the specified level
+and returns a list of the elements at that level. The 0th level is the object
+itself, the 1st level is everything contained within the object, and so on.
+
+For example,
+
+    $hash = {
+        a => { d => 4,
+               e => 5,
+               f => [7, 8, 9]
+             },
+        b => { # empty hash
+             },
+        c => [ 1,
+               2,
+               3,
+               {
+                   g => 6,
+               }
+           ]
+    };
+
+   elements_at_level($hash, 0); # $hash itself
+   elements_at_level($hash, 1); # (hash at a, hash at b, array at c)
+   elements_at_level($hash, 2); # (4, 5, [7, 8, 9], 1, 2, 3, { g => 6 })
+   elements_at_level($hash, 3); # (7, 8, 9, 6)
+   elements_at_level($hash, 4); # ()
+
+This is like taking cross-sections of a structured object.
+
+=cut
+
+# not really a hash-specific method -- works for arrays as well
+sub elements_at_level {
+    my $obj = shift // return;
+    my $level = shift or return $obj; # level 0
+    croak 'The depth/level must be non-negative' if $level < 0;
+
+    return map { elements_at_level($_, $level-1) } values %$obj
+        if ref $obj eq 'HASH';
+
+    return map { elements_at_level($_, $level-1) } @$obj
+        if ref $obj eq 'ARRAY';
+
+    return; # don't know what it is
 }
 
 1;
