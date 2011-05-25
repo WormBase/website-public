f34499bdd8b578a6596fb10dcedb70a01a9829aa
Todd Harris <bitbucket@tharris.org>
2011-05-24 21:17:41 -0400

    life stage now complete

--- lib/WormBase/API/Object/Life_stage.pm
+++ lib/WormBase/API/Object/Life_stage.pm
@@ -88,10 +88,10 @@ B<Response example>
 =cut 
 
 sub substages {
-    my $self = shift;
+    my $self   = shift;
     my $object = $self->object;
     my @substages = map { $self->_pack_obj($_) } $object->Sub_stage;
-    return { data        => \@substages,
+    return { data        => @substages ? \@substages : undef,
 	     description => 'life substage' };
 }
 
@@ -162,149 +162,7 @@ sub definition {
 # Supplied by Role; POD will automatically be inserted here.
 # << include remarks >>
 
-
-#######################################
-#
-# Expression Patterns
-#
-#######################################
-
-# sub expression_patterns {}
-# Supplied by Role; POD will automatically be inserted here.
-# << include expression_patterns >>
-
-
-
-#######################################
-#
-# Cells
-#
-#######################################
-
-=head3 cells
-
-This method will return a data structure containing
-cells linked to the requested life stage.
-
-=over
-
-=item PERL API
-
- $data = $model->cells();
-
-=item REST API
-
-B<Request Method>
-
-GET
-
-B<Requires Authentication>
-
-No
-
-B<Parameters>
-
-Life stage (eg embryo)
-
-B<Returns>
-
-=over 4
-
-=item *
-
-200 OK and JSON, HTML, or XML
-
-=item *
-
-404 Not Found
-
-=back
-
-B<Request example>
-
-curl -H content-type:application/json http://api.wormbase.org/rest/field/life_stage/embryo/cells
-
-B<Response example>
-
-<div class="response-example"></div>
-
-=back
-
-=cut 
-
-sub cells {
-    my $self = shift;
-    my $object = $self->object;
-    my @cells = $object->Cell;
-    @cells = map { $self->_pack_obj($_) } @cells;
-    return { description => 'cells at this lifestage',
-	     data        => \@cells };
-}
-
-=head3 cell_group
-
-This method will return a data structure containing
-cell groups linked to the requested life stage.
-
-=over
-
-=item PERL API
-
- $data = $model->cell_group();
-
-=item REST API
-
-B<Request Method>
-
-GET
-
-B<Requires Authentication>
-
-No
-
-B<Parameters>
-
-Life stage (eg embryo)
-
-B<Returns>
-
-=over 4
-
-=item *
-
-200 OK and JSON, HTML, or XML
-
-=item *
-
-404 Not Found
-
-=back
-
-B<Request example>
-
-curl -H content-type:application/json http://api.wormbase.org/rest/field/life_stage/embryo/cell_group
-
-B<Response example>
-
-<div class="response-example"></div>
-
-=back
-
-=cut 
-
-sub cell_group {
-    my $self   = shift;
-    my $object = $self->shift;
-    my @cell_group = map { $self->_pack_obj($_) } $object->Cell_group;
-    return { description => 'The prominent cell group for this life stage',
-	     data        => \@cell_group };
-}
-
-#######################################
-#
-# Timing
-#
-#######################################
+# Timing. These used to be in their own widget.
 
 =head3 contained_in_life_stage
 
@@ -482,6 +340,146 @@ sub followed_by_life_stage {
     my @stages = map { $self->_pack_obj($_) } $object->Followed_by;
     return { description => 'next life stage after this',
 	     data        => \@stages };
+}
+
+
+#######################################
+#
+# Expression Patterns
+#
+#######################################
+
+# sub expression_patterns {}
+# Supplied by Role; POD will automatically be inserted here.
+# << include expression_patterns >>
+
+
+
+#######################################
+#
+# Cells
+#
+#  Cell/Cell_group not used on the site.
+#
+#######################################
+
+=head3 cells
+
+This method will return a data structure containing
+cells linked to the requested life stage.
+
+=over
+
+=item PERL API
+
+ $data = $model->cells();
+
+=item REST API
+
+B<Request Method>
+
+GET
+
+B<Requires Authentication>
+
+No
+
+B<Parameters>
+
+Life stage (eg embryo)
+
+B<Returns>
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+B<Request example>
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/life_stage/embryo/cells
+
+B<Response example>
+
+<div class="response-example"></div>
+
+=back
+
+=cut 
+
+sub cells {
+    my $self = shift;
+    my $object = $self->object;
+    my @cells = $object->Cell;
+    @cells = map { $self->_pack_obj($_) } @cells;
+    return { description => 'cells at this lifestage',
+	     data        => \@cells };
+}
+
+=head3 cell_group
+
+This method will return a data structure containing
+cell groups linked to the requested life stage.
+
+=over
+
+=item PERL API
+
+ $data = $model->cell_group();
+
+=item REST API
+
+B<Request Method>
+
+GET
+
+B<Requires Authentication>
+
+No
+
+B<Parameters>
+
+Life stage (eg embryo)
+
+B<Returns>
+
+=over 4
+
+=item *
+
+200 OK and JSON, HTML, or XML
+
+=item *
+
+404 Not Found
+
+=back
+
+B<Request example>
+
+curl -H content-type:application/json http://api.wormbase.org/rest/field/life_stage/embryo/cell_group
+
+B<Response example>
+
+<div class="response-example"></div>
+
+=back
+
+=cut 
+
+sub cell_group {
+    my $self   = shift;
+    my $object = $self->shift;
+    my @cell_group = map { $self->_pack_obj($_) } $object->Cell_group;
+    return { description => 'The prominent cell group for this life stage',
+	     data        => \@cell_group };
 }
 
 
