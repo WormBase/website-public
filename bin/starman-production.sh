4b925a733b08e646a7588643481f78cb6ff60384
Todd Harris <bitbucket@tharris.org>
2011-06-11 20:25:23 -0400

    made the footer bigger in anticipation of new content; fixed path to libraries in production

--- bin/starman-production.sh
+++ bin/starman-production.sh
@@ -6,10 +6,16 @@ export APPLIB="WormBase"
 export WORKERS=5
 export PORT=5000
 
-export PERL5LIB=/usr/local/wormbase/website/production/extlib/lib/perl5:/usr/local/wormbase/website/production/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/production/lib:/usr/local/wormbase/website/production/extlib/gbrowse2/current/lib/perl5/x86_64-linux-gnu-thread-multi:$PERL5LIB
-export MODULEBUILDRC="/usr/local/wormbase/website/production/extlib/.modulebuildrc"
-export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/website/production/extlib"
-export PATH="/usr/local/wormbase/website/production/extlib/bin:$PATH"
+#export PERL5LIB=/usr/local/wormbase/extlib/lib/perl5:/usr/local/wormbase/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/website/production/lib:$PERL5LIB
+#export MODULEBUILDRC="/usr/local/wormbase/extlib/.modulebuildrc"
+#export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/extlib"
+#export PATH="/usr/local/wormbase/extlib/bin:$PATH"
+
+
+export PERL5LIB=/usr/local/wormbase/shared/extlib/lib/perl5:/usr/local/wormbase/shared/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:/usr/local/wormbase/shared/website/production/lib:$PERL5LIB
+export MODULEBUILDRC="/usr/local/wormbase/shared/extlib/.modulebuildrc"
+export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/shared/extlib"
+export PATH="/usr/local/wormbase/shared/extlib/bin:$PATH"
 
 
 # this runs site-init.sh, assuming it's in the same directory
