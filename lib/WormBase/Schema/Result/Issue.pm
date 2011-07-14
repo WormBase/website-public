3c4659d64f7f554a7bcee4018ebf8938ce810b24
Todd Harris <info@toddharris.net>
2011-07-13 21:05:02 -0400

    add severity for issues, some goofy markup for highlighting based on that; issues can ow be reported privately, too

--- lib/WormBase/Schema/Result/Issue.pm
+++ lib/WormBase/Schema/Result/Issue.pm
@@ -19,23 +19,27 @@ WormBase::Schema::Result::Issue
 __PACKAGE__->table("issues");
 
 __PACKAGE__->add_columns(
-  "id",
-  { data_type => "integer", is_nullable => 0 },
-  "reporter_id",
-  { data_type => "integer", is_nullable => 1 },
-  "responsible_id",
-  { data_type => "integer", is_nullable => 1 },
-  "title",
-  { data_type => "text", is_nullable => 1 },
-  "state",
-  { data_type => "text", is_nullable => 1 },
-   "submit_time",
-  { data_type => "char(50)", is_nullable => 1 },
-  "page_id",
-  { data_type => "integer", default_value => 0, is_nullable => 0 },
-  "content",
-  { data_type => "text", is_nullable => 1 },
-);
+    "id",
+    { data_type => "integer", is_nullable => 0 },
+    "reporter_id",
+    { data_type => "integer", is_nullable => 1 },
+    "responsible_id",
+    { data_type => "integer", is_nullable => 1 },
+    "title",
+    { data_type => "text", is_nullable => 1 },
+    "state",
+    { data_type => "text", is_nullable => 1 },
+    "severity",
+    { data_type => "text", is_nullable => 1 },
+    "is_private",
+    { data_type => "text", is_nullable => 1 },
+    "submit_time",
+    { data_type => "char(50)", is_nullable => 1 },
+    "page_id",
+    { data_type => "integer", default_value => 0, is_nullable => 0 },
+    "content",
+    { data_type => "text", is_nullable => 1 },
+    );
 
 __PACKAGE__->set_primary_key("id");
 
@@ -43,4 +47,5 @@ __PACKAGE__->has_many(threads=>'WormBase::Schema::Result::IssueThread', 'issue_i
 __PACKAGE__->belongs_to(reporter=>'WormBase::Schema::Result::User','reporter_id');
 __PACKAGE__->belongs_to(responsible=>'WormBase::Schema::Result::User','responsible_id');
 __PACKAGE__->belongs_to(page=>'WormBase::Schema::Result::Page','page_id');
+
 1;
