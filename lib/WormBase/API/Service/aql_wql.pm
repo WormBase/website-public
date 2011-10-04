4afa3986b2d1ec87cc982c4d695bbdf0774fb370
Adrian Duong <adrian.duong@oicr.on.ca>
2011-10-04 19:30:26 -0400

    AQL/WQL core implementation
    Require Controller::Tool & API::Service restructure for more optimal implementation.

--- lib/WormBase/API/Service/aql_wql.pm
+++ lib/WormBase/API/Service/aql_wql.pm
@@ -6,9 +6,54 @@ use namespace::autoclean -except => 'meta';
 
 with 'WormBase::API::Role::Object';
 
-sub index {}
+sub index {
+    # nothing to do... ?
+}
 
-sub run {}
+sub run {
+    my ($self, $params) = @_;
+
+    my %result = (
+        result_type => $params->{'result-type'},
+        query_type  => $params->{'query-type'} || 'AQL',
+        query       => $params->{'ql-query'},
+    );
+
+    unless ($params->{'ql-query'}) {
+        return { %result, error => 'No query. Please enter a query' };
+    }
+
+    my $acedb = $self->dsn->{acedb}->dbh;
+    my $data = $params->{'query-type'} eq 'AQL'
+             ? $self->_run_aql($params, $acedb)
+             : $self->_run_wql($params, $acedb);
+
+    return { %result, data => $data };
+}
+
+sub _run_aql {
+    my ($self, $params, $dbh) = @_;
+
+    my $result;
+    foreach my $row ($dbh->aql($params->{'ql-query'})) {
+        push @$result, [
+            map { $_->isObject ? $self->_pack_obj($_) : "$_" } @$row
+        ];
+    }
+    return $result;
+}
+
+sub _run_wql {
+    my ($self, $params, $dbh) = @_;
+
+    $self->log->info("Running a WQL query");
+    my $it = $dbh->fetch_many(-query => $params->{'ql-query'}); # count, offset, total ?
+    # this is a raw iterator that returns objects... no good for the view
+
+    return {
+        next => sub { return $self->_pack_obj($it->next) },
+    };
+}
 
 __PACKAGE__->meta->make_immutable;
 
