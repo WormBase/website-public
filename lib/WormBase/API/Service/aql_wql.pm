734480b17fa446c45638dffa6954449d43d8d52e
Adrian Duong <adrian.duong@oicr.on.ca>
2011-10-05 15:16:06 -0400

    AQL/WQL fully implemented. Tools restructure prepared.
    * AQL/WQL works, just needs to be made pretty with stylesheets and some
      descriptive/instructive text.
    
    * The Tools backend is now ready for a restructure. Ideally,
      complicated tools should have their own controllers instead of
      pigeonholing all the tools into one Controller logic. This results
      in the BLAST/BLAT service(/model) having to handle display and
      state logic.

--- lib/WormBase/API/Service/aql_wql.pm
+++ lib/WormBase/API/Service/aql_wql.pm
@@ -1,58 +1,65 @@
 package WormBase::API::Service::aql_wql;
 
 use Moose;
-
 use namespace::autoclean -except => 'meta';
 
 with 'WormBase::API::Role::Object';
 
-sub index {
-    # nothing to do... ?
+# TODO: error trap on malformed queries
+
+sub aql {
+    my ($self, $query) = @_;
+    my $dbh = $self->dsn->{acedb}->dbh;
+
+    return [ $dbh->aql($query) ];
 }
 
-sub run {
-    my ($self, $params) = @_;
+# params: query, [return all instead of iter?]
+sub wql {
+    my ($self, $query, $ret_all) = @_;
+    my $dbh = $self->dsn->{acedb}->dbh;
+    # TODO: error trap on dbh
 
-    my %result = (
-        result_type => $params->{'result-type'},
-        query_type  => $params->{'query-type'} || 'AQL',
-        query       => $params->{'ql-query'},
-    );
-
-    unless ($params->{'ql-query'}) {
-        return { %result, error => 'No query. Please enter a query' };
+    if ($ret_all) {
+        return [$dbh->fetch(-query => $query)];
     }
 
-    my $acedb = $self->dsn->{acedb}->dbh;
-    my $data = $params->{'query-type'} eq 'AQL'
-             ? $self->_run_aql($params, $acedb)
-             : $self->_run_wql($params, $acedb);
+    my $it = $dbh->fetch_many(-query => $query); # count, offset, total ?
 
-    return { %result, data => $data };
+    return { next => sub { return $self->_pack_obj($it->next) } };
 }
 
-sub _run_aql {
-    my ($self, $params, $dbh) = @_;
-
-    my $result;
-    foreach my $row ($dbh->aql($params->{'ql-query'})) {
-        push @$result, [
-            map { $_->isObject ? $self->_pack_obj($_) : "$_" } @$row
-        ];
+sub objs2text {
+    my $self = shift;
+    if (@_ > 1 || ref $_[0] ne 'ARRAY') {
+        die 'Expecting a single arrayref.';
     }
-    return $result;
+
+    return ref($_[0]->[0]) eq 'ARRAY' # multidimensional?
+         ? join "\n", map { join "\t", @$_ } @{$_[0]}
+         : join "\n", @{$_[0]};
 }
 
-sub _run_wql {
-    my ($self, $params, $dbh) = @_;
+sub objs2pack { # destructive for efficiency
+    my $self = shift;
+    if (@_ > 1 || ref $_[0] ne 'ARRAY' || wantarray) {
+        die 'This is a destructive operation expecting a single arrayref, returning nothing.';
+    }
 
-    $self->log->info("Running a WQL query");
-    my $it = $dbh->fetch_many(-query => $params->{'ql-query'}); # count, offset, total ?
-    # this is a raw iterator that returns objects... no good for the view
+    if (ref($_[0]->[0]) eq 'ARRAY') { # multidimensional array
+        foreach (@{$_[0]}) {          # foreach row
+            foreach (@$_) {           # foreach col
+                $_ = $_->isObject ? $self->_pack_obj($_) : "$_";
+            }
+        }
+    }
+    else {                            # unidimensional array
+        foreach (@{$_[0]}) {
+            $_ = $_->isObject ? $self->_pack_obj($_) : "$_";
+        }
+    }
 
-    return {
-        next => sub { return $self->_pack_obj($it->next) },
-    };
+    return;
 }
 
 __PACKAGE__->meta->make_immutable;
