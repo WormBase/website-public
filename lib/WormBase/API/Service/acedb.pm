8a3d4691b77b8d4f7b0755868b01a437c5288cd4
Adrian Duong <adrian.duong@oicr.on.ca>
2011-10-26 18:04:06 -0400

    Adding back Ace memory cache. Disabling Ace file cache.
    The former probably benefits us more than it hurts us.
    The latter probably hurts us more than it benefits us. (makes WB empirically slow)

--- lib/WormBase/API/Service/acedb.pm
+++ lib/WormBase/API/Service/acedb.pm
@@ -5,14 +5,6 @@ use Ace;
 
 use namespace::clean -except => 'meta';
 
-{
-    # the following stops Ace from caching in memory and default to
-    # file cache or fetch directly from AceDB.
-    no warnings 'redefine';
-    *Ace::memory_cache_store = sub {};
-}
-
-
 has 'dbh'     => (
     is        => 'rw',
     isa       => 'Ace',
@@ -76,30 +68,29 @@ sub connect {
     my $self = shift;
     my $conf = $self->conf;
 
-    my %cache_args         =  (
-        cache_root         => $conf->{cache_root},
-        max_size           => $conf->{cache_size},
-	    default_expires_in => $conf->{cache_expires},
-    ) if $conf->{cache_root};
-
-    if ($conf->{cache_auto_purge_interval} ne '') {
-        $cache_args{cache_auto_purge_interval} = 
-            $conf->{cache_auto_purge_interval};
-    }
-
     my %options =  ( # will always have this...
         -user   => $self->user,
         -pass   => $self->pass,
     );
 
-    if (my $prog                      = $self->program and my $path = $self->path) {
+    if (my $prog = $self->program and my $path = $self->path) {
         @options{'-program', '-path'} = ($prog, $path);
     }
     else {
         @options{'-host', '-port'} = ($self->host, $self->port);
     }
 
-    $options{-cache} = \%cache_args if %cache_args;
+    if ($conf->{cache_root}) { # cache root indicates need file cache
+        $options{-cache} = {
+            cache_root         => $conf->{cache_root},
+            max_size           => $conf->{cache_size},
+            default_expires_in => $conf->{cache_expires},
+        };
+
+        # ace requires the following to have a value if present
+        $options{-cache}->{cache_auto_purge_interval} = $conf->{cache_auto_purge_interval}
+            if $conf->{cache_auto_purge_interval};
+    }
 
     return Ace->connect(%options) || die Ace->error;
 }
