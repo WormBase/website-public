5759c66dd5002fc8b3671f096f7244960eb045a4
Adrian Duong <adrian.duong@oicr.on.ca>
2011-10-31 19:04:18 -0400

    General raw_* method in WormBase::Ace
    Gives performance boost for ModelMap'd common names. From 30 s to 25 s
    (20% speed boost) on large Gene object, although YMMV depending on the
    nubmer of objects with overridden common names. Whenever possible,
    write a raw_fetch_* method for each ModelMap'd common name tag to gain
    speed out of a more specialized regexp instead of using the general
    regexp, which is itself an area for optimization.

--- lib/WormBase/Ace.pm
+++ lib/WormBase/Ace.pm
@@ -5,10 +5,25 @@ use warnings;
 
 use parent 'Ace';
 
+# wishlist: list context should return a bunch
+sub raw_fetch {
+    my ($self, $object, $method) = @_;
+
+    # run the optimized/correct method if possible
+    my $raw_fetch_alt = "raw_fetch_\l$method";
+    return $self->$raw_fetch_alt($object) if $self->can($raw_fetch_alt);
+
+    my $class = $object->class;
+
+    $self->raw_query(qq(find $class "$object"));
+    my ($val) = $self->raw_query("show -a $method") =~ /^$method\s+"(.*?)(?<!(?<!\\)\\)"/sm;
+    return $val;
+}
+
 # Returns the Species entry of the given object.
 # Undef if the object does not have a Species entry.
 # ASSUMPTION: Species string is simple (does not contain \ or " in it)
-sub raw_species {
+sub raw_fetch_species {
     my ($self, $object) = @_;
     my $class = $object->class;
     $self->raw_query(qq(find $class "$object"));
