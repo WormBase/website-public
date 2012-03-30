293579325280fb07d55163d62d958979f9c0b59b
Todd Harris <info@toddharris.net>
2012-03-30 16:21:29 -0400

    expanded classic compat to handle MOST but not all old style URLs; we are not free to live in the present or plan for the future because we are mortgaged to the past

--- lib/WormBase/Web/Controller/ClassicCompatibility.pm
+++ lib/WormBase/Web/Controller/ClassicCompatibility.pm
@@ -38,6 +38,60 @@ sub get :Local Args(0) {
     $c->detach('/get');
 }
 
+
+# Capture other old URLs like /db/misc
+sub misc : LocalRegex('*') CaptureArgs(2) {
+    my ($self, $c) = @_;
+    $c->stash->{template} = 'species/report.tt2';
+
+    my $requested_class = $c->req->param('class');
+    my $name            = $c->req->param('name');
+
+    my $api    = $c->model('WormBaseAPI');
+    my $ACE2WB = $api->modelmap->ACE2WB_MAP->{class};
+
+    # hack for locus (legacy):
+    $requested_class = 'Gene' if lc $requested_class eq 'locus';
+
+    # there may be input (perhaps external, hand-typed input or even automated
+    # input from a non-WB tool) which specifies a class in the incorrect casing
+    # but is otherwise legitimate (e.g. Go_term, which should be GO_term). this
+    # could be a problem in those kinds of input.
+    my $class = $ACE2WB->{$requested_class}
+             || $ACE2WB->{lc $requested_class} # canonical Ace class
+             or $c->detach('/soft_404');
+
+    my $normed_class = lc $class;
+
+    my $url;
+    if (exists $c->config->{sections}->{species}->{$normed_class}) { # /species
+        # Fetch our external model
+        my $api = $c->model('WormBaseAPI');
+
+        my $object;
+
+        # Fetch a WormBase::API::Object::* object
+        if ($name eq '*' || $name eq 'all') {
+            $object = $api->instantiate_empty($class);
+        }
+        else {
+            $object = $api->fetch({
+                class => $class,
+                name  => $name,
+            }) or die "Couldn't fetch an object: $!";
+        }
+
+        my $species = eval { $object->Species } || 'any';
+        $url = $c->uri_for('/species', $species, $normed_class, $name);
+    }
+    else {                      # /report
+        $url = $c->uri_for('/resources', $normed_class, $name);
+    }
+
+    $c->res->redirect($url);
+}
+
+
 # TODO: POD
 
 sub gbrowse_popup :Path('misc/gbrowse_popup') :Args(0) {
