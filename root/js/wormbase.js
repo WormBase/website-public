cc247fe37366d78420c7899a320965b54da8c2a0
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2012-03-30 10:36:48 -0400

    error message - put technical message in ui-state-error

--- root/js/wormbase.js
+++ root/js/wormbase.js
@@ -548,8 +548,8 @@
         },
         error:function(xhr, textStatus, thrownError){
           var error = $jq(xhr.responseText);
-          ajaxPanel.html('<div class="ui-state-error ui-corner-all description"><p><strong>Sorry!</strong> An error has occured.</p><p><a href="/tools/support?url=' + location.pathname + '">Let us know</a></p></div>');
-          ajaxPanel.append(error.find(".error-message-technical").html());
+          ajaxPanel.html('<div class="ui-state-error ui-corner-all"><p><strong>Sorry!</strong> An error has occured.</p><p><a href="/tools/support?url=' + location.pathname + '">Let us know</a></p></div>')
+                   .children().append('<p>' + error.find(".error-message-technical").html() + '</p>');
         },
         complete:function(XMLHttpRequest, textStatus){
           if(callback){ callback(); }
