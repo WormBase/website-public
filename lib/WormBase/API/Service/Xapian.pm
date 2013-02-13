f7c4651249741905d6006826372492d3de3ab4ce
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2013-02-13 11:34:03 -0500

    merge conflict

--- lib/WormBase/API/Service/Xapian.pm
+++ lib/WormBase/API/Service/Xapian.pm
@@ -101,20 +101,27 @@ sub search_autocomplete {
 sub search_exact {
     my ($class, $c, $q, $type) = @_;
   
-    my ($query, $enq);
+    my ($query, $enq, $mset);
     if( $type && ( ($q =~ m/^WB/i) || $type eq 'disease' || $type eq 'gene_class') ){
       $query=$class->qp->parse_query( "$type$q", 1|2 );
       $enq       = $class->db->enquire ( $query );
-      $c->log->debug("query:" . $query->get_description());
-    }elsif(!($q =~ m/\s.*\s/)){
-      $q = "\"$q\"" if(($q =~ m/\s/) && !($q =~ m/.*_.*/));
-      $q = $class->_add_type_range($c, $q, $type);
-      $query=$class->syn_qp->parse_query( $q, 1|2 );
-      $enq       = $class->syn_db->enquire ( $query );
-      $c->log->debug("query:" . $query->get_description());
+      $c->log->debug("query exacta:" . $query->get_description());
+      $mset = $enq->get_mset( 0,2 ) if $enq;
+      $mset = undef if ($mset->size() > 1);
+    }
+   
+    if((!$mset || $mset->empty() || $q =~ m/\s/) && (!($q =~ m/\s.*\s/))){
+        my $qu = $q;
+        $qu = "$1 $qu" if ($qu =~ s/\.(.*)$/*/g);
+        $qu = "\"$qu\"" if(($qu =~ m/\s/) && !($qu =~ m/_/));
+        $qu = $class->_add_type_range($c, $qu, $type);
+        $query=$class->syn_qp->parse_query( $qu, 2|256 );
+        $enq       = $class->syn_db->enquire ( $query );
+        $c->log->debug("query exactb:" . $query->get_description());
+        $mset      = $enq->get_mset( 0,2 ) if $enq;
     }
 
-    my $mset      = $enq->get_mset( 0,2 ) if $enq;
+    $mset      = $enq->get_mset( 0,2 ) if $enq;
     if(!$mset || $mset->empty()){
       $q = $class->_add_type_range($c, $q, $type);
 
@@ -176,11 +183,11 @@ sub extract_data {
 
 sub _get_obj {
   my ($self, $c, $doc, $footer) = @_;
-  my $species = $doc->get_value(5);
+  my $species = $self->_get_taxonomy($doc);
 
   my %ret;
   $ret{name} = $self->_pack_search_obj($c, $doc);
-  if($species =~ m/^(.)_(.*)$/){
+  if($species =~ m/^(.).*_([^_]*)$/){
     my $s = $c->config->{sections}{species_list}{$species};
     $ret{taxonomy}{genus} = $s->{genus} || uc($1) . '.';
     $ret{taxonomy}{species} = $s->{species} || $2;
@@ -262,6 +269,15 @@ sub _get_tag_info {
   return $tag;
 }
 
+# why is species sometimes getting stored weird in xapian? 
+# eg. c_caenorhabditis_elegans instead of c_elegans
+sub _get_taxonomy {
+  my ($self, $doc) = @_;
+  my $taxonomy = $doc->get_value(5);
+  $taxonomy =~ s/_([^_]*)_/\_/g;
+  return $taxonomy;
+}
+
 sub _pack_search_obj {
   my ($self, $c, $doc, $label) = @_;
   my $id = $doc->get_value(1);
@@ -270,7 +286,7 @@ sub _pack_search_obj {
   return {  id => $id,
             label => $label || $doc->get_value(6) || $id,
             class => lc($class),
-            taxonomy => $doc->get_value(5),
+            taxonomy => $self->_get_taxonomy($doc),
             coord => { start => $doc->get_value(9),
                        end => $doc->get_value(10),
                        strand => $doc->get_value(11)}
