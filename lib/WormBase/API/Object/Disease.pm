29e6d81c85b395f72eb93f59884a53188c523d96
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2013-03-15 17:09:49 -0400

    Disease - changes from curator input * make 'Disease Ontology Term' more obvious * style dropdown to fit 'Disease Ontology Terms' * change overview layout * link to Disease ontology - update external links * update search results to link to disease ontology * fix #947

--- lib/WormBase/API/Object/Disease.pm
+++ lib/WormBase/API/Object/Disease.pm
@@ -79,7 +79,7 @@ sub type {
 
 sub synonym {
     my ($self) = @_;
-    my @synonym = map { map { $self->_pack_obj($_) } $_->col} $self->object->Synonymn;
+    my @synonym = map { $_->col} $self->object->Synonymn;
 
     return {
         description => 'Synonym of this disease',
