8b4b08ccf6af51c391ee9388333c5ba5b036b0ae
Joachim Baran <joachim.baran@gmail.com>
2013-05-08 15:48:16 -0400

    If features are added using the "add" parameter with GBrowse URIs, then also add "flip=1" if they are on the reverse strand. #155

--- lib/WormBase/API/Service/blast_blat.pm
+++ lib/WormBase/API/Service/blast_blat.pm
@@ -1021,6 +1021,7 @@ sub extract_hit_info {
 
         $hit->rewind;
 
+        my $on_reverse_strand = '';
         my @hsp_genome_link_parts;
         while (my $hsp = $hit->next_hsp) {
             $counter++;
@@ -1032,19 +1033,18 @@ sub extract_hit_info {
             my $hsp_strand =
               $hsp->strand('hit') ne $hsp->strand('query') ? -1 : 1;
 
-            # If the feature is on the reverse strand, then swap start/end coordinates of the hit.
-            my $hsp_genome_link_part;
-            if ($hsp_strand == 1) {
-              $hsp_genome_link_part = qq[$hsp_start-$hsp_end];
-            } else {
-              $hsp_genome_link_part = qq[$hsp_end-$hsp_start];
+            my $hsp_genome_link_part = qq[$hsp_start-$hsp_end];
+
+            # If the feature is on the reverse strand, then flip the coordinates in GBrowse:
+            if ($hsp_strand != 1) {
+                $on_reverse_strand = 'flip=1;'
             }
 
             push @hsp_genome_link_parts, $hsp_genome_link_part
               if @hsp_genome_link_parts < $self->pre_compile->{HSP_GENOME_LINK_PART_LIMIT};
         }
 
-	my $hit_ranges = qq[add=${hit_name}+Hits+Hits+] . join(',', @hsp_genome_link_parts);
+	my $hit_ranges = qq[${on_reverse_strand};add=${hit_name}+Hits+Hits+] . join(',', @hsp_genome_link_parts);
 
         $hit_genome_link = $gbrowse_root . qq[?name=${hit_name};$hit_ranges] if $gbrowse_root;
         $hit_expand_link = $expand_root . qq[?] . join(";", 'width=450', qq[name=${hit_name};$hit_ranges]) if $expand_root;
