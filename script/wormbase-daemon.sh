c070bf686cab759562183359c7261c82f505e16d
Joachim Baran <joachim.baran@gmail.com>
2013-07-24 11:12:07 -0400

    Bugfix. Daemon script had invalid BASH if statement.

--- script/wormbase-daemon.sh
+++ script/wormbase-daemon.sh
@@ -154,7 +154,7 @@ if [ $? -gt 0 ]; then
 #                --chdir $APP_HOME --exec $START_SERVER_DAEMON -- $START_SERVER_DAEMON_OPTS
 fi
 
-if [ "$APP" == 'staging' && "$API_TESTS" == '1' ] ; then
+if [ "$APP" == 'staging' ] && [ "$API_TESTS" == '1' ] ; then
     sleep 5
     curl http://staging.wormbase.org
     while [ $? -eq 0 ] ; do
