3d9fb6e1725a4c5048ef80deed3f2d3c3e20e202
Joachim Baran <joachim.baran@gmail.com>
2013-07-30 17:39:43 -0400

    Summary display of tested GBrowse URLs.

--- root/js/metric_viewer.js
+++ root/js/metric_viewer.js
@@ -1,40 +1,109 @@
 
+// CouchDB: URL and report database
 var couchBaseUri = 'http://dev.wormbase.org:5984';
 var couchReportDB = 'reports';
 
-function getReportListing() {
+var overviewGraph = null;
+var overviewData = {
+  totalUrls: [],
+  brokenUrls: [],
+  imageMismatches: [],
+  missingReferences: [],
+  tracks: [],
+  landmarks: []
+};
+
+function couchRequest(request, callback) {
   jQuery.ajax({
     type: 'GET',
-    url: couchBaseUri + "/" + couchReportDB + "/_all_docs",
+    url: couchBaseUri + "/" + couchReportDB + "/" + request,
     processData: true,
     data: {},
     dataType: "json",
-    success: function (data) {
-      alert(data);
+    success: function(data) {
+      callback(data, null);
+    },
+    error: function(request, textStatus) {
+      callback(null, textStatus);
     }
   });
 }
 
-function renderOverview() {
-  var graph = new Rickshaw.Graph({
-    element: document.querySelector('#graph'),
-    series: [
-      {
-        color: '#2C3E50',
-        data: [ { x: 0, y: 23}, { x: 1, y: 15 }, { x: 2, y: 79 } ]
-      }, {
-        color: '#16A085',
-        data: [ { x: 0, y: 30}, { x: 1, y: 20 }, { x: 2, y: 64 } ]
+function getReportListing(callback) {
+  couchRequest("_all_docs", callback);
+}
+
+function getReportOverview(data, error) {
+  if (error) {
+     // TODO
+     return;
+  }
+
+  if (data["type"] != "gbrowse" || !data["completed"])
+    return;
+
+  var totalUrls = 0;
+  var brokenUrls = 0;
+  var imageMismatches = 0;
+  var missingReferences = 0;
+  var tracks = 0;
+  var landmarks = 0;
+  for (var i = 0; i < data["configurations"].length; i++) {
+    totalUrls += data[data["configurations"][i]]["total_urls"];
+    brokenUrls += data[data["configurations"][i]]["broken_urls"];
+    imageMismatches += data[data["configurations"][i]]["image_mismatches"];
+    missingReferences += data[data["configurations"][i]]["missing_references"];
+    tracks += data[data["configurations"][i]]["tracks"];
+    landmarks += data[data["configurations"][i]]["example_landmarks"];
+  }
+  overviewData["totalUrls"].push([ data["started_since_epoch"] * 1000, totalUrls ]);
+  overviewData["brokenUrls"].push([ data["started_since_epoch"] * 1000, brokenUrls ]);
+  overviewData["imageMismatches"].push([ data["started_since_epoch"] * 1000, imageMismatches ]);
+  overviewData["missingReferences"].push([ data["started_since_epoch"] * 1000, missingReferences ]);
+  overviewData["tracks"].push([ data["started_since_epoch"] * 1000, tracks ]);
+  overviewData["landmarks"].push([ data["started_since_epoch"] * 1000, landmarks ]);
+
+  if (!overviewGraph) {
+    overviewGraph = $.plot($("#overviewgraph"), [], {
+      series: {
+        lines: { show: true },
+        points: { show: true }
+      },
+      xaxis: {
+        mode: "time",
+        timeformat: "%Y-%m-%dT%H:%M:%S"
+      },
+      yaxis: {
+        min: 0
       }
-    ]
-  });
+    });
+  }
 
-  new Rickshaw.Graph.HoverDetail({
-      graph: graph,
-      xFormatter: function(x) { return x + "seconds" },
-      yFormatter: function(y) { return Math.floor(y) + " percent" }
-  });
+  overviewGraph.setData([
+    { label: "URLs in configuration files", data: overviewData["totalUrls"] },
+    { label: "Tested URLs that were broken", data: overviewData["brokenUrls"] },
+    { label: "Mismatching images (reference vs. actual)", data: overviewData["imageMismatches"] },
+    { label: "URLs that lack a reference image", data: overviewData["missingReferences"] },
+    { label: "Tracks", data: overviewData["tracks"] },
+    { label: "Example landmarks", data: overviewData["landmarks"] }
+  ]);
+  overviewGraph.setupGrid();
+  overviewGraph.draw();
+}
 
-  graph.render();
+function createOverviewGraph(data, error) {
+  if (error) {
+     // TODO
+     return;
+  }
+
+  var records = data['rows'];
+  for (var i = 0; i < records.length; i++) {
+    couchRequest(records[i]["id"], getReportOverview);
+  }
+}
+
+function renderOverview() {
+  getReportListing(createOverviewGraph);
 }
 
