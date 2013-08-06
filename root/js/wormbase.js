5b61ab52964a59ddc70afbddcde2da40774d769e
Joachim Baran <joachim.baran@gmail.com>
2013-08-06 14:01:46 -0400

    Updated Datatables and Tabletools to latest versions. Fixed a bug that prevented the loading of Tabletools' Flash code. #1484

--- root/js/wormbase.js
+++ root/js/wormbase.js
@@ -1859,7 +1859,7 @@ function setupCytoscape(data, types, clazz){
                         markitup: "/js/jquery/plugins/markitup/jquery.markitup.js",
                         "markitup-wiki": "/js/jquery/plugins/markitup/sets/wiki/set.js",
                         cytoscape_web: "/js/jquery/plugins/cytoscapeweb/js/min/cytoscapeweb_all.min.js",
-                        tabletools: "/js/jquery/plugins/tabletools/media/js/TableTools.all.min.js",
+                        tabletools: "/js/jquery/plugins/tabletools/media/js/TableTools.min.js",
                         placeholder: "/js/jquery/plugins/jquery.placeholder.min.js"
           },
           pStyle = {    dataTables: "/js/jquery/plugins/dataTables/media/css/demo_table.css",
