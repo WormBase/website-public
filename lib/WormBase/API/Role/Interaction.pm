9dace7b197b914645c9eed0cb834641f62b0604e
Joachim Baran <joachim.baran@gmail.com>
2013-08-07 13:12:30 -0400

    Re-enabled exclusion of certain interactions that were removed in 4c27ba0940632acea19c69f7a4687420c6d9a609. #1365

--- lib/WormBase/API/Role/Interaction.pm
+++ lib/WormBase/API/Role/Interaction.pm
@@ -262,6 +262,7 @@ sub _get_interactions {
 sub _get_interaction_info {
     my ($self, $interaction, $nearby) = @_;
     my %results;
+    return \%results if $self->_ignored_interactions($interaction);
     my $object = $self->object;
     my $type = $interaction->Interaction_type;
     $type = $type->right ? $type->right . '' : "$type";
@@ -343,6 +344,72 @@ sub _get_gene {
       $corr_gene ||= $obj->Matching_pseudogene->Gene if $obj->Matching_pseudogene;
       return $corr_gene;
     }
+}
+
+sub _ignored_interactions {
+    my ($self, $interaction) = @_;
+    # These are interactions in the WS231 model that were requested to be ignored by Chris Grove.
+    # They should be updated/corrected/removed in the next database release so this can be removed then.
+    my %bad_interactions = (
+    WBInteraction000505886 => 1, WBInteraction000505887 => 1, WBInteraction000505888 => 1, WBInteraction000505889 => 1,
+    WBInteraction000505890 => 1, WBInteraction000505891 => 1, WBInteraction000505892 => 1, WBInteraction000505893 => 1,
+    WBInteraction000505894 => 1, WBInteraction000505895 => 1, WBInteraction000505896 => 1, WBInteraction000505897 => 1,
+    WBInteraction000505898 => 1, WBInteraction000505899 => 1, WBInteraction000505900 => 1, WBInteraction000505901 => 1,
+    WBInteraction000505902 => 1, WBInteraction000505903 => 1, WBInteraction000511628 => 1, WBInteraction000511629 => 1,
+    WBInteraction000511630 => 1, WBInteraction000511631 => 1, WBInteraction000511632 => 1, WBInteraction000511633 => 1,
+    WBInteraction000511634 => 1, WBInteraction000511635 => 1, WBInteraction000511636 => 1, WBInteraction000511637 => 1,
+    WBInteraction000511638 => 1, WBInteraction000511639 => 1, WBInteraction000511640 => 1, WBInteraction000511641 => 1,
+    WBInteraction000511642 => 1, WBInteraction000511643 => 1, WBInteraction000511644 => 1, WBInteraction000511645 => 1,
+    WBInteraction000511646 => 1, WBInteraction000511647 => 1, WBInteraction000511648 => 1, WBInteraction000511649 => 1,
+    WBInteraction000511650 => 1, WBInteraction000511651 => 1, WBInteraction000511652 => 1, WBInteraction000511653 => 1,
+    WBInteraction000511654 => 1, WBInteraction000511655 => 1, WBInteraction000511656 => 1, WBInteraction000511657 => 1,
+    WBInteraction000511658 => 1, WBInteraction000511659 => 1, WBInteraction000511660 => 1, WBInteraction000511661 => 1,
+    WBInteraction000511662 => 1, WBInteraction000511663 => 1, WBInteraction000511664 => 1, WBInteraction000511665 => 1,
+    WBInteraction000511666 => 1, WBInteraction000511667 => 1, WBInteraction000511668 => 1, WBInteraction000511669 => 1,
+    WBInteraction000511670 => 1, WBInteraction000511671 => 1, WBInteraction000511672 => 1, WBInteraction000511673 => 1,
+    WBInteraction000511674 => 1, WBInteraction000511675 => 1, WBInteraction000511676 => 1, WBInteraction000511677 => 1,
+    WBInteraction000511678 => 1, WBInteraction000511679 => 1, WBInteraction000511680 => 1, WBInteraction000511681 => 1,
+    WBInteraction000511682 => 1, WBInteraction000511683 => 1, WBInteraction000511684 => 1, WBInteraction000511685 => 1,
+    WBInteraction000511686 => 1, WBInteraction000511687 => 1, WBInteraction000511688 => 1, WBInteraction000511689 => 1,
+    WBInteraction000511690 => 1, WBInteraction000511691 => 1, WBInteraction000511692 => 1, WBInteraction000511693 => 1,
+    WBInteraction000511694 => 1, WBInteraction000511695 => 1, WBInteraction000511696 => 1, WBInteraction000511697 => 1,
+    WBInteraction000511698 => 1, WBInteraction000511719 => 1, WBInteraction000511720 => 1, WBInteraction000512922 => 1,
+    WBInteraction000512926 => 1, WBInteraction000512927 => 1, WBInteraction000512928 => 1, WBInteraction000512929 => 1,
+    WBInteraction000512930 => 1, WBInteraction000512931 => 1, WBInteraction000512932 => 1, WBInteraction000512933 => 1,
+    WBInteraction000512934 => 1, WBInteraction000512935 => 1, WBInteraction000514981 => 1, WBInteraction000514982 => 1,
+    WBInteraction000514983 => 1, WBInteraction000514984 => 1, WBInteraction000514985 => 1, WBInteraction000514986 => 1,
+    WBInteraction000514987 => 1, WBInteraction000514988 => 1, WBInteraction000514989 => 1, WBInteraction000514990 => 1,
+    WBInteraction000514991 => 1, WBInteraction000514992 => 1, WBInteraction000514993 => 1, WBInteraction000514994 => 1,
+    WBInteraction000514995 => 1, WBInteraction000514996 => 1, WBInteraction000514997 => 1, WBInteraction000514998 => 1,
+    WBInteraction000514999 => 1, WBInteraction000515000 => 1, WBInteraction000515001 => 1, WBInteraction000515002 => 1,
+    WBInteraction000515003 => 1, WBInteraction000515004 => 1, WBInteraction000515005 => 1, WBInteraction000515006 => 1,
+    WBInteraction000515007 => 1, WBInteraction000515008 => 1, WBInteraction000515009 => 1, WBInteraction000515010 => 1,
+    WBInteraction000515011 => 1, WBInteraction000515012 => 1, WBInteraction000515013 => 1, WBInteraction000515014 => 1,
+    WBInteraction000515015 => 1, WBInteraction000515016 => 1, WBInteraction000515017 => 1, WBInteraction000515018 => 1,
+    WBInteraction000515029 => 1, WBInteraction000515030 => 1, WBInteraction000515031 => 1, WBInteraction000515032 => 1,
+    WBInteraction000515033 => 1, WBInteraction000515034 => 1, WBInteraction000515035 => 1, WBInteraction000515036 => 1,
+    WBInteraction000515037 => 1, WBInteraction000515038 => 1, WBInteraction000515054 => 1, WBInteraction000515055 => 1,
+    WBInteraction000515056 => 1, WBInteraction000515057 => 1, WBInteraction000515058 => 1, WBInteraction000515059 => 1,
+    WBInteraction000515060 => 1, WBInteraction000515061 => 1, WBInteraction000515062 => 1, WBInteraction000515063 => 1,
+    WBInteraction000515064 => 1, WBInteraction000515065 => 1, WBInteraction000515066 => 1, WBInteraction000515067 => 1,
+    WBInteraction000515072 => 1, WBInteraction000515073 => 1, WBInteraction000515074 => 1, WBInteraction000515075 => 1,
+    WBInteraction000515076 => 1, WBInteraction000515077 => 1, WBInteraction000515078 => 1, WBInteraction000515079 => 1,
+    WBInteraction000515080 => 1, WBInteraction000515081 => 1, WBInteraction000515082 => 1, WBInteraction000515083 => 1,
+    WBInteraction000515084 => 1, WBInteraction000515085 => 1, WBInteraction000515086 => 1, WBInteraction000515087 => 1,
+    WBInteraction000515088 => 1, WBInteraction000515089 => 1, WBInteraction000515090 => 1, WBInteraction000515091 => 1,
+    WBInteraction000515092 => 1, WBInteraction000515093 => 1, WBInteraction000515094 => 1, WBInteraction000515095 => 1,
+    WBInteraction000515096 => 1, WBInteraction000515097 => 1, WBInteraction000515098 => 1, WBInteraction000515099 => 1,
+    WBInteraction000515100 => 1, WBInteraction000515101 => 1, WBInteraction000515102 => 1, WBInteraction000515103 => 1,
+    WBInteraction000515104 => 1, WBInteraction000515105 => 1, WBInteraction000515106 => 1, WBInteraction000515107 => 1,
+    WBInteraction000515108 => 1, WBInteraction000515109 => 1, WBInteraction000515110 => 1, WBInteraction000515111 => 1,
+    WBInteraction000515112 => 1, WBInteraction000515163 => 1, WBInteraction000515164 => 1, WBInteraction000515540 => 1,
+    WBInteraction000515541 => 1, WBInteraction000515542 => 1, WBInteraction000516462 => 1, WBInteraction000516463 => 1,
+    WBInteraction000516464 => 1, WBInteraction000516465 => 1, WBInteraction000516466 => 1, WBInteraction000516467 => 1,
+    WBInteraction000516468 => 1, WBInteraction000516469 => 1, WBInteraction000516470 => 1, WBInteraction000516471 => 1,
+    WBInteraction000516472 => 1, WBInteraction000516473 => 1, WBInteraction000516474 => 1, );
+
+    if ($bad_interactions{$interaction}) { return 1 }
+    else { return 0 }
 }
 
 1;
