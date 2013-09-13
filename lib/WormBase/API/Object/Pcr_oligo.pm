019dc2bb66312719a9d51d47648c570cbccf9db8
Jean-Daniel Wong <jdmswong@wormbase.org>
2013-09-13 16:55:03 +0000

    added extra loop for multiple records #794

--- lib/WormBase/API/Object/Pcr_oligo.pm
+++ lib/WormBase/API/Object/Pcr_oligo.pm
@@ -138,7 +138,7 @@ sub oligos {
 
 sub overlapping_genes {
 	my ($self) = @_;
-    my @results = map { $self->_pack_obj($_) } $self->object->Overlaps_CDS->Gene;
+    my @results = map { $self->_pack_obj($_) } map {$_->Gene} $self->object->Overlaps_CDS;
     
 	return {
 		description => 'Overlapping genes of this ' . $self->_object_class,
