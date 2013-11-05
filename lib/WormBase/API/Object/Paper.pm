31d7fda31b820416508fd9a27e103b4561c8a0d9
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2013-11-05 18:24:57 +0000

    DOI issue - another try * #1959

--- lib/WormBase/API/Object/Paper.pm
+++ lib/WormBase/API/Object/Paper.pm
@@ -330,7 +330,7 @@ sub affiliation {
 sub doi {
     my ($self) = @_;
     my $object = $self->object;
-    "$object" =~ m{^(?:doi.*)?(10\.[^/]+.+)$};
+    $object->Name =~ m{^(?:doi.*)?(10\.[^/]+.+)$};
     return {
         description => 'DOI of publication',
         data		=> $1,
