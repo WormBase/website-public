3b098e81b35a80a2080d6074533c3c3c367d898d
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2013-11-06 20:10:52 +0000

    Nucleotide aligner * Species agnostic * use full name of db * #1646, #1046

--- lib/WormBase/API/Service/nucleotide_aligner.pm
+++ lib/WormBase/API/Service/nucleotide_aligner.pm
@@ -29,23 +29,23 @@ use constant TGT_START => 3;
 use constant TGT_END   => 4;
 
 our %TYPES    = (BLAT_EST_BEST  => [qw(alignment:BLAT_EST_BEST)],
-		BLAT_EST_OTHER  => [qw(alignment:BLAT_EST_OTHER)],
-		BLAT_mRNA_BEST => [qw(alignment:BLAT_mRNA_BEST)],
-		BRIGGSAE   => [qw(waba_alignment)]
-	       );
+        BLAT_EST_OTHER  => [qw(alignment:BLAT_EST_OTHER)],
+        BLAT_mRNA_BEST => [qw(alignment:BLAT_mRNA_BEST)],
+        BRIGGSAE   => [qw(waba_alignment)]
+           );
 our %LABELS   = (
-	     BLAT_EST_BEST  => 'ESTs Aligned with BLAT (best)',
-	     BLAT_EST_OTHER => 'ESTs aligned by BLAT (other)',
-	     BLAT_mRNA_BEST => 'full-length mRNAs Aligned with BLAT (best)',
-	     BRIGGSAE   => 'Briggsae Alignments (WABA)',
-	     );
+         BLAT_EST_BEST  => 'ESTs Aligned with BLAT (best)',
+         BLAT_EST_OTHER => 'ESTs aligned by BLAT (other)',
+         BLAT_mRNA_BEST => 'full-length mRNAs Aligned with BLAT (best)',
+         BRIGGSAE   => 'Briggsae Alignments (WABA)',
+         );
  
 our %TRACKS   = (
-	     BLAT_EST_BEST  => 'ESTB',
-	     BLAT_EST_OTHER => 'ESTO',
-	     BLAT_mRNA_BEST => 'mRNAB',
-	     BRIGGSAE   => 'WABA',
-	     );
+         BLAT_EST_BEST  => 'ESTB',
+         BLAT_EST_OTHER => 'ESTO',
+         BLAT_mRNA_BEST => 'mRNAB',
+         BRIGGSAE   => 'WABA',
+         );
 
 sub index {
    my ($self) = @_;
@@ -60,13 +60,13 @@ sub run {
  
     my @array;
     if($param->{algorithm}) {
-	if (ref $param->{algorithm} eq 'ARRAY' ) {
-	    @array = @{$param->{algorithm}};
-	}else {
-	    push @array, $param->{algorithm};
-	}
+      if (ref $param->{algorithm} eq 'ARRAY' ) {
+          @array = @{$param->{algorithm}};
+      }else {
+          push @array, $param->{algorithm};
+      }
     }else{
-	push @array, DEFAULT_ALGORITHM;
+      push @array, DEFAULT_ALGORITHM;
     }
     $self->algorithms({map {$_=>1} @array});
     
@@ -85,7 +85,7 @@ sub run {
     my $sequence = $service_dbh->fetch(-class => $o->get_document->get_value(0), -name => $o->get_document->get_value(1));
 
 
-    return {msg=>"No such sequence ID known."} unless $sequence  ;
+    return {msg=>"No such sequence ID known."} unless $sequence;
 
     $self->object($sequence);
     $sequence->db->class('Ace::Object::Wormbase');
@@ -106,27 +106,26 @@ sub run {
     my $flip_user  = $param->{"flip"};
     my $user_ragged =  defined $param->{"ragged"} ? $param->{"ragged"} : BLUMENTHAL_FACTOR;
     my $user_override = defined $user_start && defined $user_end && ($user_start || $user_end);
-    my $hash = {	
-		sequence => $sequence_id,
-		size => length($param->{"sequence"})||8,
-		start => $user_start,
-		end => $user_end,
-		flip => 0,
-		factor => $user_ragged,
-		algorithm => $self->algorithms,
-		types	  =>\%TYPES,
-		labels	=> \%LABELS,
-		max_len => TOO_BIG,
+    my $hash = {    
+        sequence => $sequence_id,
+        size => length($param->{"sequence"})||8,
+        start => $user_start,
+        end => $user_end,
+        flip => 0,
+        factor => $user_ragged,
+        algorithm => $self->algorithms,
+        types      =>\%TYPES,
+        labels    => \%LABELS,
+        max_len => TOO_BIG,
       };
     if ($user_override && ($user_end - $user_start > TOO_BIG)) {
-	$hash->{long}=1;
-	return;
+      $hash->{long}=1;
+      return;
     }
     my ($seq) = $dbgff->segment(-name => $sequence,
-			    $user_override ? (-start	=>	$user_start,
-					      -stop	=>	$user_end) : ());
-				       
-    
+                $user_override ? (-start    =>    $user_start,
+                                  -stop    =>    $user_end) : ());
+
     my  @alignments;
 
     foreach (sort keys %{$self->algorithms}){
@@ -143,14 +142,14 @@ sub run {
       my $dna = $target->asDNA;
 
       unless ($dna) {
-	$self->log->debug( "ALIGNER missing target = $target" );
-	$dna = $dbgff->dna($target->name);
+        $self->log->debug( "ALIGNER missing target = $target" );
+        $dna = $dbgff->dna($target->name);
       }
 
       unless ($dna) {
-	push @missing,$target;
-# 	print p({-class=>'error'},"The DNA sequence is missing for $target.\n");
-	next;
+        push @missing,$target;
+  #     print p({-class=>'error'},"The DNA sequence is missing for $target.\n");
+        next;
       }
       $dna{$target} = $dna;
       clean_fasta(\$dna{$target});
@@ -170,13 +169,13 @@ sub run {
     } else {
       $align_start    =  $seq->start;
       $align_start    =  $alignments[0]->start
-	if $alignments[0] && $alignments[0]->start < $seq->start;
+      if $alignments[0] && $alignments[0]->start < $seq->start;
 
       # the same thing applies to the end of the aligned area
       my @reversed_alignments = sort { $b->end <=> $a->end } @alignments;
       $align_end           = $seq->end;
       $align_end = $reversed_alignments[0]->end
-	if $reversed_alignments[0] && $reversed_alignments[0]->end > $seq->end;
+      if $reversed_alignments[0] && $reversed_alignments[0]->end > $seq->end;
     }
 
     $self->log->debug("ALIGNER: $align_start, ...now\n");
@@ -196,8 +195,8 @@ sub run {
     # gene, we want to extend the genomic sequence, so we refetch the DNA
     ($align_start,$align_end) = ($align_end,$align_start) if($align_start > $align_end);
     my ($genomic) = $dbgff->segment(-name  => $sequence,
-				    -start => $align_start,
-				    -end   => $align_end);
+                    -start => $align_start,
+                    -end   => $align_end);
     ##eval{$genomic->absolute(1)};
     # WHAT IS THIS BEING USED FOR?
     my @dnas   = ($genomic->display_name => $genomic->dna);
@@ -221,7 +220,6 @@ sub run {
       push @align_types,$LABELS{$_};
     }
 
-    
   # Link into gbrowse image using the sequence object (a gene object)
     $self->log->debug("ALIGNER: before print_image: $align_start $align_end\n");
     my $gene = $api->fetch({aceclass=> $o->get_document->get_value(0),
@@ -236,10 +234,12 @@ sub run {
     my $flip_format = "Aligner.flip=" . $calculated_flip;
     my $ragged = "Aligner.ragged=". $user_ragged || "Aligner.ragged=BLUMENTHAL_FACTOR";
     my $test =  "$sequence:$start..$end";
-    
-    my $url_root = 'http://www.wormbase.org/tools/genome/gbrowse/c_elegans?';
-    #my $plugin_url = $url_root . "name=$test;plugin=Aligner;plugin_action=Go;label=ESTB;Aligner.upcase=CDS;Aligner.align=ESTB;". $ragged . ";" . $flip_format;
-    my $plugin_url = $url_root . "name=$test;plugin=Aligner;plugin_action=Go;label=ESTB;Aligner.upcase=CDS;Aligner.align=ESTB;";
+
+    my @db = split('_', "" . $gff_dsn->source);
+    my $source = join('_', @db[0..@db-2]);
+
+    my $plugin_url = "http://www.wormbase.org/tools/genome/gbrowse/" . $source . "/?";
+    $plugin_url .= "name=$test;plugin=Aligner;plugin_action=Go;label=ESTB;Aligner.upcase=CDS;Aligner.align=ESTB;";
    
     $plugin_url .= 'Aligner.align=ESTO;' if $self->algorithms->{BLAT_EST_OTHER};
     $plugin_url .= $ragged . ";" . $flip_format;
@@ -247,6 +247,11 @@ sub run {
     $self->log->debug( "ALIGNER: URL: $plugin_url\n");
     my $content = get $plugin_url;
     return {msg=>"Couldn't get $plugin_url" } unless defined $content;
+
+    $self->log->debug($content);
+    if($content =~ m/\<title\>Not Found\<\/title\>/){
+      $content = "Couldn't fetch alignment";
+    }
     $hash->{content}=$content;
  
     return $hash;
