2535c374d5f9d1fe41f79f68f8bb73e99b86946d
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2014-08-08 15:23:47 +0000

    Didn't update all of nucleotide aligner - new xapian fetch

--- lib/WormBase/API/Service/nucleotide_aligner.pm
+++ lib/WormBase/API/Service/nucleotide_aligner.pm
@@ -212,8 +212,8 @@ sub run {
 
     # Link into gbrowse image using the sequence object (a gene object)
     $self->log->debug("ALIGNER: before print_image: $align_start $align_end\n");
-    my $gene = $api->fetch({aceclass=> $doc->get_value(0),
-                          name => $doc->get_value(1)}) or die "$!";
+    my $gene = $api->fetch({aceclass=> $match->{class},
+                          name => $match->{id}) or die "$!";
 
     $hash->{picture} = $gene->genomic_image;
 
