6fe3d12332b8f2f5b531d2e4835747b6bb871eea
Abigail Cabunoc <abigail.cabunoc@oicr.on.ca>
2014-08-08 19:58:22 +0000

    Remove redundant CDS names from ModelMap * Fixes a search problem

--- lib/WormBase/API/ModelMap.pm
+++ lib/WormBase/API/ModelMap.pm
@@ -35,7 +35,7 @@ BEGIN {
         class => {     # HAS DEFAULT
             Pcr_oligo => [qw(PCR_product Oligo_set Oligo)],
             Person    => [qw(Person Author)],
-            Cds       => [qw(CDS cds)],
+            Cds       => 'CDS',
             Rnai      => 'RNAi',
             Disease   => 'DO_term',
             Go_term   => 'GO_term',
