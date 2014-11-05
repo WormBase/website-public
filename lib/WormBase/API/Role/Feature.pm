7d5bd03cedd27176615acf72aeebdaa276a9b182
Sibyl <sibyl@wormbase.org>
2014-11-05 17:32:21 +0000

    link transcription factor to search #3267

--- lib/WormBase/API/Role/Feature.pm
+++ lib/WormBase/API/Role/Feature.pm
@@ -49,7 +49,8 @@ sub _get_feature_associations {
         my $description = $feature->Description;
         (my $method = $feature->Method) =~ s/_/ /g;
         my @bound_by = map { $self->_pack_obj($_) } $feature->Bound_by_product_of;
-        my $tf = $feature->Transcription_factor;
+        my $tf = $self->_pack_obj($feature->Transcription_factor);
+
         my @interactions = map { $self->_pack_obj($_) } $feature->Associated_with_Interaction;
 
         my @expr_pattern = map {
@@ -67,7 +68,7 @@ sub _get_feature_associations {
             interaction => \@interactions,
             expr_pattern => \@expr_pattern,
             bound_by => \@bound_by,
-            tf => $tf && "$tf"
+            tf => $tf
         };
     }
     return @data;
