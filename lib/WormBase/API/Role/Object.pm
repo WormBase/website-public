9e4c9bfc15449d61320305cd6a2233c6c3c5649b
Sibyl <sibyl@wormbase.org>
2015-07-09 18:34:30 +0000

    type filter for _fetch_gff_gene #4016

--- lib/WormBase/API/Role/Object.pm
+++ lib/WormBase/API/Role/Object.pm
@@ -2277,9 +2277,13 @@ sub _fetch_gff_gene {
     my $GFF = $self->gff_dsn() or die "Cannot connect to GFF database, host:" . $self->host; # should probably log this?
 
     my @feats = $GFF->get_features_by_name("$transcript");
-    if (my ($mrna) = grep {$_->{'type'} eq 'mRNA'} @feats) {
-	return $mrna;
-    }
+    my $type = $self->classification->{data}->{type};
+
+    ($trans) = grep {
+        $_->{'type'} eq 'mRNA' || $_->{'type'} =~ $type;
+    } @feats;
+
+    return $trans;
 }
 
 #----------------------------------------------------------------------
