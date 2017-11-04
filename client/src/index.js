392a1586ad3ccd0941f988d1984d6eb4772c9269
Sibyl <sibyl@wormbase.org>
2017-11-04 04:10:25 +0000

    remove React from bundle since we are not using React yet

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 import './index.css';
 import './legacy/css/jquery-ui.min.css';
 import './legacy/css/main.css';
 import './legacy/js/wormbase.js';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
