acdaca0b4cc49e7336feb164e0f4f1062b3e6396
Sibyl <sibyl@wormbase.org>
2017-11-06 18:26:25 +0000

    avoid symlinking to assets in root/

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 //import React from 'react';
 //import ReactDOM from 'react-dom';
 import './index.css';
-import './legacy/css/jquery-ui.min.css';
-import './legacy/css/main.css';
-import './legacy/js/wormbase.js';
+import '../../root/css/jquery-ui.min.css';
+import '../../root/css/main.css';
+import '../../root/js/wormbase.js';
 //import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
