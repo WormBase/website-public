9d48d045b14d35f8441bed0302b4ee3296fa5f15
Sibyl <sibyl@wormbase.org>
2017-11-07 17:30:45 +0000

    remove un-used code from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,4 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
 import './index.css';
 import '../../root/css/jquery-ui.min.css';
 import '../../root/css/main.css';
 import '../../root/js/wormbase.js';
-//import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
