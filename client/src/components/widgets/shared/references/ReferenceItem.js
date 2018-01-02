da74b51b26396653f490182ac12d210ba14c396d
Sibyl <sibyl@wormbase.org>
2018-01-02 23:09:27 +0000

    style reference item

--- client/src/components/widgets/shared/references/ReferenceItem.js
+++ client/src/components/widgets/shared/references/ReferenceItem.js
@@ -1,23 +1,77 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import classNames from 'classnames';
+import Link from '../../../Link';
 
-export default class ReferenceItem extends Component {
+class ReferenceItem extends Component {
   static propTypes = {
+    classes: PropTypes.object.isRequired,
     data: PropTypes.shape({
       name: PropTypes.shape({
-        id: PropTypes.string.isRequired
+        id: PropTypes.string.isRequired,
+        "class": PropTypes.string.isRequired,
       }).isRequired,
+      author: PropTypes.array,
     }),
   };
 
   render() {
-    const {data} = this.props;
+    const {data, classes} = this.props;
     return (
-      <div>
-        {
-          JSON.stringify(data)
-        }
+      <div className={classes.referenceItem}>
+        <Link
+          id={data.name.id} label={data.title[0]} class={data.name.class}
+          classes={{
+            linkLabel: classes.title,
+          }}
+        />
+        <div>
+          {
+            data.author.map((tag, index) => (
+              <span>
+                {index !== 0 ? <span>, </span> : null}
+                <Link {...tag} class="person" key={tag.id} />
+              </span>
+            ))
+          }
+        </div>
+        <div>
+          [{data.journal[0]}, {data.year}]
+        </div>
+        <div className={classNames([classes.abstract, classes.fade])}>
+          <p>{data.abstract[0]}</p>
+        </div>
       </div>
     );
   }
 }
+
+const styles = (theme) => ({
+  referenceItem: {
+    paddingTop: theme.spacing.unit / 2,
+
+  },
+  title: {
+    textDecoration: 'underline',
+  },
+  abstract: {
+    height: '3.8em',
+    overflow: 'hidden',
+  },
+  fade: {
+    position: 'relative',
+    "&:after": {
+      content: '""',
+      textAlign: 'right',
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      width: '100%',
+      height: '1.5em',
+      background: 'linear-gradient(to bottom, transparent, rgba(255,255,255, 0.7))',
+    },
+  },
+});
+
+export default withStyles(styles, {withTheme: true})(ReferenceItem);
