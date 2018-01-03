9b4644e93b24235a6d0fd2e0098178885afd2292
Sibyl <sibyl@wormbase.org>
2018-01-03 19:53:03 +0000

    add pagination to references list

---
+++ client/src/components/Pagination.js
@@ -0,0 +1,3 @@
+import { TablePagination } from 'material-ui/Table';
+
+export default TablePagination;
--- client/src/components/widgets/shared/references/ReferenceList.js
+++ client/src/components/widgets/shared/references/ReferenceList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Pagination from '../../../Pagination';
 
 export default class ReferenceList extends Component {
   static propTypes = {
@@ -7,13 +8,47 @@ export default class ReferenceList extends Component {
     children: PropTypes.func.isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+      rowsPerPage: 5,
+    };
+  }
+
+  handleChangePage = (event, page) => {
+    this.setState({
+      page: page,
+    });
+  }
+
+  handleChangeRowsPerPage = (event) => {
+    this.setState({
+      rowsPerPage: event.target.value
+    });
+  }
   render() {
-    const pageData = this.props.data;
+    const {data} = this.props;
+    const {page, rowsPerPage} = this.state;
+    const pageData = data.slice(page * rowsPerPage, Math.min(data.count, page * rowsPerPage + rowsPerPage));
     return (
       <div>
         {
           this.props.children(pageData)
         }
+        <Pagination
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            'aria-label': 'Previous Page',
+          }}
+          nextIconButtonProps={{
+            'aria-label': 'Next Page',
+          }}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
       </div>
     );
   }
