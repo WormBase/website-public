1681ebc863e141633c28a2dccdf0344f28b38ace
Sibyl <sibyl@wormbase.org>
2018-01-25 19:47:43 +0000

    fix interaction graph on wbprocess page #6134

--- client/src/components/InteractionGraph/InteractionGraphDataProvider.js
+++ client/src/components/InteractionGraph/InteractionGraphDataProvider.js
@@ -3,16 +3,22 @@ import React, { Component } from 'react';
 export default class InteractionGraphDataProvider extends Component {
 
   render() {
-    console.log(this.props.data);
+    // console.log(this.props.data);
     const data = this.props.data;
     const error = this.props.error;
+    const edges = data.data.edges_all || data.data.edges;
+    const nodes =  data.data.nodes || edges.reduce((result, edge) => {
+      result[edge.affected.id] = edge.affected;
+      result[edge.effector.id] = edge.effector;
+      return result;
+    }, {});
     return (
       <div>
         {
           error || (
             data ? this.props.children({
-              interactorMap: data.data.nodes,
-              interactions: data.data.edges_all || data.data.edges
+              interactorMap: nodes,
+              interactions: edges,
             }) : <span>Loading...</span>
           )
         }
