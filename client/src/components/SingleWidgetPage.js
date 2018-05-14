d2ef06642bc8e70793a79f9b79c695b58fe13edc
Sibyl <sibyl@wormbase.org>
2018-05-14 20:32:20 +0000

    extract out withInnerHtmlFromUrl

--- client/src/components/SingleWidgetPage.js
+++ client/src/components/SingleWidgetPage.js
@@ -1,44 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import withInnerHtmlFromUrl from './withInnerHtmlFromUrl';
 
 class SingleWidgetPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      html: null,
-    };
-  }
-
-  componentDidMount() {
-    fetch(this.props.widgetUrl).then((response) => {
-      return response.text();
-    }).then((widgetHtmlRaw) => {
-      const re = /<script.*?>((.|\n|\r)+?)<\/script>/gi;
-      const scripts = [];
-      const widgetHtml = widgetHtmlRaw || `<script> console.log('zzzz'); </script>`;
-      var match = re.exec(widgetHtml);
-      //console.log(widgetHtml);
-      while (match) {
-        console.log(match);
-        scripts.push(match[1]);
-        match = re.exec(widgetHtml);
-      }
-      //console.log(scripts);
-
-      this.setState({
-        html: widgetHtml,
-      }, () => {
-        scripts.forEach((script) => {
-          console.log(script);
-          window.eval(script);
-        });
-      });
-    });
-  }
-
   render() {
+    const Widget = withInnerHtmlFromUrl('div', this.props.widgetUrl);
     return (
-      <div id={`${this.props.widgetConf.name}-content`} className="content" style={{position: "relative", paddingTop: '2em',}} dangerouslySetInnerHTML={{__html: this.state.html}} />
+      <Widget id={`${this.props.widgetConf.name}-content`} className="content" style={{position: "relative", paddingTop: '2em',}} />
     );
   }
 }
---
+++ client/src/components/withInnerHtmlFromUrl.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default function withInnerHtmlFromUrl(WrappedComponent, url) {
+  class WithInnerHtml extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        html: null,
+      };
+    }
+
+    componentDidMount() {
+      fetch(url).then((response) => {
+        return response.text();
+      }).then((htmlRaw) => {
+        const re = /<script.*?>((.|\n|\r)+?)<\/script>/gi;
+        const scripts = [];
+        const html = htmlRaw || `<script> console.log('zzzz'); </script>`;
+        var match = re.exec(html);
+        //console.log(html);
+        while (match) {
+          console.log(match);
+          scripts.push(match[1]);
+          match = re.exec(html);
+        }
+        //console.log(scripts);
+
+        this.setState({
+          html: html,
+        }, () => {
+          scripts.forEach((script) => {
+            console.log(script);
+            window.eval(script);
+          });
+        });
+      });
+    }
+
+    render() {
+      return (
+        <WrappedComponent
+          dangerouslySetInnerHTML={{__html: this.state.html}}
+          {...this.props} />
+      );
+    }
+  }
+
+
+  return WithInnerHtml;
+};
