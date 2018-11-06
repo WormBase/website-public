723e0a0ca4bc5c9e64c14c42296ed61039311b7b
Sibyl <sibyl@wormbase.org>
2018-11-06 23:30:38 +0000

    check in jenkins deployment script to git

---
+++ jenkins-ci-deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+export APP="staging"
+
+#export PERL5LIB="/usr/local/wormbase/extlib/lib/perl5:/usr/local/wormbase/extlib/lib/perl5/x86_64-linux-gnu-thread-multi:$ENV{APP_HOME}/lib:$PERL5LIB"
+#export MODULEBUILDRC="/usr/local/wormbase/extlib/.modulebuildrc"
+#export PERL_MM_OPT="INSTALL_BASE=/usr/local/wormbase/extlib"
+#export PATH="/usr/local/wormbase/extlib/bin:$PATH"
+
+export PATH="${HOME}/.local/bin:$PATH";
+
+#perl Makefile.PL
+
+# Output of unit tests; path cannot contain spaces:
+export TEST_LOG=/tmp/jenkins_test_log_`date '+%s'`.log
+
+# build container
+make build
+
+# Unit tests: WormBase API and REST API
+#make build-run-test | tee $TEST_LOG
+
+# Check if there was an error reported by the unit tests, return non-zero if that is the case
+if [ "`grep -E '^[ ]*not ok [0-9]+ ' $TEST_LOG`" != '' ] ; then
+    rm -f $TEST_LOG
+    exit 1
+fi
+
+rm -f $TEST_LOG
+
+# tag container
+docker tag wormbase/website:latest 357210185381.dkr.ecr.us-east-1.amazonaws.com/wormbase/website:latest
+
+# push containers to AWS ECR
+$(aws ecr get-login --no-include-email --region us-east-1)
+docker push 357210185381.dkr.ecr.us-east-1.amazonaws.com/wormbase/website:latest
+
+# deploy container
+eb use wormbase-website-staging
+make eb-setenv
+eb deploy --region us-east-1
