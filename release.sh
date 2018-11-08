73bce6c1604a5d399815076fbc2ade4afdd0257b
Sibyl <sibyl@wormbase.org>
2018-11-08 21:55:55 +0000

    echo commands in release.sh

--- release.sh
+++ release.sh
@@ -2,23 +2,25 @@
 
 # ensure $VERSION is set
 if [ -z "$VERSION" ] ; then
-    echo "Environment variable VERSION must be set"
+    echo "Error: environment variable VERSION must be set"
     exit 1
 
 fi
 
 # ensure no uncommited change
 if ! git diff --quiet HEAD -- ; then
-    echo "All changes needs to be committed to git"
+    echo "Error: all changes needs to be committed to git"
     exit 1
 fi
 
 # check if a release by this tag already exists
 git fetch --tags
 if [ $(git tag -l "$VERSION") ]; then
-    echo "Tag $VERSION already exists. Please use another one."
+    echo "Error: tag $VERSION already exists. Please use another one."
     exit 1
 fi
+
+set -x #echo on
 
 # update Version in Dockerrun.aws.json
 sed -i -r 's/website:[^"]+/website:'"$VERSION"'/g' Dockerrun.aws.json
@@ -44,5 +46,7 @@ docker push 357210185381.dkr.ecr.us-east-1.amazonaws.com/wormbase/website:$VERSI
 sed -i -r 's/website:[^"]+/website:'"latest"'/g' Dockerrun.aws.json
 git add Dockerrun.aws.json
 git commit -m "post-release cleanup"
+
+set -x #echo off
 
 echo "Successfully created release $VERSION"
