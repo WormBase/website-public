f9ee236dc1dbefd957aac300901670c2eafbb752
Sibyl <sibyl@wormbase.org>
2019-01-03 20:13:02 +0000

    avoid ripple effect in CardActionArea

---
+++ client/src/components/CardActionArea.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MuiCardActionArea from '@material-ui/core/CardActionArea';
+
+export default (props) => (
+  <MuiCardActionArea {...props} disableRipple={true} />
+);
