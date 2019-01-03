2e2d2bfb164491d4ace8ff0d5d18cb1d0803e9e9
Sibyl <sibyl@wormbase.org>
2019-01-03 20:18:50 +0000

    disable ripple effect on tab

--- client/src/components/Tab.js
+++ client/src/components/Tab.js
@@ -1,1 +1,6 @@
-export { default } from '@material-ui/core/Tab';
+import React from 'react';
+import MuiTab from '@material-ui/core/Tab';
+
+export default (props) => (
+  <MuiTab {...props} disableRipple={true} />
+);
