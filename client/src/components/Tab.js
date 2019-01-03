bf074d4c428077234716a78bba9a2b8d299ff2c5
Sibyl <sibyl@wormbase.org>
2019-01-03 21:46:22 +0000

    add hover indicator to tab

--- client/src/components/Tab.js
+++ client/src/components/Tab.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import MuiTab from '@material-ui/core/Tab';
+import { withStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
-export default (props) => (
-  <MuiTab {...props} disableRipple={true} />
-);
+const Tab = (props) => {
+  return (
+    <MuiTab
+      {...props}
+      disableRipple={true}
+    />
+  );
+};
+
+const styles = (theme) => ({
+  root: {
+    '&:hover': {
+      textDecoration: 'none',
+      backgroundColor: fade(theme.palette.text.primary, theme.palette.action.hoverOpacity),
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        backgroundColor: 'transparent',
+      },
+      '&$disabled': {
+        backgroundColor: 'transparent',
+      },
+    },
+  },
+});
+
+export default withStyles(styles, {withTheme: true})(Tab);
