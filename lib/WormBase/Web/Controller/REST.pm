48e3b75d045e92715387a05fbd53749740173c39
Sibyl <sibyl@wormbase.org>
2019-01-04 18:39:57 +0000

    capture SendEmail error in log

--- lib/WormBase/Web/Controller/REST.pm
+++ lib/WormBase/Web/Controller/REST.pm
@@ -487,7 +487,7 @@ sub send_email :Private {
 
     $c->log->debug(" AWS SES input $send_email_cli_input_json");
 
-    my $out = `aws --region us-east-1 ses send-email --from $from_address --cli-input-json '$send_email_cli_input_json'`;
+    my $out = `aws --region us-east-1 ses send-email --from $from_address --cli-input-json '$send_email_cli_input_json' 2>&1`;
 
     $c->log->debug(" AWS SES response $out");
 
