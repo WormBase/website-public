634978a6b927fb08f0b865ba79b5663f8435598b
Sibyl <sibyl@wormbase.org>
2019-04-11 19:10:51 +0000

    display one of the transcript sequences

--- client/src/components/Sequence/TranscriptSequencesCard.js
+++ client/src/components/Sequence/TranscriptSequencesCard.js
@@ -2,18 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SequenceCard from './SequenceCard';
 
-const TranscriptSequenceCard = (props) => {
+function resolveStrand(sequenceContext = {}) {
+  if (sequenceContext.sequence_strand) {
+    return sequenceContext[`${sequenceContext.sequence_strand}-strand`];
+  } else {
+    return null;
+  };
+}
+
+function TranscriptSequenceCard(props) {
   const {
     wbId,
     proteinSequence,
+    splicedSequenceContext: splicedSequenceContextRaw,
+    unsplicedSequenceContext: unsplicedSequenceContextRaw,
   } = props;
+  const splicedSequenceContext = resolveStrand(splicedSequenceContextRaw);
+  const unsplicedSequenceContext = resolveStrand(unsplicedSequenceContextRaw);
   return (
     <div>
       <SequenceCard
-        title={`Conceptual translation ${proteinSequence.sequence.length}aa`}
+        title={`Spliced ${splicedSequenceContext.sequence.length}aa`}
         downloadFileName={`conceptual_translation_${wbId}.fasta`}
-        sequence={proteinSequence.sequence}
-        />
+        sequence={splicedSequenceContext.sequence}
+        features={[]}
+      />
+      {
+        proteinSequence ?
+          <SequenceCard
+            title={`Conceptual translation ${proteinSequence.sequence.length}aa`}
+            downloadFileName={`conceptual_translation_${wbId}.fasta`}
+            sequence={proteinSequence.sequence}
+          /> : null
+      }
+
     </div>
   );
 };
