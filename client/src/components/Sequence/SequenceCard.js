8f77e458fe959ce24f30970bb08b3532f983ad34
Sibyl <sibyl@wormbase.org>
2019-04-22 20:52:53 +0000

    set initial expand of sequence card

--- client/src/components/Sequence/SequenceCard.js
+++ client/src/components/Sequence/SequenceCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -14,68 +14,58 @@ import CardActionArea from '../CardActionArea';
 import Tooltip from '../Tooltip';
 
 
-class SequenceCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expand: true,
-    };
-  }
+const  SequenceCard = (props) => {
 
-  handleExpandToggle = () => {
-    this.setState((prevState) => ({
-      expand: !prevState.expand,
-    }));
-  }
+  const {
+    classes,
+    className,
+    sequence = '',
+    title = 'Sequence',
+    downloadFileName = 'sequence.fasta',
+    initialExpand = true,
+    ...sequenceProps,
+  } = props;
 
-  render() {
-    const {
-      classes,
-      className,
-      sequence = '',
-      title = 'Sequence',
-      downloadFileName = 'sequence.fasta',
-      ...sequenceProps,
-    } = this.props;
-    const {expand} = this.state;
-    return sequence ? (
-      <Card elevation={0} className={className}>
-        <CardActions className={classes.actions}>
-          <Tooltip title={expand ? 'Hide sequence' : 'Show sequence'}>
-            <CardActionArea className={classes.expandToggleAction} onClick={this.handleExpandToggle}>
+  const [expand, setExpand] = useState(initialExpand);
 
-              {
-                expand ? <ExpandLessIcon /> : <ExpandMoreIcon />
-              }
+  return sequence ? (
+    <Card elevation={0} className={className}>
+      <CardActions className={classes.actions}>
+        <Tooltip title={expand ? 'Hide sequence' : 'Show sequence'}>
+          <CardActionArea className={classes.expandToggleAction} onClick={() => setExpand(!expand)}>
 
-              <h5 className={classes.title}>{title}</h5>
-            </CardActionArea>
-          </Tooltip>
-          <Tooltip title="Save to file">
-            <DownloadButton
-              fileName={downloadFileName}
-              renderer={(props) => (
-                <Button variant="outlined" {...props}>
-                  Download
-                  <SaveIcon />
-                </Button>
-              )}
-              contentFunc={() => `> ${title}\r\n${sequence}` }
-            />
-          </Tooltip>
-          {/* <Tooltip title="Copy to clipboard">
-              <IconButton >
-              <CopyIcon />
-              </IconButton>
-              </Tooltip> */}
-        </CardActions>
-        {
-          expand ? <Sequence title={title} sequence={sequence} {...sequenceProps} /> : null
-        }
-      </Card>
-    ) : null;
-  }
-}
+            {
+              expand ? <ExpandLessIcon /> : <ExpandMoreIcon />
+            }
+
+            <h5 className={classes.title}>{title}</h5>
+          </CardActionArea>
+        </Tooltip>
+        <Tooltip title="Save to file">
+          <DownloadButton
+            fileName={downloadFileName}
+            renderer={(props) => (
+              <Button variant="outlined" {...props}>
+                Download
+                <SaveIcon />
+              </Button>
+            )}
+            contentFunc={() => `> ${title}\r\n${sequence}` }
+          />
+        </Tooltip>
+        {/* <Tooltip title="Copy to clipboard">
+           <IconButton >
+           <CopyIcon />
+           </IconButton>
+           </Tooltip> */}
+      </CardActions>
+      {
+        expand ? <Sequence title={title} sequence={sequence} {...sequenceProps} /> : null
+      }
+    </Card>
+  ) : null;
+
+};
 
 SequenceCard.propTypes = {
   classes: PropTypes.object.isRequired,
