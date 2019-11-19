c5e4f4b1d09388caa173952c556a585f7fe718b9
Sibyl <sibyl@wormbase.org>
2019-11-19 22:55:08 +0000

    analyze with intermine

--- client/src/components/BrowseCollection/index.js
+++ client/src/components/BrowseCollection/index.js
@@ -15,10 +15,49 @@ export default function BrowseCollection({
 }) {
   const [isOpen, setOpen] = useState(false);
   const handleToggle = () => setOpen((prevIsOpen) => !prevIsOpen);
+
   const toText = useCallback(
     () => collection.map(({ id, label }) => `${label}\t${id}`).join('\n'),
     [collection]
   );
+
+  const toWormMine = useCallback(() => {
+    // modiied from code from SGD https://github.com/yeastgenome/SGDFrontend
+    function post_to_wormmine(bioent_ids) {
+      // The rest of this code assumes you are not using a library.
+      // It can be made less wordy if you use one.
+      const form = document.createElement('form');
+      form.setAttribute('method', 'post');
+      form.setAttribute(
+        'action',
+        'http://intermine.wormbase.org/tools/wormmine/buildBag.do'
+      );
+
+      const cinp = document.createElement('input');
+      cinp.setAttribute('type', 'hidden');
+      cinp.setAttribute('name', 'type');
+      cinp.setAttribute('value', 'Gene');
+      form.appendChild(cinp);
+
+      const vinp = document.createElement('input');
+      vinp.setAttribute('type', 'hidden');
+      vinp.setAttribute('name', 'extraFieldValue');
+      vinp.setAttribute('value', 'C. elegans');
+      form.appendChild(vinp);
+
+      const hiddenField = document.createElement('input');
+      hiddenField.setAttribute('type', 'hidden');
+      hiddenField.setAttribute('name', 'text');
+      hiddenField.setAttribute('value', bioent_ids);
+      hiddenField.id = 'data';
+      form.appendChild(hiddenField);
+
+      document.body.appendChild(form);
+      form.submit();
+    }
+    post_to_wormmine(collection.map(({ id }) => id));
+  }, [collection]);
+
   return (
     <React.Fragment>
       {renderButton({
@@ -31,7 +70,7 @@ export default function BrowseCollection({
         <DialogTitle>{title}</DialogTitle>
         <div>
           <DownloadButton contentFunc={toText} />
-          <Button variant={'outlined'}>Analyze</Button>
+          <Button onClick={toWormMine}>Analyze</Button>
         </div>
         <DialogContent>
           <ul>
