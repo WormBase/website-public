95072ce4b51f4a91204cae8f07f636e234e12da6
Sibyl <sibyl@wormbase.org>
2019-11-22 20:23:09 +0000

    improve layout of venn diagram

--- client/src/components/InteractorVennDiagram/index.js
+++ client/src/components/InteractorVennDiagram/index.js
@@ -2,12 +2,15 @@ import React, { useRef, useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import Badge from '@material-ui/core/Badge';
 import Typography from '@material-ui/core/Typography';
+import PageViewIcon from '@material-ui/icons/Pageview';
+import ClearIcon from '@material-ui/icons/Clear';
+import { withStyles } from '@material-ui/core/styles';
 import BrowseCollection from '../BrowseCollection';
 import Button from '../Button';
 import draw from './draw';
 import { subsets, isSuperSet } from './utils';
 
-export default function InteractorVennDiagram({ data = [] }) {
+function InteractorVennDiagram({ data = [], classes = {} }) {
   const d3Element = useRef();
   const venn = useRef({});
   const [selections, setSelections] = useState([]);
@@ -70,30 +73,37 @@ export default function InteractorVennDiagram({ data = [] }) {
   }, [data, selections]);
 
   return (
-    <div>
-      <BrowseCollection
-        collection={selectedInteractors}
-        renderButton={(props) => (
-          <Badge
-            color="secondary"
-            badgeContent={selectedInteractors.length}
-            invisible={!selectedInteractors.length}
-          >
-            <Button {...props}>Browse selected</Button>
-          </Badge>
-        )}
-      />
-      {selectedInteractors.length ? (
-        <Button variant={'outlined'} onClick={venn.current.clearSelection}>
-          Clear selection
+    <div className={classes.root}>
+      <div className={classes.toolbar}>
+        <BrowseCollection
+          collection={selectedInteractors}
+          renderButton={(props) => (
+            <Badge
+              color="secondary"
+              badgeContent={selectedInteractors.length}
+              invisible={!selectedInteractors.length}
+            >
+              <Button {...props}>
+                Browse selection <PageViewIcon />
+              </Button>
+            </Badge>
+          )}
+        />
+        <Button
+          variant={'outlined'}
+          onClick={venn.current.clearSelection}
+          disabled={!selectedInteractors.length}
+        >
+          Clear <ClearIcon />
         </Button>
-      ) : null}
-      <div ref={d3Element}>Place holder for venn diagram </div>
+      </div>
+      <div ref={d3Element} />
     </div>
   );
 }
 
 InteractorVennDiagram.propTypes = {
+  classes: PropTypes.object.isRequired,
   data: PropTypes.arrayOf(
     PropTypes.shape({
       types: PropTypes.arrayOf(PropTypes.string),
@@ -103,3 +113,19 @@ InteractorVennDiagram.propTypes = {
     })
   ),
 };
+
+const styles = (theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: 600,
+  },
+  toolbar: {
+    '& > *': {
+      marginRight: theme.spacing.unit / 2,
+    },
+  },
+});
+
+export default withStyles(styles)(InteractorVennDiagram);
