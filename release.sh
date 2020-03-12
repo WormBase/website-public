752a6a748788a7f77f9aea551f0d1e4777823204
Sibyl <sibyl@wormbase.org>
2020-03-12 16:13:44 -0400

    Revert "allow make local to run without ECR permission" Due to 36dc92942c0752c7cc054b50e7e30fa8fb8ceda7
    This reverts commit 45d3d78a6e98ca375cd77fd28646cf1f97a6714f.

--- release.sh
+++ release.sh
@@ -23,12 +23,12 @@ set -x #echo on
 
 # update Version in Dockerrun.aws.json and docker-compose.yml
 sed -i -r 's/website:[^"]+/website:'"$VERSION"'/g' Dockerrun.aws.json
-sed -i -r 's/website:[^"]+/website:'"$VERSION"'/g' docker-compose.prod.yml
+sed -i -r 's/website:[^"]+/website:'"$VERSION"'/g' docker-compose.yml
 
 # release commit
 git diff
 git add Dockerrun.aws.json
-git add docker-compose.prod.yml
+git add docker-compose.yml
 git commit -m "release $VERSION"
 git tag "$VERSION"
 git push origin "$VERSION"
