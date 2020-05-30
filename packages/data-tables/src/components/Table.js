5f1580d9e6f9d1e80fe6f9a49b412913b6081ad1
sthtnr <sthtnr@gmail.com>
2020-05-30 21:00:07 +0900

    Set example component and story

---
+++ packages/data-tables/src/components/Table.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div>
+      <h1>hoge</h1>
+      <h2>fuga</h2>
+    </div>
+  )
+}
+
+export default Table
---
+++ packages/data-tables/src/components/Table.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Table from './Table'
+
+export default {
+  component: Table,
+  title: 'Table',
+}
+
+export const Default = () => <Table />
