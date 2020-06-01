6a88daecc23392f8855d28d8d98e8d1c3f108595
sthtnr <sthtnr@gmail.com>
2020-06-01 23:06:27 +0900

    Enables fetching data from cross origin

--- packages/data-tables/src/components/Table.js
+++ packages/data-tables/src/components/Table.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Table = () => {
+  const [tableData, setTableData] = useState([])
+
+  const proxyUrl = 'https://calm-reaches-60051.herokuapp.com/'
+  const targetUrl =
+    'http://rest.wormbase.org/rest/widget/gene/WBGene00000904/location'
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  const loadData = async () => {
+    const res = await fetch(proxyUrl + targetUrl)
+    const data = await res.json()
+    setTableData(JSON.stringify(data))
+  }
+
   return (
     <div>
       <h1>hoge</h1>
       <h2>fuga</h2>
+      {tableData}
     </div>
   )
 }
