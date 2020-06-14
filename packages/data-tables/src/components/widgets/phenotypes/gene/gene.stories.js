dbc6590d9dcb7830f033326e143b03a0ded8d505
sthtnr <sthtnr@gmail.com>
2020-06-14 17:23:01 +0900

    Refactor data loading implementation to be nice

--- packages/data-tables/src/components/widgets/phenotypes/gene/gene.stories.js
+++ packages/data-tables/src/components/widgets/phenotypes/gene/gene.stories.js
@@ -4,9 +4,17 @@ import PhenoBI from './PhenotypeByInteraction'
 import PhenoNO from './PhenotypeNotObserved'
 import DrivesOE from './DrivesOverexpression'
 
-const targetUrl = {
-  daf8: 'http://rest.wormbase.org/rest/field/gene/WBGene00000904/phenotype',
-  daf16: 'http://rest.wormbase.org/rest/field/gene/WBGene00000912/phenotype',
+const target = {
+  WBid: {
+    daf8: 'WBGene00000904',
+    daf16: 'WBGene00000912',
+  },
+  tableType: {
+    pheno: 'phenotype',
+    phenoNO: 'phenotype_not_observed',
+    phenoBI: 'phenotype_by_interaction',
+    drivesOE: 'drives_overexpression',
+  },
 }
 
 export default {
@@ -17,8 +25,13 @@ export default {
   title: 'Table|Widgets/Phenotypes/Gene page',
 }
 
-export const Phenotype_daf8 = () => <Pheno targetUrl={targetUrl.daf8} />
-export const Phenotype_daf16 = () => <Pheno targetUrl={targetUrl.daf16} />
+export const Phenotype_daf8 = () => (
+  <Pheno WBid={target.WBid.daf8} tableType={target.tableType.pheno} />
+)
+export const Phenotype_daf16 = () => (
+  <Pheno WBid={target.WBid.daf16} tableType={target.tableType.pheno} />
+)
+
 export const PhenotypeByInteraction = () => <PhenoBI />
 export const PhenotypeNotObserved = () => <PhenoNO />
 export const DrivesOverexpression = () => <DrivesOE />
--- packages/data-tables/src/services/loadData.js
+++ packages/data-tables/src/services/loadData.js
@@ -1,8 +1,10 @@
-const loadData = async (targetUrl) => {
+const loadData = async (WBid, tableType) => {
   const proxyUrl = 'https://calm-reaches-60051.herokuapp.com/'
+  const targetUrl = `http://rest.wormbase.org/rest/field/gene/${WBid}/${tableType}`
   const res = await fetch(proxyUrl + targetUrl)
   const json = await res.json()
-  return json
+  const jsonSpecific = await json[`${tableType}`]
+  return jsonSpecific
 }
 
 export default loadData
