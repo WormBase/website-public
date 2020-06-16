700f9ce3977c565b15306fa51e41896b0dcd85c0
sthtnr <sthtnr@gmail.com>
2020-06-16 21:38:01 +0900

    Add case-insensitive sort function

--- packages/table-storybook/src/components/widgets/phenotypes/gene/Phenotype.js
+++ packages/table-storybook/src/components/widgets/phenotypes/gene/Phenotype.js
@@ -88,7 +88,7 @@ const Phenotype = ({ WBid, tableType }) => {
   const defaultColumn = useMemo(
     () => ({
       filter: 'text', // Default. Used builtin 'text' filter.
-      sortType: 'alphanumeric', // Default. Used builtin 'alphanumeric' sortType.
+      sortType: 'caseInsensitiveAlphaNumeric',
       Filter: defaultColumnFilter,
       minWidth: 120,
       width: 180,
@@ -123,6 +123,71 @@ const Phenotype = ({ WBid, tableType }) => {
           : comparisonStandardOfRowA < comparisonStandardOfRowB
           ? -1
           : 0
+      },
+      caseInsensitiveAlphaNumeric: (rowA, rowB, columnId) => {
+        const getRowValueByColumnID = (row, columnId) => row.values[columnId]
+        const toString = (a) => {
+          if (typeof a === 'number') {
+            if (isNaN(a) || a === Infinity || a === -Infinity) {
+              return ''
+            }
+            return String(a)
+          }
+          if (typeof a === 'string') {
+            return a
+          }
+          return ''
+        }
+        const reSplitAlphaNumeric = /([0-9]+)/gm
+
+        let a = getRowValueByColumnID(rowA, columnId)
+        let b = getRowValueByColumnID(rowB, columnId)
+        // Force to strings (or "" for unsupported types)
+        // And lowercase to accomplish insensitive sort
+        a = toString(a).toLowerCase()
+        b = toString(b).toLowerCase()
+
+        // Split on number groups, but keep the delimiter
+        // Then remove falsey split values
+        a = a.split(reSplitAlphaNumeric).filter(Boolean)
+        b = b.split(reSplitAlphaNumeric).filter(Boolean)
+
+        // While
+        while (a.length && b.length) {
+          let aa = a.shift()
+          let bb = b.shift()
+
+          const an = parseInt(aa, 10)
+          const bn = parseInt(bb, 10)
+
+          const combo = [an, bn].sort()
+
+          // Both are string
+          if (isNaN(combo[0])) {
+            if (aa > bb) {
+              return 1
+            }
+            if (bb > aa) {
+              return -1
+            }
+            continue
+          }
+
+          // One is a string, one is a number
+          if (isNaN(combo[1])) {
+            return isNaN(an) ? -1 : 1
+          }
+
+          // Both are numbers
+          if (an > bn) {
+            return 1
+          }
+          if (bn > an) {
+            return -1
+          }
+        }
+
+        return a.length - b.length
       },
     }),
     []
