26dbada2c7f579bf449acdd44efb39aff1214d41
Sibyl <sibyl@wormbase.org>
2020-06-25 17:00:00 -0400

    not bundle fs with webpack

--- sites/notebooks/next.config.js
+++ sites/notebooks/next.config.js
@@ -6,5 +6,15 @@ module.exports = {
 		REPO_FULL_NAME: process.env.REPO_FULL_NAME,
 		BASE_BRANCH: process.env.BASE_BRANCH,
 	},
+	webpack: (config, { isServer }) => {
+		// Fixes npm packages that depend on `fs` module
+		if (!isServer) {
+			config.node = {
+				fs: 'empty'
+			}
+		}
+
+		return config
+	},
 	// ...
 }
