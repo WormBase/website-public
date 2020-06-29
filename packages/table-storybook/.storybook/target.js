8b6339a2247511c7335ed7859a847ae4400c6bb1
sthtnr <sthtnr@gmail.com>
2020-06-29 23:36:11 +0900

    Organize Storybook directory structure

---
+++ packages/table-storybook/.storybook/target.js
@@ -0,0 +1,20 @@
+const phenotype_widget = {
+  WBid: {
+    daf8: 'WBGene00000904',
+    daf16: 'WBGene00000912',
+    mig2: 'WBGene00003239',
+  },
+  tableType: {
+    pheno: 'phenotype',
+    phenoNO: 'phenotype_not_observed',
+    phenoBI: 'phenotype_by_interaction',
+    drivesOE: 'drives_overexpression',
+    staging: {
+      pheno: 'phenotype_flat',
+      phenoNO: 'phenotype_not_observed_flat',
+      drivesOE: 'drives_overexpression_flat',
+    },
+  },
+}
+
+export { phenotype_widget }
