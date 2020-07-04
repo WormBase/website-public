8cc88644524bdd141357bd3db07e0799cb6621db
sthtnr <sthtnr@gmail.com>
2020-07-04 16:20:38 +0900

    Add Homology widget(WIP)

--- packages/table-storybook/src/services/loadData.js
+++ packages/table-storybook/src/services/loadData.js
@@ -1,11 +1,6 @@
 const loadData = async (WBid, tableType) => {
   const proxyUrl = 'https://calm-reaches-60051.herokuapp.com/'
-  const targetUrl =
-    tableType === 'phenotype_flat' ||
-    'phenotype_not_observed_flat' ||
-    'drives_overexpression_flat'
-      ? `http://rest-staging.wormbase.org/rest/field/gene/${WBid}/${tableType}`
-      : `http://rest.wormbase.org/rest/field/gene/${WBid}/${tableType}`
+  const targetUrl = `http://rest.wormbase.org/rest/field/gene/${WBid}/${tableType}`
   const res = await fetch(proxyUrl + targetUrl)
   const json = await res.json()
   const jsonSpecific = await json[`${tableType}`]
