547bd0a38a41592fced832cef7a4020506cedef1
sthtnr <sthtnr@gmail.com>
2020-07-07 19:58:26 +0900

    Add new table "other_orthologs"(WIP)

--- packages/data-tables/src/components/widgets/Homology/nematodeOrthologs.stories.js
+++ packages/data-tables/src/components/widgets/Homology/nematodeOrthologs.stories.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import NematodeOrthologs from './NematodeOrthologs'
+import Orthologs from './Orthologs'
 import { homology_widget } from '../../../../.storybook/target'
 
 export default {
-  component: NematodeOrthologs,
+  component: Orthologs,
   title: 'Table|Widgets/Homology/nematode_orthologs',
 }
 
 export const daf16 = () => (
-  <NematodeOrthologs
+  <Orthologs
     WBid={homology_widget.WBid.daf16}
     tableType={homology_widget.tableType.nematodeOrthologs}
   />
