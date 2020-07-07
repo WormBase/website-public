05bdbb26c6ffdeff1d2d864065f6669b6c2f5cab
Sibyl <sibyl@wormbase.org>
2020-07-07 13:09:19 -0400

    add apollo-client

--- sites/third-party-data-api/src/index.stories.js
+++ sites/third-party-data-api/src/index.stories.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Button } from '@storybook/react/demo';
 
-export default { title: 'Button' };
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
 
-export const withText = () => <Button>Hello Button</Button>;
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://rrmvef187j.execute-api.us-east-1.amazonaws.com/dev/graphql'
+});
 
-export const withEmoji = () => (
-  <Button>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+client.query({
+  query: gql`
+    query ReactomePathways {
+      getReactomePathwayByGene(id: "WBGene00006791") {
+        displayName
+        stId
+      }
+    }
+  `
+}).then(result => console.log(JSON.stringify(result, null, 2)));
+
+export default { title: 'Reactome' };
+
+export const example = () => 'test';
