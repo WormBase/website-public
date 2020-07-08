45b389236b8453a5410623d7676bd89e2c6a700c
sthtnr <sthtnr@gmail.com>
2020-07-08 17:16:58 +0900

    Improve so that data of daf-8 and mig-2 can be displayed

--- packages/table-storybook/src/components/widgets/Homology/otherOrthologs.stories.js
+++ packages/table-storybook/src/components/widgets/Homology/otherOrthologs.stories.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import Blastp from './Blastp'
+import Orthologs from './Orthologs'
 import { homology_widget } from '../../../../.storybook/target'
 
 export default {
-  component: Blastp,
-  title: 'Table|Widgets/Homology/best_blastp_matches',
+  component: Orthologs,
+  title: 'Table|Widgets/Homology/other_orthologs',
 }
 
 export const daf8 = () => (
-  <Blastp
+  <Orthologs
     WBid={homology_widget.WBid.daf8}
-    tableType={homology_widget.tableType.bestBlastpMatches}
+    tableType={homology_widget.tableType.otherOrthologs}
   />
 )
 export const daf16 = () => (
-  <Blastp
+  <Orthologs
     WBid={homology_widget.WBid.daf16}
-    tableType={homology_widget.tableType.bestBlastpMatches}
+    tableType={homology_widget.tableType.otherOrthologs}
   />
 )
 export const mig2 = () => (
-  <Blastp
+  <Orthologs
     WBid={homology_widget.WBid.mig2}
-    tableType={homology_widget.tableType.bestBlastpMatches}
+    tableType={homology_widget.tableType.otherOrthologs}
   />
 )
