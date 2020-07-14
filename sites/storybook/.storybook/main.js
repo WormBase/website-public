e5ed37f929ed3a69b67d3146b6b0bd0358516543
Sibyl <sibyl@wormbase.org>
2020-07-14 16:24:36 -0400

    fix storybook webpack loader issue

--- sites/storybook/.storybook/main.js
+++ sites/storybook/.storybook/main.js
@@ -1,6 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+
 module.exports = {
   stories: [
     '../../../sites/**/src/**/*.stories.[tj]s',
     '../../../packages/**/src/**/*.stories.[tj]s'
   ],
+  webpackFinal: (config) => {
+    const sitesPath = path.resolve(__dirname, '../../../sites');
+    const packagesDir = path.resolve(__dirname, '../../../packages');
+    config.module.rules[0].include = fs.readdirSync(sitesPath).map((dir) => path.resolve(sitesPath, dir));
+    config.module.rules[0].exclude = fs.readdirSync(sitesPath).map((dir) => path.resolve(sitesPath, dir, 'node_modules'));
+    console.dir(config, { depth: null });
+    return config;
+  },
 };
