cc61247d114fdc2b2c7a964c136de30b445c9443
Sibyl <sibyl@wormbase.org>
2020-07-14 21:25:51 +0000

    include packages/ in loader

--- sites/storybook/.storybook/main.js
+++ sites/storybook/.storybook/main.js
@@ -8,9 +8,11 @@ module.exports = {
   ],
   webpackFinal: (config) => {
     const sitesPath = path.resolve(__dirname, '../../../sites');
-    const packagesDir = path.resolve(__dirname, '../../../packages');
-    config.module.rules[0].include = fs.readdirSync(sitesPath).map((dir) => path.resolve(sitesPath, dir));
-    config.module.rules[0].exclude = fs.readdirSync(sitesPath).map((dir) => path.resolve(sitesPath, dir, 'node_modules'));
+    const packagesPath = path.resolve(__dirname, '../../../packages');
+    config.module.rules[0].include = fs.readdirSync(sitesPath).map((dir) => path.resolve(sitesPath, dir))
+      .concat(fs.readdirSync(packagesPath).map((dir) => path.resolve(packagesPath, dir)));
+    config.module.rules[0].exclude = fs.readdirSync(sitesPath).map((dir) => path.resolve(sitesPath, dir, 'node_modules'))
+      .concat(fs.readdirSync(packagesPath).map((dir) => path.resolve(packagesPath, dir, 'node_modules')));
     console.dir(config, { depth: null });
     return config;
   },
