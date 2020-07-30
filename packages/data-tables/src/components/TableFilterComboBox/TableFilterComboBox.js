1d75c3fc9d7967cdd4eacedc359240a39ba873a6
Sibyl <sibyl@wormbase.org>
2020-07-30 13:04:06 -0400

    fix filter by interaction label with / : / pattern

--- packages/data-tables/src/components/TableFilterComboBox/TableFilterComboBox.js
+++ packages/data-tables/src/components/TableFilterComboBox/TableFilterComboBox.js
@@ -1,28 +1,29 @@
 import React, { useState, useMemo } from 'react';
 import { useCombobox, useMultipleSelection } from 'downshift';
-import matchSorter from 'match-sorter'
+import matchSorter from 'match-sorter';
 
 function parseSelectedItem(selectedItem) {
-  let [filterName, filterValue] = selectedItem.split(' : ', 2).map(value => value.trim())
+  let [filterName, ...filterValues] = selectedItem.split(' : ');
   return {
-    filterName,
-    filterValue,
-  }
+    filterName: filterName.trim(),
+    filterValue: filterValues.join(' : ').trim(),
+  };
 }
 
-const TableFilterComboBox = ({
-  options,
-  onChange,
-}) => {
-  const [inputValue, setInputValue] = useState('')
+const TableFilterComboBox = ({ options, onChange }) => {
+  const [inputValue, setInputValue] = useState('');
   const items = useMemo(() => {
-    const { filterName } = inputValue.match(/ : /) ? parseSelectedItem(inputValue) : {}
+    const { filterName } = inputValue.match(/ : /)
+      ? parseSelectedItem(inputValue)
+      : {};
     if (filterName) {
-      return (options[filterName] || []).map (item => `${filterName} : ${item}`)
+      return (options[filterName] || []).map(
+        (item) => `${filterName} : ${item}`
+      );
     } else {
-      return Object.keys(options)
+      return Object.keys(options);
     }
-  }, [options, inputValue])
+  }, [options, inputValue]);
   const {
     getSelectedItemProps,
     getDropdownProps,
@@ -31,30 +32,41 @@ const TableFilterComboBox = ({
     reset: resetMultiple,
     selectedItems,
   } = useMultipleSelection({
-    onSelectedItemsChange: ({selectedItems}) => {
-      onChange && onChange(selectedItems.map(item => {
-        const {filterName: key, filterValue: value} = parseSelectedItem(item);
-        return {
-          key,
-          value,
-        }
-      }))
+    onSelectedItemsChange: ({ selectedItems }) => {
+      onChange &&
+        onChange(
+          selectedItems.map((item) => {
+            const { filterName: key, filterValue: value } = parseSelectedItem(
+              item
+            );
+            return {
+              key,
+              value,
+            };
+          })
+        );
     },
-  })
-  const getFilteredItems = items => {
-    const {priorityItems, otherItems} = matchSorter(items, inputValue.trim()).reduce((result, item) => {
-      if (item.match(/\(.+\)$/)) {
-        result.priorityItems.push(item)
-      } else {
-        result.otherItems.push(item)
+  });
+  const getFilteredItems = (items) => {
+    const { priorityItems, otherItems } = matchSorter(
+      items,
+      inputValue.trim()
+    ).reduce(
+      (result, item) => {
+        if (item.match(/\(.+\)$/)) {
+          result.priorityItems.push(item);
+        } else {
+          result.otherItems.push(item);
+        }
+        return result;
+      },
+      {
+        priorityItems: [],
+        otherItems: [],
       }
-      return result
-    }, {
-      priorityItems: [],
-      otherItems: [],
-    })
-    return [...priorityItems, ...otherItems]
-  }
+    );
+    return [...priorityItems, ...otherItems];
+  };
 
   const {
     isOpen,
@@ -72,62 +84,64 @@ const TableFilterComboBox = ({
   } = useCombobox({
     inputValue,
     items: getFilteredItems(items),
-    onStateChange: ({inputValue, type, selectedItem}) => {
+    onStateChange: ({ inputValue, type, selectedItem }) => {
       console.log([inputValue, type, selectedItem]);
       switch (type) {
         case useCombobox.stateChangeTypes.InputChange:
-          setInputValue(inputValue)
+          setInputValue(inputValue);
 
-          break
+          break;
         case useCombobox.stateChangeTypes.InputKeyDownEnter:
         case useCombobox.stateChangeTypes.ItemClick:
         case useCombobox.stateChangeTypes.InputBlur:
           if (selectedItem) {
-            const { filterName, filterValue } =  parseSelectedItem(selectedItem)
+            const { filterName, filterValue } = parseSelectedItem(selectedItem);
             if (filterName && filterValue) {
-              setInputValue('')
-              addSelectedItem(selectedItem)
-              closeMenu()
-              selectItem(null)
+              setInputValue('');
+              addSelectedItem(selectedItem);
+              closeMenu();
+              selectItem(null);
             } else {
-              setInputValue(`${filterName} : `)
-              openMenu() // show suggestion on the values available for the key entered
+              setInputValue(`${filterName} : `);
+              openMenu(); // show suggestion on the values available for the key entered
             }
           }
 
-          break
+          break;
         case useCombobox.stateChangeTypes.FunctionSelectItem:
           if (selectedItem) {
-            setInputValue('')
-            const { filterName, filterValue } =  parseSelectedItem(selectedItem)
-            if (filterName && filterValue ) {
-              addSelectedItem(selectedItem)
+            setInputValue('');
+            const { filterName, filterValue } = parseSelectedItem(selectedItem);
+            if (filterName && filterValue) {
+              addSelectedItem(selectedItem);
             } else {
-              addSelectedItem(`search : ${selectedItem}`)
+              addSelectedItem(`search : ${selectedItem}`);
             }
-            closeMenu()
-            selectItem(null)
+            closeMenu();
+            selectItem(null);
           }
 
-          break
+          break;
         default:
-          break
+          break;
       }
     },
-  })
+  });
 
   return (
     <div>
       <label {...getLabelProps()}>Filering by:</label>
-      <div style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        border: '1px solid #ccc',
-      }}>
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          border: '1px solid #ccc',
+        }}
+      >
         {selectedItems.map((selectedItem, index) => (
           <div
             key={`selected-item-${index}`}
-            {...getSelectedItemProps({selectedItem, index})}
+            {...getSelectedItemProps({ selectedItem, index })}
             style={{
               padding: 5,
               margin: 5,
@@ -135,38 +149,45 @@ const TableFilterComboBox = ({
             }}
           >
             {selectedItem}
-            <span
-              onClick={() => removeSelectedItem(selectedItem)}
-            >
+            <span onClick={() => removeSelectedItem(selectedItem)}>
               &#10005;
             </span>
           </div>
         ))}
-        <div {...getComboboxProps()}
+        <div
+          {...getComboboxProps()}
           style={{
             flex: '1 0 500px',
             padding: 5,
           }}
         >
           <input
-            {...getInputProps(getDropdownProps({
-              onFocus: openMenu,
-              onBlur: closeMenu,
-              onKeyDown: (event) => {
-                if (event.key === 'Enter' && highlightedIndex === -1 && inputValue) {
-                  selectItem(inputValue);
-                }
-              }
-            }))}
+            {...getInputProps(
+              getDropdownProps({
+                onFocus: openMenu,
+                onBlur: closeMenu,
+                onKeyDown: (event) => {
+                  if (
+                    event.key === 'Enter' &&
+                    highlightedIndex === -1 &&
+                    inputValue
+                  ) {
+                    selectItem(inputValue);
+                  }
+                },
+              })
+            )}
             style={{
               width: 'calc(100% - 60px)',
             }}
           />
-          <button onClick={() => {
-            setInputValue('')
-            resetCombobox()
-            resetMultiple()
-          }}>
+          <button
+            onClick={() => {
+              setInputValue('');
+              resetCombobox();
+              resetMultiple();
+            }}
+          >
             &#10005;
           </button>
           <button {...getToggleButtonProps()} aria-label={'toggle menu'}>
@@ -174,22 +195,22 @@ const TableFilterComboBox = ({
           </button>
         </div>
       </div>
-      <ul {...getMenuProps()} style={{backgroundColor: '#eee'}}>
+      <ul {...getMenuProps()} style={{ backgroundColor: '#eee' }}>
         {isOpen &&
           getFilteredItems(items).map((item, index) => (
             <li
               style={
-                highlightedIndex === index ? {backgroundColor: '#bde4ff'} : {}
+                highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}
               }
               key={`${item}${index}`}
-              {...getItemProps({item, index})}
+              {...getItemProps({ item, index })}
             >
               {item}
             </li>
           ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default TableFilterComboBox;
