d66d3c11573f4ac1501d842476e669877ccfe106
Sibyl <sibyl@wormbase.org>
2020-08-14 16:50:51 -0400

    wider margin for simple cell

--- packages/data-tables/src/components/SimpleCell.js
+++ packages/data-tables/src/components/SimpleCell.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: `${theme.spacing(0.5)}px 0`,
+    margin: `${theme.spacing(0.75)}px 0`,
   },
 }));
 
