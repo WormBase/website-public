129ffd4cd49e39d95a3e6928bf8b02d7d98f8f3e
Sibyl <sibyl@wormbase.org>
2020-08-14 18:29:03 -0400

    style evidence and hash cell

--- packages/data-tables/src/components/HashCell.js
+++ packages/data-tables/src/components/HashCell.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import { hasContent } from '../util/hasContent';
 import SimpleCell from './SimpleCell';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    margin: 0,
+  },
+}));
+
 const HashCell = ({ data, render }) => {
+  const classes = useStyles();
+
   if (data.species) {
     return <SimpleCell data={`${data.genus}. ${data.species}`} />;
   }
   return (
-    <dl>
+    <dl className={classes.root}>
       {Object.keys(data)
         .filter((key) => hasContent(data[key]))
         .map((key) => (
