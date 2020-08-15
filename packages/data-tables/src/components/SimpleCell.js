94ee504a9186a00786da82bd8e87e0f756d0a98a
Sibyl <sibyl@wormbase.org>
2020-08-14 20:59:27 -0400

    avoid long word overflow the cell

--- packages/data-tables/src/components/SimpleCell.js
+++ packages/data-tables/src/components/SimpleCell.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     margin: `${theme.spacing(0.75)}px 0`,
+    overflowWrap: 'anywhere',
   },
 }));
 
