ef101f9c2140dcab2effd3ba66fe84112b9e4078
Sibyl <sibyl@wormbase.org>
2020-10-06 15:58:25 -0400

    add story for diesese table

---
+++ packages/data-tables/src/components/stories/Diseases/disease.stories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Wrapper from '../../Wrapper';
+
+const id = 'table_table_detailed_disease_model';
+const order = [
+  'disease_term',
+  'genetic_entity',
+  'genotype',
+  'evidence_code',
+  'experimental_condition',
+  'modifier',
+  'description',
+  'reference',
+];
+
+const columnsHeader = {
+  disease_term: 'Disease',
+  genetic_entity: 'Genetic entity',
+  genotype: 'Genotype',
+  association_type: 'Association type',
+  evidence_code: 'Evidence code',
+  experimental_condition: 'Inducer',
+  modifier: 'Modifier',
+  modifier_association_type: 'Modifier association type',
+  description: 'Description',
+  reference: 'Reference',
+};
+
+export default {
+  component: Wrapper,
+  title: 'Table/Generic/Widgets/Diseases/detailed_disease_model',
+};
+
+export const nlg1 = () => (
+  <Wrapper
+    WBid="WBGene00006412"
+    tableType="detailed_disease_model"
+    {...{ id, order, columnsHeader }}
+  />
+);
