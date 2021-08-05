263e340d295f29ae71b95e56d7a23e6a04c8028a
Sibyl <sibyl@wormbase.org>
2021-08-05 17:12:41 -0400

    basic chart for cengen tpm

--- client/src/components/CenGenChart/index.js
+++ client/src/components/CenGenChart/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import useGeneExpression from './useGeneExpression';
+import ExpressionChart from './ExpressionChart';
 
 export default function({ geneId }) {
   const { loading, error, data } = useGeneExpression(geneId);
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <ExpressionChart data={data} />;
 }
