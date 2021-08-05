7573aecbdafd1968ea536947a6698fb6a6c4f11a
Sibyl <sibyl@wormbase.org>
2021-08-05 17:10:57 -0400

    move fetch into useEffect

--- client/src/components/CenGenChart/useGeneExpression.js
+++ client/src/components/CenGenChart/useGeneExpression.js
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function useGeneExpression(geneId) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
 
-  fetch(
-    `https://22rx59z6c7.execute-api.us-east-1.amazonaws.com/dev/items/${geneId}`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setData(data);
-      setLoading(false);
-    })
-    .catch((error) => {
-      setError(error);
-      setLoading(false);
-    });
+  useEffect(() => {
+    fetch(
+      `https://22rx59z6c7.execute-api.us-east-1.amazonaws.com/dev/items/${geneId}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [setLoading, setError, setData, geneId]);
 
   return {
     data,
