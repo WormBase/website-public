c5675caf25c60592fcf86a0f1351887c19f1c4ac
Paulo Nuin <nuin@genedrift.org>
2023-11-17 09:47:55 -0700

    finalizing link move from tazendra

--- lib/WormBase/API/Object/Person.pm
+++ lib/WormBase/API/Object/Person.pm
@@ -508,7 +508,7 @@ sub worked_with {
 sub ancestors_data {
     my $self    = shift;
     my $json        = JSON->new->allow_nonref;
-    my $jsonUrl     = 'http://tazendra.caltech.edu/~azurebrd/cgi-bin/forms/wbpersonLineageScaling.json';
+    my $jsonUrl     = 'http://caltech-curation.textpressolab.com/~azurebrd/cgi-bin/forms/wbpersonLineageScaling.json';
     my $page        = get $jsonUrl;
     my $perl_scalar = $json->decode( $page );         # get the solr data
     my %scaling     = %$perl_scalar;                     # decode the solr page into a hash
